2a3dbf224cd972f67aede3ddc59c23adc9bb356e
kaushalmodi <kaushal.modi@gmail.com>
2014-02-04 11:17:36 -0500

    Added files

---
+++ custom.el
@@ -0,0 +1,46 @@
+;; Do NOT manually edit the below custom-set-variables block
+;; Comments added manually in above custom-set-variables block are not
+;; retained. To add comments, do `M-x customize` search for that variable, click
+;; on State button for that variable and click on "Add comment". After doing
+;; that, click on State button once again and click on "Save for future sessions".
+;; *Manually added comments get deleted when emacs updates this block.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-ignores (quote ("/" "//" "/*" "//*" "///" "////")))
+ '(ac-stop-words (quote ("/" "//" "/*" "//*" "///" "////")))
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(auto-compression-mode t nil (jka-compr) "uncompress->edit->save->compress .gz, .bz2, .Z files on the fly")
+ '(case-fold-search t nil nil "ignore case when searching; searching using an all-lowercase string will match the same strings with uppercase characters too")
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes (quote ("bbd99b10462e3d0f4ac6da09e23e5de1fed4f7fe87d679a6468a14b0b6c8e634" "b1a6b113840c7f739ccf2a330f28dd8f9f29a37081312a3f00845fd4ba3f3489" "9370aeac615012366188359cb05011aea721c73e1cb194798bc18576025cabeb" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "f3ceb7a30f6501c1093bc8ffdf755fe5ddff3a85437deebf3ee8d7bed8991711" "5f7580488c9eb32997d0cd29ab1720c944cdf76d543a860588c733cead175e15" "8eef22cd6c122530722104b7c82bc8cdbb690a4ccdd95c5ceec4f3efa5d654f5" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "f3d2144fed1adb27794a45e61166e98820ab0bbf3cc7ea708e4bf4b57447ee27" "a2c537c981b4419aa3decac8e565868217fc2995b74e1685c5ff8c6d77b198d6" "31bfef452bee11d19df790b82dea35a3b275142032e06c6ecdc98007bf12466c" default)))
+ '(default-input-method "rfc1345")
+ '(delete-selection-mode t nil nil "typing anything after highlighting text overwrites that text; source: http://emacsredux.com/blog/2013/04/12/delete-selection-on-insert/")
+ '(doc-view-continuous t)
+ '(fci-rule-color "dim gray")
+ '(fringe-mode nil nil (fringe))
+ '(indent-tabs-mode nil nil nil "use spaces instead of tabs for indentation")
+ '(indicate-buffer-boundaries (quote ((top . right) (bottom . right))) nil nil "show frame boundaries in the fringe; as the fringe is activated only for the right-hand side, the buffer boundaries (top, bottom) are marked on the right side")
+ '(keyboard-coding-system (quote utf-8-unix) nil nil "default EOL system = that of Unix")
+ '(major-mode (quote text-mode) nil nil "If the default value of major-mode is nil, the major mode is taken from the previously current buffer")
+ '(next-line-add-newlines nil nil nil "Do not auto-add newlines at the end of the file on pressing `C-n` or down arrow")
+ '(projectile-global-mode t)
+ '(require-final-newline nil nil nil "Do not auto-add a final newline (if one is not present) when saving/visiting a file")
+ '(save-place t nil (saveplace) "save the last cursor location for each file")
+ '(show-paren-mode t nil (paren) "allow one to see matching pairs of parentheses; when point is on one of the paired characters, the other is highlighted")
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3")
+ '(visible-bell t nil nil "enable the visible bell or screen blink to happen when there's any error")
+ '(x-select-enable-clipboard t nil nil "after copy Ctrl+c in X11 apps, you can paste by 'yank' in emacs")
+ '(x-select-enable-primary t nil nil "after mouse selection in X11, you can paste by 'yank' in emacs"))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(linum ((t (:foreground "dim gray" :slant italic))) t))
---
+++ highlight-global.el
@@ -0,0 +1,246 @@
+;;; highlight-global.el --- package for highlighting multi symbols accross ALL buffer.
+;;;
+;;; When reading source code with EMACS, couples of related files will
+;;; be opened simultaneously. A function/variable defined in one file
+;;; will referenced by another function in another file. A highlight
+;;; of the corresponding function/vairalbe accross these buffers will
+;;; make code reading much more friendly.
+;;;
+;;; EMACS support multi highlight symbols in one buffer but not
+;;; accross multi buffers. This package provide highlighting of
+;;; symbols accross all buffer.
+;;;
+;;; When new highlight is being added, only the windows of current
+;;; frame are updated. Whenever frame configration change(say new
+;;; window is added to current frame), window's highlight will be
+;;; updated to make windows currently showing always has the updated
+;;; highlights. This way is much more effecient than iterating the
+;;; overall buffer-list to update highlights for every buffer.
+;;;
+;;; Multi symbols can be highlighted simultaneously. Different
+;;; highlights have different face. You could add your highlight face
+;;; to highlight-faces.
+
+;; toggle highlight of current region(all symbol under cursor if region is not active)
+;; (global-set-key (kbd "M-\"") 'highlight-frame-toggle)
+
+;; clear all highlight of current frame
+;; (global-set-key (kbd "M-+") 'clear-highlight-frame)
+
+(require 'hi-lock)
+(setq hi-lock-file-patterns-policy 'never)
+
+(defvar highlight-faces
+  '(('hi-yellow . 0)
+    ('hi-pink . 0)
+    ('hi-green . 0)
+    ('hi-blue . 0))
+  "Default faces for hi-lock interactive functions, you could add your own.")
+
+(defun clear-all-faces ()
+  "Reset all face's usage count to zero."
+  (dolist (item highlight-faces)
+    (setcdr item 0)))
+
+(defun find-and-use-face ()
+  "Find the least used face and increase it, the face will be returned to caller"
+  (let ((least-used-one (nth 0 highlight-faces)))
+    (progn
+      (dolist (face highlight-faces)
+        (when (< (cdr face) (cdr least-used-one))
+          (setq least-used-one face)))
+      (setcdr least-used-one (+ 1 (cdr least-used-one)))
+      (car least-used-one))))
+
+(defun release-face (face-to-release)
+  "Release the use of a face by decreasing the counting"
+  (dolist (face highlight-faces)
+    (when (equal (car face) face-to-release)
+      (setcdr face (- (cdr face) 1)))))
+
+;; list to store what had been highlighted
+(defvar global-highlight-list nil
+  "Global highlight list, always store the updated highlight
+  regexp list, and every item is stored like this ((hilight-str1
+  . hilight-face1) (hilight-str2 . hilight-face2) ...)")
+
+(defvar global-highlight-list-update-timestamp 0.0
+  "Store the timestamp when `global-highlight-list' was updated")
+
+(defvar new-unhighlight nil "Store's thing to be unhighlight")
+(defvar new-highlight nil "Store's thing to be highlight")
+
+(defvar buffer-highlight-list nil
+  "Stores the regexp highlighed by `highlight-windows' package of
+  current buffer")
+(make-variable-buffer-local 'buffer-highlight-list)
+(put 'buffer-highlight-list 'permanent-local t)
+
+;; set it to -0.5 to make sure first time it will update
+(defvar buffer-highlight-list-update-timestamp -0.5
+  "Stores the recently timestamp when `buffer-highlight-list' was
+  updated")
+(make-variable-buffer-local 'buffer-highlight-list-update-timestamp)
+(put 'buffer-highlight-list-update-timestamp 'permanent-local t)
+
+(defun clear-highlight-window (win)
+  "Clear all highlight of current buffer, called by
+  `unhighlit-windows-all' when iterating all windows. When a
+  buffer is being burry, this funciton also will be called to
+  clear all highlight"
+  (select-window win)
+  (setq buffer-highlight-list-update-timestamp (float-time))
+  (dolist (item global-highlight-list)
+    (font-lock-remove-keywords
+     nil
+     `((,(car item) 0 ,(cdr item) prepend)))
+    (font-lock-fontify-buffer)))
+
+(defun clear-highlight-frame ()
+  "Clear all highlights of all windows "
+  (interactive)
+  (walk-windows 'clear-highlight-window)
+  (setq global-highlight-list nil)
+  (clear-all-faces)
+  (setq global-highlight-list-update-timestamp (float-time)))
+
+(defun unhighlight-window (win)
+  "Highligt a buffer, should update of buffer-local
+highlight-list and timestamp, used by `walk-windows'"
+  (select-window win)
+  (setq buffer-highlight-list-update-timestamp (float-time))
+  (setq buffer-highlight-list
+        (delete new-unhighlight buffer-highlight-list))
+  ;; add new highlight to current buffer's keyword list
+  (font-lock-remove-keywords
+   nil
+   `((,(car new-unhighlight) 0 ,(cdr new-unhighlight) prepend)))
+  (font-lock-fontify-buffer))
+
+(defun highlight-window (win)
+  "Highligt a buffer, should update buffer-local highlight-list
+and timestamp"
+  (select-window win)
+  (setq buffer-highlight-list-update-timestamp (float-time))
+  (push new-highlight buffer-highlight-list)
+  (font-lock-add-keywords
+   nil
+   `((,(car new-highlight) 0 ,(cdr new-highlight) prepend)) 'append)
+  (font-lock-fontify-buffer))
+
+(defun get-thing-to-highlight ()
+  "Get thing to highlight. If active region, get reigon, else get
+symbol under cursor"
+  (if (use-region-p)
+      (buffer-substring-no-properties (region-beginning) (region-end))
+    (if (thing-at-point 'symbol)
+        (buffer-substring-no-properties
+         (car (bounds-of-thing-at-point 'symbol))
+         (cdr (bounds-of-thing-at-point 'symbol))))))
+
+(defun check-whether-highlighted (hi)
+  "Check if HI is already highlighted by checking
+global-highlight-list"
+  (let ((the-found-one nil))
+  (progn
+    (dolist (item global-highlight-list)
+      (when (equal hi (car item))
+        (progn
+          (setq the-found-one item))))
+    the-found-one)))
+
+(defun highlight-frame-toggle ()
+  (interactive)
+  (let* ((thing-to-highlight (get-thing-to-highlight))
+         (hi nil)
+         (face nil))
+    (if (stringp thing-to-highlight)
+        (progn
+          (setq hi (check-whether-highlighted thing-to-highlight))
+        ;; toogle highlight, 2 cases
+        ;; 1) thing already unlighlight and stored in list, unhighight it
+        ;; 2) new highlight, highlight it and add it to list
+          (if hi
+            ;; 1) toogle off
+            ;;    1. delete item from global-list && update timestamp
+            ;;    2. set new-unhighlight and unhighight each window
+            (progn
+              (setq new-unhighlight hi)
+              (release-face (cdr new-unhighlight))
+              (setq global-highlight-list
+                    (delete new-unhighlight global-highlight-list))
+              (setq global-highlight-list-update-timestamp (float-time))
+              (walk-windows 'unhighlight-window))
+          ;; 2) new highlight
+            (progn
+              (setq new-highlight (cons thing-to-highlight (find-and-use-face)))
+              (setq global-highlight-list (cons new-highlight global-highlight-list))
+              (setq global-highlight-list-update-timestamp (float-time))
+              (walk-windows 'highlight-window))))
+      (message "No vaidate region, or no validate symbol under cursor!"))))
+
+(defun highlight-update-current-buffer ()
+  "Update a buffer's highlight to be consistent with global
+highlight"
+  (if (<= buffer-highlight-list-update-timestamp
+          global-highlight-list-update-timestamp)
+      (if (null global-highlight-list)      ; clear buffer
+          ;; 1) global null
+          (progn
+            (dolist (item buffer-highlight-list)
+              (font-lock-remove-keywords
+               nil
+               `((,(car item) 0 ,(cdr item) prepend)))
+              (font-lock-fontify-buffer))
+            (setq buffer-highlight-list-update-timestamp (float-time))
+            (setq buffer-highlight-list nil))
+        ;; 2) globla is not null, now update local to global
+        (progn
+          ;; (message "Updating buffer : %s" (current-buffer))
+          ;; 2.1) iterate buffer-list to delete
+          (dolist (item buffer-highlight-list)
+            (if (not (member item global-highlight-list))
+                (progn
+                  ;; `((,(car new-highlight) 0 ,(cdr new-highlight) prepend))
+                  ;; (list (car item))
+                  (font-lock-remove-keywords
+                   nil
+                   `((,(car item) 0 ,(cdr item) prepend)))
+                  (font-lock-fontify-buffer)
+                  (setq buffer-highlight-list
+                        (delete item buffer-highlight-list)))))
+          ;; 2.2) iterate global-list to add
+          (dolist (item global-highlight-list)
+            (if (not (member item buffer-highlight-list))
+                (progn
+                  (font-lock-add-keywords
+                   nil
+                   `((,(car item) 0 ,(cdr item) prepend)) 'append)
+                  (font-lock-fontify-buffer)
+                  (push item buffer-highlight-list))))
+          (setq buffer-highlight-list-update-timestamp (float-time)))
+        ;; (message "no need to update : %s" (current-buffer))
+        )))
+
+;; Force to update highlights on current frame, call this function
+;; will update highlights in every window within current frame
+(defun force-highlight-frame ()
+  (interactive)
+  (save-excursion
+    (walk-windows #'(lambda (win)
+                    (select-window win)
+                    (highlight-update-current-buffer)))))
+
+;; Automatically update new buffer's highlights when any windows on
+;; current frame changed. This will make buffers that to be showned
+;; because of window splitting alway has highlights updated to date.
+(defun update-highlight-fixup (frame)
+  (force-highlight-frame))
+
+;; register the on-the-fly highlight-list updating strategy to
+;; window-size-change-functions hook
+(if (null window-size-change-functions)
+    (setq window-size-change-functions '(update-highlight-fixup))
+  (add-to-list 'window-size-change-functions 'update-highlight-fixup))
+
+(provide 'highlight-global)
