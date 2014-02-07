841d7c4a389f5b8f2c6355ff0c484c818e29bebe
Kaushal Modi <kaushal.modi@gmail.com>
2014-02-07 00:36:31 -0500

    Uncommented the font-size* functions because they change the font size of text outside the buffer too, example in the mode-line

--- setup-files/setup-visual.el
+++ setup-files/setup-visual.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-02-04 12:28:10 kmodi>
+;; Time-stamp: <2014-02-07 00:35:19 Kaushal>
 
 ;; Set up the looks of emacs
 
@@ -83,26 +83,28 @@
 ;; So a 10pt font size is equal to 100 in internal font size value.
 (set-face-attribute 'default nil :height (* font-size-pt 10))
 
-;; Below custom function are not required, the default C-x C-0/-/= bindings do
-;; excellent job
+;; Below custom function are not required usually as the default C-x C-0/-/=
+;; bindings do excellent job. The default binding though does not change the
+;; font sizes for the text outside the buffer, example in mode-line.
+;; Below functions change the font size in those areas too.
 
-;; (defun font-size-incr ()
-;;   "Increase font size by 1 pt"
-;;   (interactive)
-;;   (setq font-size-pt (+ font-size-pt 1))
-;;   (set-face-attribute 'default nil :height (* font-size-pt 10)))
+(defun font-size-incr ()
+  "Increase font size by 1 pt"
+  (interactive)
+  (setq font-size-pt (+ font-size-pt 1))
+  (set-face-attribute 'default nil :height (* font-size-pt 10)))
 
-;; (defun font-size-decr ()
-;;   "Decrease font size by 1 pt"
-;;   (interactive)
-;;   (setq font-size-pt (- font-size-pt 1))
-;;   (set-face-attribute 'default nil :height (* font-size-pt 10)))
+(defun font-size-decr ()
+  "Decrease font size by 1 pt"
+  (interactive)
+  (setq font-size-pt (- font-size-pt 1))
+  (set-face-attribute 'default nil :height (* font-size-pt 10)))
 
-;; (defun font-size-reset ()
-;;   "Reset font size to default-font-size-pt"
-;;   (interactive)
-;;   (setq font-size-pt default-font-size-pt)
-;;   (set-face-attribute 'default nil :height (* font-size-pt 10)))
+(defun font-size-reset ()
+  "Reset font size to default-font-size-pt"
+  (interactive)
+  (setq font-size-pt default-font-size-pt)
+  (set-face-attribute 'default nil :height (* font-size-pt 10)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
