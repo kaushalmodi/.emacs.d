0c503e0bf20fd57c7ed88bc0a3215bce430b5c59
Kaushal Modi <kaushal.modi@gmail.com>
2014-02-18 14:01:34 -0500

    Added ido-based completion to magit; by setting magit-repo-dirs, youcan call (magit-status) even from a non-git buffer

--- setup-files/setup-magit.el
+++ setup-files/setup-magit.el
@@ -1,11 +1,14 @@
-;; Time-stamp: <2014-02-04 13:24:09 kmodi>
+;; Time-stamp: <2014-02-18 13:26:32 kmodi>
 
 ;; magit
 ;; Source: https://github.com/magit/magit
 
 (require 'magit)
 
-;;
+(setq magit-completing-read-function 'magit-ido-completing-read
+      magit-repo-dirs '( "~/.emacs.d"
+                         )
+      )
 
 
 (setq setup-magit-loaded t)
