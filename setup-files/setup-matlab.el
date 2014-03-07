32cbc3c7213332246fa0caae50e4705335ca75c2
Kaushal Modi <kaushal.modi@gmail.com>
2014-03-07 15:17:54 -0500

    Added comments to show how default matlab-shell-mode-map key bindings can be overriden

--- setup-files/setup-matlab.el
+++ setup-files/setup-matlab.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2013-12-02 17:07:12 kmodi>
+;; Time-stamp: <2014-03-04 09:18:51 kmodi>
 
 ;; Matlab
 
@@ -9,5 +9,9 @@
 (autoload 'matlab-mode "matlab" "Enter Matlab mode." t)
 (setq auto-mode-alist (cons '("\\.m\\'" . matlab-mode) auto-mode-alist))
 (autoload 'matlab-shell "matlab" "Interactive Matlab mode." t)
+
+;; (define-key matlab-shell-mode-map (kbd "C-p") 'matlab-shell-previous-matching-input-from-input)
+;; (define-key matlab-shell-mode-map (kbd "C-n") 'matlab-shell-next-matching-input-from-input)
+
 
 (provide 'setup-matlab)
