e82b42406e8ff2dc343c0b3660aaa9a1c01b5fa1
Kaushal Modi <kaushal.modi@gmail.com>
2014-03-07 15:25:19 -0500

    Initial commit

---
+++ setup-files/setup-yaml-mode.el
@@ -0,0 +1,12 @@
+;; Time-stamp: <2014-03-07 15:24:51 kmodi>
+
+;; yaml-mode
+;; Useful for configuring Octopress' _config.yml
+
+(require 'yaml-mode)
+
+;;
+
+
+(setq setup-yaml-mode-loaded t)
+(provide 'setup-yaml-mode)
