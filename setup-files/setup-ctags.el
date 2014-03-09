21f1662ae83acb5d331a28e575488890be432e85
Kaushal Modi <kaushal.modi@gmail.com>
2014-03-09 04:57:11 -0400

    Removed redundant code, used loop to sequentially call ctags for each folder in tags-table-list in the build-ctags function.

--- setup-files/setup-ctags.el
+++ setup-files/setup-ctags.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-03-08 16:13:14 kmodi>
+;; Time-stamp: <2014-03-09 04:57:01 kmodi>
 
 ;; ctags, etags
 
@@ -14,31 +14,18 @@
 ;; Don't warn when TAGS files are large
 (setq large-file-warning-threshold nil)
 
+;; List all directories from which you want to read the TAGS files
+(setq tags-table-list '("~/.emacs.d"))
+
+(if (boundp 'setup-sos-loaded)
+    (add-to-list 'tags-table-list project-root t))
 
 (defun build-ctags ()
   (interactive)
-  (message "building .emacs.d tags")
-  (shell-command (concat "ctags -Re -f " user-emacs-directory "/TAGS " user-emacs-directory "/"))
-  (when (boundp 'setup-sos-loaded)
-    (message "building project tags")
-    (shell-command (concat "ctags -Re -f " project-root "TAGS " project-root)))
-  (update-ctags)
+  (loop for dir in tags-table-list do
+        (message (concat "building tags in " dir))
+        (shell-command (concat "ctags -Re -f " dir "/TAGS " dir)))
   (message "tags built successfully"))
-
-;; Source: https://www.gnu.org/software/emacs/manual/html_node/emacs/Select-Tags-Table.html#Select-Tags-Table
-;; You cannot set both tags-file-name (that's what visit-tags-table does) and
-;; tags-table-list
-(defun update-ctags ()
-  (interactive)
-  (setq tags-file-name 'nil)
-  ;; List all directories from which you want to read the TAGS files
-  (if (boundp 'setup-sos-loaded)
-      (setq tags-table-list
-            `("~/.emacs.d"
-              ,project-root))
-    (setq tags-table-list
-          `("~/.emacs.d")))
-  (message "Updated tags-table-list."))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -61,30 +48,6 @@
 ;;  1 -> (etags-select-by-tag-number "1")
 ;;  ..                               ..
 ;;  9 -> (etags-select-by-tag-number "9")
-
-;; (defun my-find-tag ()
-;;   (interactive)
-;;   (if (file-exists-p (concat user-emacs-directory "/TAGS"))
-;;       (update-ctags)
-;;     (build-ctags))
-;;   (when (boundp 'setup-sos-loaded)
-;;     (if (file-exists-p (concat project-root "TAGS"))
-;;         (update-ctags)
-;;       (build-ctags)))
-;;   (etags-select-find-tag-at-point))
-
-;; ;; Source: http://www.emacswiki.org/EtagsSelect
-;; ;; Use ido to list tags, but then select via etags-select (best of both worlds!)
-;; (defun my-ido-find-tag ()
-;;   "Find a tag using ido"
-;;   (interactive)
-;;   (tags-completion-table)
-;;   (let (tag-names)
-;;     (mapatoms (lambda (x)
-;;                 (push (prin1-to-string x t) tag-names))
-;;               tags-completion-table)
-;;     (etags-select-find (ido-completing-read "Tag: " tag-names))))
-;; ;; (global-set-key (kbd "M-.") 'my-ido-find-tag)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -119,7 +82,7 @@
 (require 'ctags-update)
 
 ;; Auto update
-(setq ctags-update-delay-seconds (* 60 60)) ;; every 1 hour
+(setq ctags-update-delay-seconds (* 30 60)) ;; every 1/2 hour
 (autoload 'turn-on-ctags-auto-update-mode "ctags-update" "turn on `ctags-auto-update-mode'." t)
 (add-hook 'verilog-mode-hook    'turn-on-ctags-auto-update-mode)
 (add-hook 'emacs-lisp-mode-hook 'turn-on-ctags-auto-update-mode)
@@ -149,3 +112,8 @@
 
 (setq setup-ctags-loaded t)
 (provide 'setup-ctags)
+
+
+;; NOTES
+
+;; (1) Emacs reread the TAGS file during every tag find operation.
