dc8d845e30f869c4e888ed8bfb276b9fc2d8d4dc
Kaushal Modi <kaushal.modi@gmail.com>
2014-03-09 04:54:32 -0400

    Reverted back to using etags-select package for tag finding instead of helm-etags+ because helm-etags+ can't jump to macro/define definitions in Verilog. If the macro used is `XYZ, helm strips off the prefix backquote and so the macro at point can't be searched in the TAGS file

--- setup-files/setup-key-bindings.el
+++ setup-files/setup-key-bindings.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-03-08 16:09:21 kmodi>
+;; Time-stamp: <2014-03-08 16:57:30 kmodi>
 
 ;; KEY BINDINGS
 
@@ -445,8 +445,8 @@
 
 (when (boundp 'setup-ctags-loaded)
   ;; overriding the default binding to `find-tag'
-  ;; (global-set-key (kbd "M-.") 'etags-select-find-tag-at-point)
-  (global-set-key (kbd "M-.") 'helm-etags+-select)
+  (global-set-key (kbd "M-.") 'etags-select-find-tag-at-point)
+  ;; (global-set-key (kbd "M-.") 'helm-etags+-select)
   )
 
 
