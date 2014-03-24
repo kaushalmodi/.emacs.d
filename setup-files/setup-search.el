8c0dc56a194542c69eb39b47bf0f27bcd93fb8a2
Kaushal Modi <kaushal.modi@gmail.com>
2014-03-24 17:05:56 -0400

    Add function to exchange string1 and string2 in the buffer or selected region

--- setup-files/setup-search.el
+++ setup-files/setup-search.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-02-18 11:55:55 kmodi>
+;; Time-stamp: <2014-03-20 14:29:15 kmodi>
 
 ;; Source: http://www.emacswiki.org/emacs/SearchAtPoint
 
@@ -109,6 +109,36 @@ searches all buffers."
       anzu-search-threshold            1000 ;; anzu stops searching after reaching 1000 matches
       anzu-replace-to-string-separator " => "
       )
+
+
+;; Inspired from http://www.emacswiki.org/emacs/QueryExchange and definition of
+;; `query-replace-regexp' from replace.el
+(defun query-exchange (string-1 string-2 &optional delimited start end)
+  "Exchange string-1 and string-2 interactively.
+The user is prompted at each instance like query-replace. Exchanging
+happens within a region if one is selected."
+ (interactive
+   (let ((common
+	  (query-replace-read-args
+	   (concat "Query replace"
+		   (if current-prefix-arg " word" "")
+		   " regexp"
+		   (if (and transient-mark-mode mark-active) " in region" ""))
+	   t)))
+     (list (nth 0 common) (nth 1 common) (nth 2 common)
+	   ;; These are done separately here
+	   ;; so that command-history will record these expressions
+	   ;; rather than the values they had this time.
+	   (if (and transient-mark-mode mark-active)
+	       (region-beginning))
+	   (if (and transient-mark-mode mark-active)
+	       (region-end)))))
+
+  (perform-replace
+   (concat "\\(" string-1 "\\)\\|" string-2)
+   '(replace-eval-replacement replace-quote
+                              (if (match-string 1) string-2 string-1))
+   t t delimited nil nil start end))
 
 
 (setq setup-search-loaded t)
