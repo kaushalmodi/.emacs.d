d110966d66707749d011ac0bc9c8710e1c878dc6
Kaushal Modi <kaushal.modi@gmail.com>
2014-04-16 13:20:07 -0400

    Theme update, fringe color update

--- custom.el
+++ custom.el
@@ -16,9 +16,10 @@
  '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
  '(auto-compression-mode t nil (jka-compr) "uncompress->edit->save->compress .gz, .bz2, .Z files on the fly")
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(bzg-big-fringe-mode nil t)
  '(case-fold-search t nil nil "ignore case when searching; searching using an all-lowercase string will match the same strings with uppercase characters too")
  '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes (quote ("16248150e4336572ff4aa21321015d37c3744a9eb243fbd1e934b594ff9cf394" "75b3e2b7fcf51439686d51dba028d933adac88b69fd725f03bfc5975f4efd145" "11d069fbfb0510e2b32a5787e26b762898c7e480364cbc0779fe841662e4cf5d" "ab3a5935a219ca4d4c6aea5defc9f4b4199e248d45bf93b9e72e43f1242e44e3" "0f0e3af1ec61d04ff92f238b165dbc6d2a7b4ade7ed9812b4ce6b075e08f49fe" "31928331f4eb5d343e70c9b56afabdbb5514e8b906fa67db1ccc6d751a05a5d4" "a3d519ee30c0aa4b45a277ae41c4fa1ae80e52f04098a2654979b1ab859ab0bf" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "76b226dd750d085eaaf7efa5eb07a3282223d74f327a0f4319512c0a59f6df39" "bbd99b10462e3d0f4ac6da09e23e5de1fed4f7fe87d679a6468a14b0b6c8e634" "b1a6b113840c7f739ccf2a330f28dd8f9f29a37081312a3f00845fd4ba3f3489" "9370aeac615012366188359cb05011aea721c73e1cb194798bc18576025cabeb" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "f3ceb7a30f6501c1093bc8ffdf755fe5ddff3a85437deebf3ee8d7bed8991711" "5f7580488c9eb32997d0cd29ab1720c944cdf76d543a860588c733cead175e15" "8eef22cd6c122530722104b7c82bc8cdbb690a4ccdd95c5ceec4f3efa5d654f5" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "f3d2144fed1adb27794a45e61166e98820ab0bbf3cc7ea708e4bf4b57447ee27" "a2c537c981b4419aa3decac8e565868217fc2995b74e1685c5ff8c6d77b198d6" "31bfef452bee11d19df790b82dea35a3b275142032e06c6ecdc98007bf12466c" default)))
+ '(custom-safe-themes (quote ("f479660a3c4e3134a840113aeb092e50382779220a0d23017ac66481682fcdcb" "335b556e74ffe1230ba04e7eda8def0288f324b5c1b30fc7bd8c77477cd13611" "a53714de04cd4fdb92ed711ae479f6a1d7d5f093880bfd161467c3f589725453" "16248150e4336572ff4aa21321015d37c3744a9eb243fbd1e934b594ff9cf394" "75b3e2b7fcf51439686d51dba028d933adac88b69fd725f03bfc5975f4efd145" "11d069fbfb0510e2b32a5787e26b762898c7e480364cbc0779fe841662e4cf5d" "ab3a5935a219ca4d4c6aea5defc9f4b4199e248d45bf93b9e72e43f1242e44e3" "0f0e3af1ec61d04ff92f238b165dbc6d2a7b4ade7ed9812b4ce6b075e08f49fe" "31928331f4eb5d343e70c9b56afabdbb5514e8b906fa67db1ccc6d751a05a5d4" "a3d519ee30c0aa4b45a277ae41c4fa1ae80e52f04098a2654979b1ab859ab0bf" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "76b226dd750d085eaaf7efa5eb07a3282223d74f327a0f4319512c0a59f6df39" "bbd99b10462e3d0f4ac6da09e23e5de1fed4f7fe87d679a6468a14b0b6c8e634" "b1a6b113840c7f739ccf2a330f28dd8f9f29a37081312a3f00845fd4ba3f3489" "9370aeac615012366188359cb05011aea721c73e1cb194798bc18576025cabeb" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "f3ceb7a30f6501c1093bc8ffdf755fe5ddff3a85437deebf3ee8d7bed8991711" "5f7580488c9eb32997d0cd29ab1720c944cdf76d543a860588c733cead175e15" "8eef22cd6c122530722104b7c82bc8cdbb690a4ccdd95c5ceec4f3efa5d654f5" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "f3d2144fed1adb27794a45e61166e98820ab0bbf3cc7ea708e4bf4b57447ee27" "a2c537c981b4419aa3decac8e565868217fc2995b74e1685c5ff8c6d77b198d6" "31bfef452bee11d19df790b82dea35a3b275142032e06c6ecdc98007bf12466c" default)))
  '(default-input-method "rfc1345")
  '(delete-selection-mode t nil nil "typing anything after highlighting text overwrites that text; source: http://emacsredux.com/blog/2013/04/12/delete-selection-on-insert/")
  '(doc-view-continuous t)
@@ -32,6 +33,7 @@
  '(next-line-add-newlines nil nil nil "Do not auto-add newlines at the end of the file on pressing `C-n` or down arrow")
  '(projectile-global-mode t)
  '(require-final-newline nil nil nil "Do not auto-add a final newline (if one is not present) when saving/visiting a file")
+ '(safe-local-variable-values (quote ((py-indent-offset . 4) (header-auto-update-enabled))))
  '(save-place t nil (saveplace) "save the last cursor location for each file")
  '(send-mail-function nil)
  '(show-paren-mode t nil (paren) "allow one to see matching pairs of parentheses; when point is on one of the paired characters, the other is highlighted")
@@ -48,4 +50,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fringe ((t (:background "#3F3F3F"))))
  '(linum ((t (:foreground "dim gray" :slant italic))) t))
