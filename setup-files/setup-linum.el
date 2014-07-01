2c1084698d444eeb0678ad2a11f6c34d3924b0b7
Kaushal Modi <kaushal.modi@gmail.com>
2014-07-01 14:20:10 -0400

    Move major mode hook based nlinum activation to each major mode's setup file. Not doing so gave "Invalid face: linum" error ONLY when launching emacs in daemon mode.

--- setup-files/setup-linum.el
+++ setup-files/setup-linum.el
@@ -1,23 +1,13 @@
-;; Time-stamp: <2014-05-04 22:25:59 kmodi>
+;; Time-stamp: <2014-07-01 12:20:25 kmodi>
 
 ;; NLinum
 
 (require 'nlinum)
-;; Enable the line number column on the left
-(global-nlinum-mode -1)
-(setq nlinum-format "%4d ") ; right aligned, 4 char wide line num col
 
-;; disable linum for selected modes
-;; (specified in `linum-disabled`modes-list` in linum-off.el)
-;; (require 'linum-off)
+(setq global-nlinum-mode -1
+      nlinum-format      "%4d ") ; right aligned, 4 char wide line num col
 
-;; Turn on nlinum only in the specified modes
-(add-hook 'verilog-mode-hook    'nlinum-mode)
-(add-hook 'sh-mode-hook         'nlinum-mode)
-(add-hook 'emacs-lisp-mode-hook 'nlinum-mode)
-(add-hook 'cperl-mode-hook      'nlinum-mode)
-(add-hook 'matlab-mode-hook     'nlinum-mode)
-(add-hook 'python-mode-hook     'nlinum-mode)
+;; NOTE: nlinum mode is turned on in the setup files of the desired major modes
 
 
 (setq setup-linum-loaded t)
