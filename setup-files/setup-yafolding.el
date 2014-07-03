5497d9f429515636ec4ba4843c972ea194e98c62
Kaushal Modi <kaushal.modi@gmail.com>
2014-07-03 12:04:27 -0400

    Fix typos

--- setup-files/setup-yafolding.el
+++ setup-files/setup-yafolding.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-07-03 11:09:14 kmodi>
+;; Time-stamp: <2014-07-03 11:46:12 kmodi>
 
 ;; Yet Another Folding, Folding code blocks based on indentation.
 ;; Source: https://github.com/zenozeng/yafolding.el
@@ -11,8 +11,8 @@
 (setq yafolding-ellipsis-content ">>>>>")
 
 
-(setq setup-xkcd-loaded t)
-(provide 'setup-xkcd)
+(setq setup-yafolding-loaded t)
+(provide 'setup-yafolding)
 
 ;; C-Enter <- Fold the current block; cursor has to be on the top-most line of the block
 ;; C-S-Enter <- Toggle folding all blocks
