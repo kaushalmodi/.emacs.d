66ccf1d397dc07cdd5d85a16f1e2237f38c2d109
Kaushal Modi <kaushal.modi@gmail.com>
2014-07-03 12:05:03 -0400

    Don't stripify dired; doesn't work well with zenburn theme

--- setup-files/setup-stripe-buffer.el
+++ setup-files/setup-stripe-buffer.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2013-12-02 17:06:00 kmodi>
+;; Time-stamp: <2014-07-03 11:26:09 kmodi>
 
 ;; Stripe Mode
 ;; Source: https://github.com/sabof/stripe-buffer/blob/master/README.md
@@ -6,7 +6,6 @@
 (require 'cl-lib)
 (require 'stripe-buffer)
 
-(add-hook 'dired-mode-hook 'stripe-listify-buffer) ;; stripify dired
 (add-hook 'org-mode-hook 'turn-on-stripe-table-mode) ;; stripify tables in org-mode
 
 (provide 'setup-stripe-buffer)
