710cefbedca97eb18d6f01b0eecc2045ee725daa
Kaushal Modi <kaushal.modi@gmail.com>
2014-07-24 22:09:08 -0400

    Updating yafolding face

--- custom.el
+++ custom.el
@@ -35,9 +35,11 @@
  '(keyboard-coding-system (quote utf-8-unix) nil nil "default EOL system = that of Unix")
  '(magit-auto-revert-mode nil)
  '(major-mode (quote text-mode) nil nil "If the default value of major-mode is nil, the major mode is taken from the previously current buffer")
+ '(menu-bar-mode nil)
  '(next-line-add-newlines nil nil nil "Do not auto-add newlines at the end of the file on pressing `C-n` or down arrow")
+ '(org-confirm-elisp-link-not-regexp "\\(.*switch\\-to\\-buffer.*\\|org-show\\)")
  '(require-final-newline nil nil nil "Do not auto-add a final newline (if one is not present) when saving/visiting a file")
- '(safe-local-variable-values (quote ((lisp-backquote-indentation . t) (eval when (require (quote rainbow-mode) nil t) (rainbow-mode 1)) (py-indent-offset . 4) (header-auto-update-enabled))))
+ '(safe-local-variable-values (quote ((org-export-allow-bind-keywords . t) (org-confirm-babel-evaluate) (lisp-backquote-indentation . t) (eval when (require (quote rainbow-mode) nil t) (rainbow-mode 1)) (py-indent-offset . 4) (header-auto-update-enabled))))
  '(save-place t nil (saveplace) "save the last cursor location for each file")
  '(show-paren-mode t nil (paren) "allow one to see matching pairs of parentheses; when point is on one of the paired characters, the other is highlighted")
  '(sml/theme (quote automatic))
@@ -45,6 +47,7 @@
  '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
  '(vc-annotate-very-old-color "#DC8CC3")
  '(visible-bell t nil nil "enable the visible bell or screen blink to happen when there's any error")
+ '(visual-line-fringe-indicators (quote (nil nil)))
  '(w3m-mode-hook (quote (bookmark-w3m-prepare (lambda nil (set (make-local-variable (quote bookmark-make-record-function)) (quote bmkp-make-w3m-record))))) t)
  '(x-select-enable-clipboard t nil nil "after copy Ctrl+c in X11 apps, you can paste by 'yank' in emacs")
  '(x-select-enable-primary t nil nil "after mouse selection in X11, you can paste by 'yank' in emacs"))
@@ -54,5 +57,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fringe ((t (:background "#3F3F3F"))))
  '(stripe-hl-line ((t (:inherit nil :overline "gray" :underline "gray" :weight bold :height 1.1))))
- '(yafolding-ellipsis-face ((t (:foreground "orange" :slant italic :height 0.9))) t))
+ '(yafolding-ellipsis-face ((t (:foreground "deep sky blue" :slant italic :weight bold :height 1.1))) t))
