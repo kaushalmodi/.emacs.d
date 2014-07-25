58c48818abe06e1c9b4f540553900e757f8f7576
Kaushal Modi <kaushal.modi@gmail.com>
2014-07-24 22:10:01 -0400

    Customized expand region function so that the cursor ends up begin on the right hand side of the selection after exiting expand region mode

--- setup-files/setup-expand-region.el
+++ setup-files/setup-expand-region.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-06-20 09:29:08 kmodi>
+;; Time-stamp: <2014-07-15 14:02:30 kmodi>
 
 ;; Popwin
 ;; Source: https://github.com/magnars/expand-region.el
@@ -8,6 +8,13 @@
 (setq expand-region-contract-fast-key "|"
       expand-region-reset-fast-key    "<ESC>"
       )
+
+(defun my-expand-region ()
+  "Be default `er/expand-region' takes the cursor to the beginning of the
+selected text. I'd like the cursor to end up at the end of that text."
+  (interactive)
+  (er/expand-region 1)
+  (exchange-point-and-mark))
 
 ;; Key bindings for expand-region are saved in setup-key-bindings.el
 ;; Search for `setup-expand-region-loaded' there.
