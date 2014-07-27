f8fa2eae55a36f01665142622ec8f1838f5ae06c
Kaushal Modi <kaushal.modi@gmail.com>
2014-07-27 13:06:55 -0400

    Fix: Avoid redefining a function already defined in hungry-mode package

--- setup-files/setup-hungry-delete.el
+++ setup-files/setup-hungry-delete.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-07-27 00:14:47 kmodi>
+;; Time-stamp: <2014-07-27 13:06:13 kmodi>
 
 ;; Hungry Delete
 
@@ -6,9 +6,7 @@
 
 ;; (global-hungry-delete-mode)
 
-(defun turn-on-hungry-delete-mode()
-  (interactive)
-  (hungry-delete-mode 1))
+;; `turn-on-hungry-delete-mode' is defined in the hungry-mode package
 
 (defun turn-off-hungry-delete-mode()
   (interactive)
