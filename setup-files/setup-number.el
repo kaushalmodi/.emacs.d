e518148e11f3adc02e5324c3fd3f5a76ab2d76a6
Kaushal Modi <kaushal.modi@gmail.com>
2014-07-28 13:15:45 -0400

    Added placeholder file for number package setup

---
+++ setup-files/setup-number.el
@@ -0,0 +1,12 @@
+;; Time-stamp: <2014-07-28 13:11:37 kmodi>
+
+;; Number
+;; https://github.com/chrisdone/chrisdone-emacs/blob/master/packages/number/number.el
+
+(require 'number)
+
+;;
+
+
+(setq setup-number-loaded t)
+(provide 'setup-number)
