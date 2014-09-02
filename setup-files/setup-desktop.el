0427691dd2553653c911b776d9728b0a58113aea
Kaushal Modi <kaushal.modi@gmail.com>
2014-09-02 00:39:10 -0400

    Code organization; use of bind-keys

--- setup-files/setup-desktop.el
+++ setup-files/setup-desktop.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-02-18 12:32:05 kmodi>
+;; Time-stamp: <2014-08-19 09:46:48 kmodi>
 
 ;; Desktop save and restore
 
@@ -6,6 +6,53 @@
 ;; or restore a desktop. Restored desktops are deleted from disk.
 
 (desktop-save-mode 1)
+
+;; Source: https://github.com/purcell/emacs.d/blob/master/lisp/init-sessions.el
+; save a bunch of variables to the desktop file
+;; for lists specify the len of the maximal saved data also
+(setq desktop-globals-to-save
+      (append '((comint-input-ring . 50)
+                (compile-history . 30)
+                desktop-missing-file-warning
+                (dired-regexp-history . 20)
+                (extended-command-history . 30)
+                (face-name-history . 20)
+                (file-name-history . 100)
+                (grep-find-history . 30)
+                (grep-history . 30)
+                (ido-buffer-history . 100)
+                (ido-last-directory-list . 100)
+                (ido-work-directory-list . 100)
+                (ido-work-file-list . 100)
+                (magit-read-rev-history . 50)
+                (minibuffer-history . 50)
+                (org-clock-history . 50)
+                (org-refile-history . 50)
+                (org-tags-history . 50)
+                (query-replace-history . 60)
+                (read-expression-history . 60)
+                (regexp-history . 60)
+                (regexp-search-ring . 20)
+                register-alist
+                (search-ring . 20)
+                (shell-command-history . 50)
+                tags-file-name
+                tags-table-list)))
+
+(defun save-desktop-save-buffers-kill-emacs ()
+  "Save buffers and current desktop every time when quitting emacs."
+  (interactive)
+  (desktop-save-in-desktop-dir)
+  (save-buffers-kill-emacs))
+
+(bind-keys
+ :map modi-mode-map
+ ("<S-f2>" . desktop-save-in-desktop-dir))
+
+
+(provide 'setup-desktop)
+
+;; (desktop-save-mode nil)
 ;; comment (desktop-save-mode 1) is you want to use only one desktop
 ;; using the below code
 
@@ -53,39 +100,3 @@
 ;; 	     (if (saved-session)
 ;; 		 (if (y-or-n-p "Restore desktop? ")
 ;; 		     (session-restore)))))
-
-;; Source: https://github.com/purcell/emacs.d/blob/master/lisp/init-sessions.el
-; save a bunch of variables to the desktop file
-;; for lists specify the len of the maximal saved data also
-(setq desktop-globals-to-save
-      (append '((comint-input-ring . 50)
-                (compile-history . 30)
-                desktop-missing-file-warning
-                (dired-regexp-history . 20)
-                (extended-command-history . 30)
-                (face-name-history . 20)
-                (file-name-history . 100)
-                (grep-find-history . 30)
-                (grep-history . 30)
-                (ido-buffer-history . 100)
-                (ido-last-directory-list . 100)
-                (ido-work-directory-list . 100)
-                (ido-work-file-list . 100)
-                (magit-read-rev-history . 50)
-                (minibuffer-history . 50)
-                (org-clock-history . 50)
-                (org-refile-history . 50)
-                (org-tags-history . 50)
-                (query-replace-history . 60)
-                (read-expression-history . 60)
-                (regexp-history . 60)
-                (regexp-search-ring . 20)
-                register-alist
-                (search-ring . 20)
-                (shell-command-history . 50)
-                tags-file-name
-                tags-table-list)))
-
-
-(setq setup-desktop-loaded t)
-(provide 'setup-desktop)
