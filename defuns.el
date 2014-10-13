dfa64de2f6f0b0d445b19cce2ce807b3ffdd2921
Kaushal Modi <kaushal.modi@gmail.com>
2014-10-13 11:20:30 -0400

    Comment out update-custom-theme-load-path; not required any more

--- defuns.el
+++ defuns.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-10-13 09:53:46 kmodi>
+;; Time-stamp: <2014-10-13 11:03:42 kmodi>
 
 ;; Collection of general purposes defuns and macros
 
@@ -12,23 +12,25 @@
 (key-chord-define-global "^^" (λ (insert "λ")))
 
 
-(defun update-custom-theme-load-path ()
-  "Ensure that the custom-theme-load-path has all the theme paths added.
-Source: http://stackoverflow.com/a/15381087/1219634"
-  (interactive)
-  (require 'dash)
-  (require 's)
-  (-each
-      (-map
-       (lambda (item)
-         (format (concat elpa-dir "/%s") item))
-       (-filter
-        (lambda (item)
-          (or (s-contains? "theme" item)
-              (s-contains? "smart-mode-line" item)))
-        (directory-files elpa-dir)))
-    (lambda (item)
-      (add-to-list 'custom-theme-load-path item))))
+;; Below is not required any more as per
+;; http://emacs.stackexchange.com/questions/2112/why-does-load-theme-reset-the-custom-theme-load-path
+;; (defun update-custom-theme-load-path ()
+;;   "Ensure that the custom-theme-load-path has all the theme paths added.
+;; Source: http://stackoverflow.com/a/15381087/1219634"
+;;   (interactive)
+;;   (require 'dash)
+;;   (require 's)
+;;   (-each
+;;       (-map
+;;        (lambda (item)
+;;          (format (concat elpa-dir "/%s") item))
+;;        (-filter
+;;         (lambda (item)
+;;           (or (s-contains? "theme" item)
+;;               (s-contains? "smart-mode-line" item)))
+;;         (directory-files elpa-dir)))
+;;     (lambda (item)
+;;       (add-to-list 'custom-theme-load-path item))))
 
 
-  (provide 'defuns)
+(provide 'defuns)
