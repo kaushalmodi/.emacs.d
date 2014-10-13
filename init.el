28c8db1e787635534065019236b51d712a6df9ad
Kaushal Modi <kaushal.modi@gmail.com>
2014-10-13 10:32:13 -0400

    Added temp-mode to allow buffer-local key bindings; updated indentation

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-09-02 18:20:52 kmodi>
+;; Time-stamp: <2014-10-13 10:09:59 kmodi>
 ;; Author: Kaushal Modi
 
 ;; Record the start time
@@ -13,77 +13,78 @@
 
 ;; A list of packages to ensure are installed at launch
 (setq my-packages
-  '(
-    ;; projectile
-    ;; header2
-    ;; helm helm-swoop ;; Replaced with swoop
-    ;; etags-select etags-table ctags-update ;; Replacing these with ggtags
-    ;; zenburn-theme ;; Using my own forked version
-    ace-jump-mode ace-window
-    ag wgrep wgrep-ag s ;; ag > ack > grep, wgrep+wgrep-ag allow editing files directly in ag buffer
-                      ;; You need to have ag installed on your machine
-    anzu ;; shows total search hits in mode line, better query-replace alternative
-    auctex ;; You also need to install auctex from http://www.gnu.org/software/auctex/
-    auto-complete fuzzy
-    auto-highlight-symbol
-    benchmark-init
-    bookmark+ ;; able to bookmark desktop sessions
-    buffer-move
-    cperl-mode
-    csv-nav ;; editing csv files
-    dired+ dired-single
-    drag-stuff
-    eimp ;; required by org-show
-    elisp-slime-nav ;; tag based code navigation for elisp; works even for compressed code
-    expand-region
-    fill-column-indicator
-    fiplr ;; quick file search in a project (marked by folders like .git)
-    ggtags
-    guide-key
-    hardcore-mode
-    hl-line+
-    hungry-delete
-    ido-vertical-mode flx-ido ido-ubiquitous
-    iregister ;; Interactive access to registers
-    iy-go-to-char ;; Go to next char which is similar to "f" and "t" in vim
-    key-chord ;; map pairs of simultaneously pressed keys to commands
-    leuven-theme ;; awesome white background theme
-    magit ;; for git management
-    manage-minor-mode
-    markdown-mode
-    multiple-cursors
-    mwe-log-commands ;; for logging commands; useful when demoing emacs
-    nlinum ;; reviews say it's better than linum
-    number ;; number manipulation
-    org ox-reveal htmlize poporg ;; Get the latest org-mode package from MELPA
-    ;; ox-reveal for HTML slides
-    ;; poporg to edit comments from any other mode in org mode
-    page-break-lines ;; Convert the ^L (form feed) chars to horizontal lines
-    popwin ;; Open windows like *Help*, *Completions*, etc in minibuffer
-    rainbow-delimiters
-    rainbow-mode
-    region-bindings-mode ;; complements really well with multiple-cursors
-    req-package ;; optimize package loading
-    smart-compile
-    smart-mode-line popup rich-minority
-    smex ;; smart M-x
-    stripe-buffer
-    swoop
-    undo-tree ;; supercool undo visualization
-    visual-regexp
-    volatile-highlights
-    w3m ;; web-browsing in emacs
-    web-mode
-    ;; workgroups2 ;; testing
-    wrap-region ;; wrap selection with punctuations, tags (org-mode, markdown-mode, ..)
-    xkcd ;; comic
-    yafolding ;; indentation detected code folding
-    yaml-mode ;; Useful for editing Octopress' _config.yml
-    yasnippet
-    ))
+      '(
+        ;; projectile
+        ;; header2
+        ;; helm helm-swoop ;; Replaced with swoop
+        ;; etags-select etags-table ctags-update ;; Replacing these with ggtags
+        ;; zenburn-theme ;; Using my own forked version
+        ;; workgroups2 ;; tested but unstable at the time of testing
+        ace-jump-mode
+        ace-window
+        ag wgrep wgrep-ag s ;; ag > ack > grep, wgrep+wgrep-ag allow editing files directly in ag buffer
+        ;; You need to have ag installed on your machine
+        anzu ;; shows total search hits in mode line, better query-replace alternative
+        auctex ;; You also need to install auctex from http://www.gnu.org/software/auctex/
+        auto-complete fuzzy
+        auto-highlight-symbol
+        benchmark-init
+        bookmark+ ;; able to bookmark desktop sessions
+        buffer-move
+        cperl-mode
+        csv-nav ;; editing csv files
+        dired+ dired-single
+        drag-stuff
+        eimp ;; required by org-show
+        elisp-slime-nav ;; tag based code navigation for elisp; works even for compressed code
+        expand-region
+        fill-column-indicator
+        fiplr ;; quick file search in a project (marked by folders like .git)
+        ggtags
+        guide-key
+        hardcore-mode
+        hl-line+
+        hungry-delete
+        ido-vertical-mode flx-ido ido-ubiquitous
+        iregister ;; Interactive access to registers
+        iy-go-to-char ;; Go to next char which is similar to "f" and "t" in vim
+        key-chord ;; map pairs of simultaneously pressed keys to commands
+        leuven-theme ;; awesome white background theme
+        magit ;; for git management
+        manage-minor-mode
+        markdown-mode
+        multiple-cursors
+        mwe-log-commands ;; for logging commands; useful when demoing emacs
+        nlinum ;; reviews say it's better than linum
+        number ;; number manipulation
+        org ox-reveal htmlize poporg ;; Get the latest org-mode package from MELPA
+        ;; ox-reveal for HTML slides
+        ;; poporg to edit comments from any other mode in org mode
+        page-break-lines ;; Convert the ^L (form feed) chars to horizontal lines
+        popwin ;; Open windows like *Help*, *Completions*, etc in minibuffer
+        rainbow-delimiters
+        rainbow-mode
+        region-bindings-mode ;; complements really well with multiple-cursors
+        req-package ;; optimize package loading
+        smart-compile
+        smart-mode-line popup rich-minority
+        smex ;; smart M-x
+        stripe-buffer
+        swoop
+        undo-tree ;; supercool undo visualization
+        visual-regexp
+        volatile-highlights
+        w3m ;; web-browsing in emacs
+        web-mode
+        wrap-region ;; wrap selection with punctuations, tags (org-mode, markdown-mode, ..)
+        xkcd ;; comic
+        yafolding ;; indentation detected code folding
+        yaml-mode ;; Useful for editing Octopress' _config.yml
+        yasnippet
+        ))
 
-(load setup-packages-file) ;; Load the packages
 (load custom-file) ;; Load the emacs `M-x customize` generated file
+(load setup-packages-file) ;; Load the packages
 
 (require 'benchmark-init)
 (require 'req-package)
@@ -91,6 +92,7 @@
 (req-package setup-region-bindings-mode)
 (req-package setup-key-chord)
 (req-package modi-mode)
+(req-package temp-mode)
 (req-package defuns)
 
 (require 'setup-secret nil t) ;; No error if not found
@@ -136,13 +138,12 @@
 (req-package setup-smex)
 (req-package setup-stripe-buffer)
 (req-package setup-undo-tree)
-(req-package setup-visual-regexp)
 (req-package setup-w3m)
-;; (req-package setup-workgroups2)
 (req-package setup-wrap-region)
 (req-package setup-xkcd)
 (req-package setup-yafolding)
 (req-package setup-yasnippet)
+;; (req-package setup-workgroups2)
 ;; (req-package setup-helm) ;; Not required; replaced with swoop
 ;; (req-package setup-projectile)
 
