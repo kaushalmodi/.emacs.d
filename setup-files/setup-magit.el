ab6e033becc07b471257dbab6ea153edd72d0acb
Kaushal Modi <kaushal.modi@gmail.com>
2014-10-15 11:23:17 -0400

    Make the COMMIT_EDITMSG buffer open in a new window and not reuse the *magit:..* window

--- setup-files/setup-magit.el
+++ setup-files/setup-magit.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-08-18 13:25:47 kmodi>
+;; Time-stamp: <2014-10-15 09:31:43 kmodi>
 
 ;; magit
 ;; Source: https://github.com/magit/magit
@@ -16,10 +16,20 @@
     (key-chord-define-global "-[" 'magit-status)) ;; alternative for F11
   :config
   (progn
-    (setq magit-completing-read-function 'magit-ido-completing-read
-          magit-auto-revert-mode nil
-          magit-repo-dirs '( "~/.emacs.d"))
-    (magit-auto-revert-mode -1))) ;; Disable magit auto revert
+    (setq magit-completing-read-function 'magit-ido-completing-read)
+    (setq magit-auto-revert-mode nil)
+    (setq magit-repo-dirs '( "~/.emacs.d"))
+    (setq magit-diff-options '("--ignore-space-change"))
+    (magit-auto-revert-mode -1) ;; Disable magit auto revert
+    ;; While in "*magit..*" buffer on doing Commit (`c c'), the
+    ;; ".. COMMIT_EDITMSG" buffer opens and reuses the "*magit .." window.
+    ;; This is not useful when you'd want to add details about what you are
+    ;; committing while reviewing the diff in "*magit .." window. So ensure
+    ;; that the ".. COMMIT_EDITMSG" buffer always pops up in a new window.
+    ;; `display-buffer-alist' = '(CONDITION . (FUNCTION . ALIST))
+    (add-to-list 'display-buffer-alist
+                 '(".*COMMIT_EDITMSG". ((display-buffer-pop-up-window) .
+                                        ((inhibit-same-window . t)))))))
 
 
 (provide 'setup-magit)
