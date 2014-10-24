33d4ec2b250114459906a004f0d6d24ff7b6072b
Kaushal Modi <kaushal.modi@gmail.com>
2014-10-24 15:38:06 -0400

    Added comments on how to use prefix bindings with ace-window

--- setup-files/setup-ace-window.el
+++ setup-files/setup-ace-window.el
@@ -1,6 +1,8 @@
-;; Time-stamp: <2014-08-13 10:24:34 kmodi>
+;; Time-stamp: <2014-10-24 15:12:58 kmodi>
 
 ;; Ace Window
+;; Source: https://github.com/abo-abo/ace-window
+
 (req-package ace-window
   :config
   (progn
@@ -12,3 +14,7 @@
 
 
 (provide 'setup-ace-window)
+
+;;         `ace-window-BINDING' -> `ace-select-window'
+;;     C-u `ace-window-BINDING' -> `ace-swap-window'
+;; C-u C-u `ace-window-BINDING' -> `ace-delete-window'
