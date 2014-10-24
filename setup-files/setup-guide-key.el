570b500cbe94144514121d113b5e27b118af45a9
Kaushal Modi <kaushal.modi@gmail.com>
2014-10-24 15:33:29 -0400

    Launch guide-key on projectile prefixes too

--- setup-files/setup-guide-key.el
+++ setup-files/setup-guide-key.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-10-09 14:23:25 kmodi>
+;; Time-stamp: <2014-10-21 09:38:32 kmodi>
 
 ;; Guide Key
 ;; https://github.com/kai2nenobu/guide-key
@@ -6,17 +6,20 @@
 (req-package guide-key
   :config
   (progn
-    (setq guide-key/guide-key-sequence '("C-x r"   ;; rectangle, registers
-                                         "C-x 4"   ;; commands that operate in other window (buffer)
-                                         "C-x 5"   ;; commands that operate in other frame (window)
-                                         "C-c p"   ;; projectile
-                                         "C-x m"   ;; my custom pseudo map
+    (setq guide-key/guide-key-sequence '("C-x r"   ; rectangle, registers
+                                         "C-x 4"   ; commands that operate in other window (buffer)
+                                         "C-x 5"   ; commands that operate in other frame (window)
+                                         "C-c p"   ; projectile
+                                         "C-x m"   ; my custom pseudo map
                                          "C-x"
                                          "C-c"
                                          "C-c C-c"
-                                         "C-c C-t" ;; verilog-mode insert blocks
-                                         "s-f"     ;; fiplr
-                                         "s-w")    ;; workgroups2
+                                         "C-c C-t" ; verilog-mode insert blocks
+                                         "s-f"     ; fiplr
+                                         "s-w"     ; workgroups2
+                                         "C-c p"   ; projectile
+                                         "C-c p 4" ; projectile
+                                         )
           guide-key/highlight-command-regexp "rectangle"
           ;; guide-key can highlight commands which match a specified regular expression.
           ;; Key bindings following "C-x r" are rectangle family and register family.
