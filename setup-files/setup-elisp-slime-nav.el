8742a6ad6ac3e50dce6240a884202c2920f509c6
Kaushal Modi <kaushal.modi@gmail.com>
2014-10-24 15:34:46 -0400

    Add a function to launch elisp-slime-nav from anywhere, even when the cursor is not on a variable/function name; added autocompletion in mini-buffer--still needs work

--- setup-files/setup-elisp-slime-nav.el
+++ setup-files/setup-elisp-slime-nav.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-08-13 10:38:11 kmodi>
+;; Time-stamp: <2014-10-23 11:58:16 kmodi>
 
 ;; Elisp Slime Nav
 ;; gtags/ctags like navigation into elisp source codes (even the compressed ones)
@@ -9,7 +9,89 @@
   (progn
     (dolist (hook '(emacs-lisp-mode-hook
                     ielm-mode-hook))
-      (add-hook hook 'elisp-slime-nav-mode))))
+      (add-hook hook 'elisp-slime-nav-mode))
+
+    ;;   (defun modi/elisp-slime-nav-find-elisp-interactive (sym-name)
+    ;;       "Find the elisp thing at point, be it a function, variable, library or face
+    ;; from minibuffer."
+    ;;       (interactive
+    ;;        (list (read-from-minibuffer "Type elisp function/var/library/face to search: ")))
+    ;;       (when sym-name
+    ;;         (let ((sym (intern sym-name)))
+    ;;           (message "Searching for %s..." (pp-to-string sym))
+    ;;           (cond
+    ;;            ((fboundp sym) (find-function sym))
+    ;;            ((boundp sym) (find-variable sym))
+    ;;            ((or (featurep sym) (locate-library sym-name))
+    ;;             (find-library sym-name))
+    ;;            ((facep sym)
+    ;;             (find-face-definition sym))
+    ;;            (:else
+    ;;             (progn
+    ;;               (error "Don't know how to find '%s'" sym)))))))
+
+    (defun modi/elisp-slime-nav-find-elisp-interactive (sym-name)
+      "Find the elisp thing at point, be it a function, variable, library or face
+    from minibuffer."
+      (interactive
+       (let ((v (cond
+                 ((variable-at-point) (variable-at-point))
+                 ((function-called-at-point) (function-called-at-point))
+                 (:else nil)))
+             val)
+         (setq val (completing-read
+                    ;; PROMPT
+                    "Type elisp function/var/library/face to search: "
+                    ;; COLLECTION
+                    obarray
+                    ;; PREDICATE
+                    ;; (lambda (vv)
+                    ;;   (or
+                    ;;    ;; Filter valid variable names
+                    ;;    ;; (get vv 'variable-documentation)
+                    ;;    ;; (and (boundp vv) (not (keywordp vv)))
+                    ;;    ;; Filter valid function names
+                    ;;    (boundp vv)
+                    ;;    (fboundp vv)
+                    ;;    ))
+                    nil
+                    ;; (lambda (vv)
+                    ;; (or
+                    ;; (fboundp vv)
+                    ;; (boundp vv)))
+                    ;; REQUIRE-MATCH
+                    nil
+                    ;; INITIAL-INPUT
+                    nil
+                    ;; HIST
+                    nil
+                    ;; DEF
+                    nil
+                    ;; (if (symbolp v) (symbol-name v))
+                    ;; INHERIT-INPUT-METHOD
+                    nil
+                    ))
+         ;; (message "Here 1 %s" val)
+         (list (if (equal val "")
+                   v
+                 (intern val)))))
+      ;; (message "Here 2 %s" sym-name)
+      (when sym-name
+        ;; (let ((sym (intern sym-name)))
+        (let ((sym sym-name))
+          (message "Searching for %s..." (pp-to-string sym))
+          (cond
+           ((fboundp sym) (find-function sym))
+           ((boundp sym) (find-variable sym))
+           ((or (featurep sym) (locate-library sym-name))
+            (find-library sym-name))
+           ((facep sym)
+            (find-face-definition sym))
+           (:else
+            (progn
+              (error "Don't know how to find '%s'" sym)))))))
+
+    (bind-to-modi-map "?" modi/elisp-slime-nav-find-elisp-interactive)))
 
 
 (provide 'setup-elisp-slime-nav)
