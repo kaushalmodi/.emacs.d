a19634d917debc6ecd01e257f8c88b0a9373bd2a
Kaushal Modi <kaushal.modi@gmail.com>
2014-10-30 19:10:27 -0400

    Enable recursive-key-sequence-flag, add outshine prefix

--- setup-files/setup-guide-key.el
+++ setup-files/setup-guide-key.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-10-21 09:38:32 kmodi>
+;; Time-stamp: <2014-10-29 18:56:38 kmodi>
 
 ;; Guide Key
 ;; https://github.com/kai2nenobu/guide-key
@@ -6,19 +6,23 @@
 (req-package guide-key
   :config
   (progn
-    (setq guide-key/guide-key-sequence '("C-x r"   ; rectangle, registers
-                                         "C-x 4"   ; commands that operate in other window (buffer)
-                                         "C-x 5"   ; commands that operate in other frame (window)
-                                         "C-c p"   ; projectile
-                                         "C-x m"   ; my custom pseudo map
+    (setq guide-key/recursive-key-sequence-flag t)
+    (setq guide-key/guide-key-sequence '(
                                          "C-x"
+                                         ;; "C-x r"   ; rectangle, registers
+                                         ;; "C-x 4"   ; commands that operate in other window (buffer)
+                                         ;; "C-x 5"   ; commands that operate in other frame (window)
+                                         ;; "C-x m"   ; my custom pseudo map
                                          "C-c"
-                                         "C-c C-c"
-                                         "C-c C-t" ; verilog-mode insert blocks
+                                         ;; "C-c p"   ; projectile
+                                         ;; "C-c C-c"
+                                         ;; "C-c C-t" ; verilog-mode insert blocks
+                                         ;; "C-c p"   ; projectile
+                                         ;; "C-c p 4" ; projectile
+                                         "C-q"
                                          "s-f"     ; fiplr
                                          "s-w"     ; workgroups2
-                                         "C-c p"   ; projectile
-                                         "C-c p 4" ; projectile
+                                         "M-#"     ; outshine
                                          )
           guide-key/highlight-command-regexp "rectangle"
           ;; guide-key can highlight commands which match a specified regular expression.
