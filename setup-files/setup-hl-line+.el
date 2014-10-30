b9e9c4af619e8bbaecad41e92cf882f1b2b8a38f
Kaushal Modi <kaushal.modi@gmail.com>
2014-10-30 19:43:33 -0400

    Bind `C-c C-f' in global map instead of in modi-mode-map so that it can be overriden if needed

--- setup-files/setup-hl-line+.el
+++ setup-files/setup-hl-line+.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-08-12 17:41:44 kmodi>
+;; Time-stamp: <2014-10-28 16:43:02 kmodi>
 
 ;; hl-line+
 ;; Source: http://www.emacswiki.org/emacs/hl-line%2B.el
@@ -8,8 +8,10 @@
   (progn
     (toggle-hl-line-when-idle 1) ; Highlight line only when idle
     (setq hl-line-flash-show-period 2) ;; Number of seconds for `hl-line-flash' to highlight the line
+
+    ;; Below bindings are made in global map and not in my minor mode as I want
+    ;; other modes to override those bindings
     (bind-keys
-     :map modi-mode-map
      ("C-c C-f" . hl-line-flash))))
 
 
