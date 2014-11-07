95ac48e557503ccc49405acbcda4af76960e2e13
Kaushal Modi <kaushal.modi@gmail.com>
2014-11-07 13:52:46 -0500

    Add information on how to install auctex

--- setup-files/setup-latex.el
+++ setup-files/setup-latex.el
@@ -1,6 +1,18 @@
-;; Time-stamp: <2014-11-07 12:45:03 kmodi>
-
+;; Time-stamp: <2014-11-07 13:52:39 kmodi>
+;;
 ;; LaTeX
+;;
+;; NOTE: auctex has to be installed from outside emacs for the below `load's
+;; to work.
+;;
+;; 1. Download the latest auctex from http://www.gnu.org/software/auctex/download-for-unix.html
+;; 2. tar xvzf auctex-VERSION.tar.gz
+;; 3. ./configure --prefix=/home/kmodi/usr_local --with-lispdir=/home/kmodi/.emacs.d/auctex/ --with-texmf-dir=/home/kmodi/texlive/texmf-local/
+;;    - prefix <- Location of your /usr/local
+;;    - with-lispdir <- I prefer to keep auctex elisp stuff in my ~/.emacs.d
+;;    - with-texmf-dir <- Location of texlive texmf directory
+;; 4. make
+;; 5. make install
 
 (prepend-path (concat user-emacs-directory "/auctex"))
 
@@ -11,6 +23,7 @@
 
 ;; Source: http://www.sigmafield.org/2009/10/03/using-doc-view-with-auto-revert-to-view-latex-pdf-output-in-emacs
 (add-hook 'doc-view-mode-hook 'auto-revert-mode)
+
 ;; Source: http://www.gnu.org/software/auctex/manual/auctex/Multifile.html
 (setq TeX-PDF-mode t
       TeX-auto-save t
