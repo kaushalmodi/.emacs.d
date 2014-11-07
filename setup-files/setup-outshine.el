2e6858d6d05bd3f29979734815375a4fa4404632
Kaushal Modi <kaushal.modi@gmail.com>
2014-11-07 13:30:02 -0500

    Indent heading names in TOC instead of simply prepending them with flat incrementing numbers; Map M-p/M-n to jump to previous and next visible headings in outline-minor-mode-map

--- setup-files/setup-outshine.el
+++ setup-files/setup-outshine.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-10-29 13:25:50 kmodi>
+;; Time-stamp: <2014-11-07 10:10:41 kmodi>
 
 ;; Outshine
 ;; https://github.com/tj64/outshine
@@ -17,17 +17,24 @@
     (defun modi/outshine-table-of-contents ()
       "Create a table of content for outshine headers
 Insert/update the TOC after the line that has the \"// Contents:\" string.
-Don't add \"Revision\" heading to TOC.
+Don't add \"Revision Control\" heading to TOC.
 "
       (interactive)
       (save-excursion
         (goto-char (point-min))
-        (let* ((headings-found ()))
+        (let* ((headings-list nil)
+               (stars-list nil)
+               heading)
           (while (search-forward-regexp
-                  "^// \\*+ \\(.+\\)"
-                  nil :noerror)
-            (when (not (string= (match-string 1) "Revision Control"))
-              (setq headings-found (cons (match-string 1) headings-found))))
+                  "^// \\*\\(\\**\\) \\(.+\\)" nil :noerror)
+            (setq star    (match-string 1))
+            (setq heading (match-string 2))
+            (message "%s %s" star heading)
+            (when (not (string= heading "Revision Control"))
+              (setq stars-list    (cons star stars-list))
+              (setq headings-list (cons heading headings-list))))
+          (setq stars-list    (nreverse stars-list))
+          (setq headings-list (nreverse headings-list))
 
           (goto-char (point-min))
           (while (search-forward-regexp
@@ -42,15 +49,23 @@ Don't add \"Revision\" heading to TOC.
             ;; Then print table of contents
             (insert "//\n")
             (let ((n 1))
-              (dolist (heading (reverse headings-found))
-                (insert (format "// %2d. %s\n" n heading))
+              (dolist (h headings-list)
+                ;; (insert (format "// %2d. %s\n" n heading))
+                (insert (format "// %s%s\n" (replace-regexp-in-string "\\*" "  " (pop stars-list)) h))
                 (setq n (1+ n))))))))
 
     (add-hook 'outline-minor-mode-hook #'outshine-hook-function)
     (dolist (hook '(verilog-mode-hook
                     matlab-mode-hook))
       (add-hook hook #'outline-minor-mode)
-      (add-hook hook (λ (add-hook 'local-write-file-hooks #'modi/outshine-table-of-contents))))))
+      (add-hook hook (λ (add-hook 'local-write-file-hooks #'modi/outshine-table-of-contents))))
+
+    (bind-keys
+     :map outline-minor-mode-map
+     ("<M-up>"   . nil)
+     ("M-p"      . outline-previous-visible-heading)
+     ("<M-down>" . nil)
+     ("M-n"      . outline-next-visible-heading))))
 
 
 (provide 'setup-outshine)
