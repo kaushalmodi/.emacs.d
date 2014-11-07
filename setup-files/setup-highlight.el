8e85b9c82dd65fd69a2bf04dc42e65ea8ca43672
Kaushal Modi <kaushal.modi@gmail.com>
2014-11-07 13:16:26 -0500

    Improve comment formatting

--- setup-files/setup-highlight.el
+++ setup-files/setup-highlight.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-10-23 09:47:57 kmodi>
+;; Time-stamp: <2014-11-07 09:37:17 kmodi>
 
 ;; Highlight Symbol at point/cursor
 ;; (require 'highlight-symbol)
@@ -7,11 +7,12 @@
 
 ;; Highlight Global
 ;; Source: https://github.com/glen-dai/highlight-global
-;; Unlike highlight-symbol which only do highlighting in current buffer, this
-;; package highlights all matches accross ALL buffers. Multiple highlights are
-;; supported. Different highlight shows in different faces.
-;; There are 2 ways to select a highlight target: 1. Mark the selection by
-;; region (very useful when you want to highlight a pattern accross all symbols),
+;; Unlike highlight-symbol which does highlighting only in the current buffer,
+;; this package highlights all matches accross ALL buffers. Multiple highlights
+;; are supported. Different highlights show in different faces.
+;; There are 2 ways to select a highlight target:
+;; 1. Mark the selection by region (very useful when you want to
+;;    highlight a pattern across all symbols),
 ;; 2. Put cursor on a symbol to pick the symbol to highlight.
 (req-package highlight-global
   :config
