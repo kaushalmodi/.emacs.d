e59c0bd3b052e78bec4147fdfc6f00648a05fbc1
Kaushal Modi <kaushal.modi@gmail.com>
2014-12-08 09:15:15 -0500

    Remove my-expand-region

--- setup-files/setup-expand-region.el
+++ setup-files/setup-expand-region.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-08-13 10:38:39 kmodi>
+;; Time-stamp: <2014-12-08 09:14:58 kmodi>
 
 ;; Expand Region
 ;; Source: https://github.com/magnars/expand-region.el
@@ -9,19 +9,12 @@
     (setq expand-region-contract-fast-key "|"
           expand-region-reset-fast-key    "<ESC><ESC>")
 
-    (defun my-expand-region ()
-      "Be default `er/expand-region' takes the cursor to the beginning of the
-selected text. I'd like the cursor to end up at the end of that text."
-      (interactive)
-      (er/expand-region 1)
-      (exchange-point-and-mark))
-
     (bind-keys
      :map modi-mode-map
      ;; bind er/expand-region to `C-\' instead of `C-=' because `=' sign clashes
      ;; when trying to wrap a selection with `=' in org-mode using the wrap-region
      ;; package
-     ("C-\\" . my-expand-region))))
+     ("C-\\" . er/expand-region))))
 
 
 (provide 'setup-expand-region)
