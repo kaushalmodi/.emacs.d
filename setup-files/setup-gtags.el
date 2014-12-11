0ae2d44f123dc7980db3fd71291555946bb5a284
Kaushal Modi <kaushal.modi@gmail.com>
2014-12-11 14:13:28 -0500

    Set ggtags oversize limit to 30 MB

--- setup-files/setup-gtags.el
+++ setup-files/setup-gtags.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-11-19 10:19:17 kmodi>
+;; Time-stamp: <2014-12-11 14:03:10 kmodi>
 
 ;; gtags, GNU global
 
@@ -7,10 +7,13 @@
   :init
   (progn
     (setq ggtags-navigation-mode-lighter nil)
+    (setq ggtags-oversize-limit (* 30 1024 1024))
+
     (dolist (hook '(verilog-mode-hook
                     matlab-mode-hook
                     c-mode-hook))
       (add-hook hook 'ggtags-mode))
+
     (key-chord-define-global "??" 'ggtags-show-definition)))
 
 
