5fd6f8287db1527d942fe55c602deefd0f848796
Kaushal Modi <kaushal.modi@gmail.com>
2014-12-23 16:13:24 -0500

    Don't consider back-quotes (`) as part of tags; the tag parsed from `uvm_info should be just uvm_info

--- setup-files/setup-gtags.el
+++ setup-files/setup-gtags.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-12-11 14:03:10 kmodi>
+;; Time-stamp: <2014-12-23 15:32:46 kmodi>
 
 ;; gtags, GNU global
 
@@ -13,6 +13,18 @@
                     matlab-mode-hook
                     c-mode-hook))
       (add-hook hook 'ggtags-mode))
+
+    ;; Don't consider ` (back quote) as part of `tag' when looking for a
+    ;; Verilog macro definition
+    (defun ggtags-tag-at-point ()
+      (pcase (funcall ggtags-bounds-of-tag-function)
+        (`(,beg . ,end)
+         (if (eq ?` (string-to-char (buffer-substring beg end)))
+             ;; If `(buffer-substring beg end)' returns "`uvm_info" (for example),
+             ;; discard the ` and return just "uvm_info"
+             (buffer-substring (1+ beg) end)
+           ;; else return the whole `(buffer-substring beg end)'
+           (buffer-substring beg end)))))
 
     (key-chord-define-global "??" 'ggtags-show-definition)))
 
