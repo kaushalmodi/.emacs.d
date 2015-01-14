4d102873bd6af120c2fcf1896c822fa725268c49
Kaushal Modi <kaushal.modi@gmail.com>
2015-01-14 10:54:25 -0500

    Add bindings to quickly add and remove edebug instrumentation

--- setup-files/setup-elisp.el
+++ setup-files/setup-elisp.el
@@ -1,12 +1,16 @@
-;; Time-stamp: <2014-07-01 14:16:05 kmodi>
+;; Time-stamp: <2015-01-13 19:55:03 kmodi>
 
 ;; Emacs Lisp Mode
 
-(defun my-emacs-lisp-mode-customizations()
-  (when (boundp 'setup-linum-loaded)
-    (nlinum-mode 1))
-  )
-(add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-mode-customizations)
+(defun my/edebug-defun ()
+  (interactive)
+  (eval-defun :edebug))
+
+;; edebug
+(bind-keys
+ :map modi-mode-map
+ ("<f7>" . my/edebug-defun) ; add edebug instrumentation
+ ("<S-f7>" . eval-defun)) ; remove edebug instrumentation
 
 
 (provide 'setup-elisp)
