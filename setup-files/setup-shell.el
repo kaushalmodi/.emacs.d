7a614ef93be59eb69fa07845420f90323c4de20c
Kaushal Modi <kaushal.modi@gmail.com>
2015-01-15 18:27:15 -0500

    Add .gpms as one of the files to be viewed in tcsh mode

--- setup-files/setup-shell.el
+++ setup-files/setup-shell.el
@@ -1,10 +1,11 @@
-;; Time-stamp: <2015-01-14 10:57:34 kmodi>
+;; Time-stamp: <2015-01-15 13:33:25 kmodi>
 
 ;; Shell Script Mode
 
 (defun my/tcsh-set-indent-functions ()
   (when (or (string-match ".*\\.alias" (buffer-file-name))
             (string-match ".*\\.setup.*" (buffer-file-name))
+            (string-match ".*\\.gpms" (buffer-file-name))
             (string-match ".*\\.cfg" (buffer-file-name))
             (string-match ".*csh$" (file-name-extension (buffer-file-name))))
     (require 'csh-mode) ; https://github.com/Tux/tcsh/blob/master/csh-mode.el
