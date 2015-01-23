dcf39435575836e5e3d096abc888e19a88457c12
Kaushal Modi <kaushal.modi@gmail.com>
2015-01-23 09:06:16 -0500

    Bug fix: don't try doing buffer file name string match if the buffer is not a file <- this is the case when org does fontification of shell script code blocks using html export

--- setup-files/setup-shell.el
+++ setup-files/setup-shell.el
@@ -1,16 +1,17 @@
-;; Time-stamp: <2015-01-15 13:33:25 kmodi>
+;; Time-stamp: <2015-01-22 01:59:32 kmodi>
 
 ;; Shell Script Mode
 
 (defun my/tcsh-set-indent-functions ()
-  (when (or (string-match ".*\\.alias" (buffer-file-name))
-            (string-match ".*\\.setup.*" (buffer-file-name))
-            (string-match ".*\\.gpms" (buffer-file-name))
-            (string-match ".*\\.cfg" (buffer-file-name))
-            (string-match ".*csh$" (file-name-extension (buffer-file-name))))
-    (require 'csh-mode) ; https://github.com/Tux/tcsh/blob/master/csh-mode.el
-    (setq-local indent-line-function   'csh-indent-line)
-    (setq-local indent-region-function 'csh-indent-region)))
+  (when (buffer-file-name) ; do this only if the buffer is a file
+    (when (or (string-match ".*\\.alias" (buffer-file-name))
+              (string-match ".*\\.setup.*" (buffer-file-name))
+              (string-match ".*\\.gpms" (buffer-file-name))
+              (string-match ".*\\.cfg" (buffer-file-name))
+              (string-match ".*csh$" (file-name-extension (buffer-file-name))))
+      (require 'csh-mode) ; https://github.com/Tux/tcsh/blob/master/csh-mode.el
+      (setq-local indent-line-function   'csh-indent-line)
+      (setq-local indent-region-function 'csh-indent-region))))
 (add-hook 'sh-set-shell-hook #'my/tcsh-set-indent-functions)
 
 
