23511098930b445deabf3285b2fcce7024ea3a4e
Kaushal Modi <kaushal.modi@gmail.com>
2015-01-23 09:19:52 -0500

    Don't set the 2's complement mode as default; set engineering notation as default

--- setup-files/setup-calc-defaults.el
+++ setup-files/setup-calc-defaults.el
@@ -1,6 +1,24 @@
-;; Time-stamp: <2014-12-17 23:29:26 kmodi>
+;; Time-stamp: <2015-01-16 17:32:53 kmodi>
 
 ;; Calc settings file
 ;; https://www.gnu.org/software/emacs/manual/html_node/calc/Customizing-Calc.html
 
-(setq calc-twos-complement-mode t)
+;; (setq calc-twos-complement-mode t)
+(setq calc-twos-complement-mode nil)
+
+;; Calculator output value format
+(setq calc-float-format '(eng 4)) ; Engineering notation
+;; |------------+--------------------------------------------------------------+-------------|
+;; | value      | Description                                                  | Key Binding |
+;; |------------+--------------------------------------------------------------+-------------|
+;; | (float 0)  | Floating point format, display full precision.               | d n         |
+;; | (float N)  | N > 0: Floating point format, at most N significant figures. |             |
+;; | (float -N) | -N < 0: Floating point format, calc-internal-prec - N figs.  |             |
+;; | (fix N)    | N >= 0: Fixed point format, N places after decimal point.    | d f         |
+;; | (sci 0)    | Scientific notation, full precision.                         | d s         |
+;; | (sci N)    | N > 0: Scientific notation, N significant figures.           |             |
+;; | (sci -N)   | -N < 0: Scientific notation, calc-internal-prec - N figs.    |             |
+;; | (eng 0)    | Engineering notation, full precision.                        | d e         |
+;; | (eng N)    | N > 0: Engineering notation, N significant figures.          |             |
+;; | (eng -N)   | -N < 0: Engineering notation, calc-internal-prec - N figs."  |             |
+;; |------------+--------------------------------------------------------------+-------------|
