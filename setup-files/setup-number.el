c991ba76aa5411dbd757995f30bfdd630b9a6012
Kaushal Modi <kaushal.modi@gmail.com>
2015-01-27 12:08:39 -0500

    Bind the number/* functions with `C-c m' prefix

--- setup-files/setup-number.el
+++ setup-files/setup-number.el
@@ -1,9 +1,19 @@
-;; Time-stamp: <2015-01-21 14:19:14 kmodi>
+;; Time-stamp: <2015-01-23 14:29:31 kmodi>
 
 ;; Number
 ;; https://github.com/chrisdone/number
 
-(req-package number)
+(req-package number
+  :config
+  (progn
+    (hydra-create "C-c m"
+      '(("+" number/add      "Add")
+        ("-" number/sub      "Sub")
+        ("*" number/multiply "Mul")
+        ("/" number/divide   "Div")
+        ("0" number/pad      "Pad 0s")
+        ("=" number/eval     "Eval"))
+      modi-mode-map)))
 
 
 (provide 'setup-number)
