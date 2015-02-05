6df2234fd57b15d559a319be5d69ab76a5c89d5e
Kaushal Modi <kaushal.modi@gmail.com>
2015-02-05 14:54:03 -0500

    List the recommended set of bi-grams that can be used as key-chords

--- setup-files/setup-key-chord.el
+++ setup-files/setup-key-chord.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-08-13 00:24:43 kmodi>
+;; Time-stamp: <2015-02-04 10:45:34 kmodi>
 
 ;; Key Chord Mode
 ;; http://www.emacswiki.org/emacs/key-chord.el
@@ -9,3 +9,25 @@
 
 
 (provide 'setup-key-chord)
+
+;; John Cook's post http://www.johndcook.com/blog/2015/02/01/rare-bigrams/
+;; provides a list of rare bi-grams that would work great for key-chords
+;;      BF BG BQ BX BZ
+;;      CJ CV
+;;      DX DZ
+;;      FB FJ FQ FV FZ
+;;      GB GJ GP GQ GV GX GZ
+;;      HJ HQ HV
+;;  JJ  JC JF JG JH JK JL JM JP JQ JS JT JV JW JX JY JZ
+;;  KK  KJ KQ KV KX KZ
+;;      LJ LQ
+;;      MJ MQ MV MX MZ
+;;      PG PJ PQ PV PZ
+;;  QQ  QB QF QG QH QJ QK QL QM QP QT QV QW QX QY QZ
+;;      SJ SX SZ
+;;      TJ TQ
+;;  VV  VC VF VG VH VJ VK VM VP VQ VW VZ
+;;  WW  WJ WQ WV WX WZ
+;;      XB XD XG XJ XK XM XQ XS XW XZ
+;;  YY  YJ YQ
+;;      ZB ZD ZF ZG ZJ ZK ZM ZP ZQ ZS ZV ZW ZX
