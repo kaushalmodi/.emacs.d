2d141df12698aea5999a4b1db75d987e96b5fca5
Kaushal Modi <kaushal.modi@gmail.com>
2015-02-05 14:45:12 -0500

    Remove sunshine package from "from-git" dir; use the Melpa version directly; update the bindings to call sunshine-quick-weather

--- setup-files/setup-sunshine.el
+++ setup-files/setup-sunshine.el
@@ -1,16 +1,16 @@
-;; Time-stamp: <2015-01-27 12:10:50 kmodi>
+;; Time-stamp: <2015-02-05 14:44:48 kmodi>
 
 ;; Sunshine - weather forecast
 ;; Source: https://github.com/aaronbieber/sunshine.el
 
 (req-package sunshine
-  :load-path "from-git/sunshine.el"
   :config
   (progn
     (setq sunshine-location "27278,USA")
     (setq sunshine-show-icons t)
 
-    (bind-to-modi-map "w" sunshine-forecast)))
+    (bind-to-modi-map "w" sunshine-quick-forecast)
+    (bind-to-modi-map "W" sunshine-forecast)))
 
 
 (provide 'setup-sunshine)
