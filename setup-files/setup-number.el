7675afb3ff59a1b6f2dfa9d1420ea0ee2dbbd779
Kaushal Modi <kaushal.modi@gmail.com>
2015-02-09 14:43:00 -0500

    Use defhydra instead of hydra-create for hydra-math

--- setup-files/setup-number.el
+++ setup-files/setup-number.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-01-23 14:29:31 kmodi>
+;; Time-stamp: <2015-02-09 09:59:42 kmodi>
 
 ;; Number
 ;; https://github.com/chrisdone/number
@@ -6,14 +6,15 @@
 (req-package number
   :config
   (progn
-    (hydra-create "C-c m"
-      '(("+" number/add      "Add")
-        ("-" number/sub      "Sub")
-        ("*" number/multiply "Mul")
-        ("/" number/divide   "Div")
-        ("0" number/pad      "Pad 0s")
-        ("=" number/eval     "Eval"))
-      modi-mode-map)))
+    (defhydra hydra-math
+        (nil "C-c m" :bind (lambda (key cmd) (bind-key key cmd modi-mode-map)))
+      "math-operation"
+      ("+" number/add      "Add")
+      ("-" number/sub      "Sub")
+      ("*" number/multiply "Mul")
+      ("/" number/divide   "Div")
+      ("0" number/pad      "Pad 0s")
+      ("=" number/eval     "Eval"))))
 
 
 (provide 'setup-number)
