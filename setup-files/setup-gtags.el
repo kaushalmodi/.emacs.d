504e0cd8c42e1bb34ff7b94d7684fbd4bffafb13
Kaushal Modi <kaushal.modi@gmail.com>
2015-02-17 19:34:45 -0500

    Change: Improve proj name derivation
    Add `my/ggtags-project-name'; use that for generating
    `ggtags-mode-line-project-name'; enable ggtags-mode in more major modes

--- setup-files/setup-gtags.el
+++ setup-files/setup-gtags.el
@@ -1,18 +1,49 @@
-;; Time-stamp: <2014-12-23 15:32:46 kmodi>
+;; Time-stamp: <2015-02-17 16:13:20 kmodi>
 
 ;; gtags, GNU global
 
 (req-package ggtags
   :require (verilog-mode key-chord)
-  :init
+  :config
   (progn
-    (setq ggtags-navigation-mode-lighter nil)
+
+    (defun my/ggtags-project-name ()
+      "Return gtags project name."
+      (if (stringp ggtags-project-root)
+          (let ((project-root ggtags-project-root)
+                project-name)
+            (setq project-name (file-name-nondirectory
+                                (directory-file-name project-root)))
+            (while (string-match "\\(sos_\\|\\bsrc\\b\\)" project-name)
+              (setq project-root (replace-regexp-in-string
+                                  "\\(.*\\)/.+/*$" "\\1" project-root))
+              (setq project-name (file-name-nondirectory
+                                  (directory-file-name project-root))))
+            project-name)
+        nil))
+
+    ;; (setq ggtags-navigation-mode-lighter nil)
+    ;; (setq ggtags-mode-line-project-name nil)
+    (setq ggtags-mode-line-project-name
+          '("[" (:eval (let ((name (if (stringp (my/ggtags-project-name))
+                                       (my/ggtags-project-name)
+                                     "?")))
+                         (propertize
+                          name 'face compilation-info-face
+                          'help-echo (if (stringp ggtags-project-root)
+                                         (concat "mouse-1 to visit " ggtags-project-root)
+                                       "mouse-1 to set project")
+                          'mouse-face 'mode-line-highlight
+                          'keymap ggtags-mode-line-project-keymap)))
+            "]"))
     (setq ggtags-oversize-limit (* 30 1024 1024))
 
     (dolist (hook '(verilog-mode-hook
                     matlab-mode-hook
+                    sh-mode-hook
+                    cperl-mode-hook
                     c-mode-hook))
-      (add-hook hook 'ggtags-mode))
+      (add-hook hook #'ggtags-mode))
 
     ;; Don't consider ` (back quote) as part of `tag' when looking for a
     ;; Verilog macro definition
