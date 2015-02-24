4e21c8f3e9fe0d538b3825b6b2ad7c50fc08cc09
Kaushal Modi <kaushal.modi@gmail.com>
2015-02-24 14:21:09 -0500

    Perf: idle load auto-complete

--- setup-files/setup-auto-complete.el
+++ setup-files/setup-auto-complete.el
@@ -1,17 +1,18 @@
-;; Time-stamp: <2015-02-23 11:42:32 kmodi>
+;; Time-stamp: <2015-02-24 13:05:37 kmodi>
 
 ;; Auto complete
 
 (use-package auto-complete-config
-  :config
+  :idle
   (progn
-    (setq ac-stop-words (quote ("/" "//" "/*" "//*" "///" "////"))
-          ac-ignore-case t ;; ignore case
-          ac-use-fuzzy t ;; enable fuzzy auto complete
-          ac-trigger-key "TAB"
-          ;; Source: http://cx4a.org/software/auto-complete/manual.html#Select_candidates_with_C-n_C-p_only_when_completion_menu_is_displayed
-          ;; Use C-n/p instead of arrow keys to select ac options from the ac menu
-          ac-use-menu-map t)
+    (setq ac-stop-words (quote ("/" "//" "/*" "//*" "///" "////")))
+    (setq ac-ignore-case t) ; ignore case
+    (setq ac-use-fuzzy t) ; enable fuzzy auto complete
+    (setq ac-trigger-key "TAB")
+
+    ;; http://cx4a.org/software/auto-complete/manual.html#Select_candidates_with_C-n_C-p_only_when_completion_menu_is_displayed
+    ;; Use C-n/p instead of arrow keys to select ac options from the ac menu
+    (setq ac-use-menu-map t)
     (bind-keys
      :map ac-menu-map
      ("C-n" . ac-next)
