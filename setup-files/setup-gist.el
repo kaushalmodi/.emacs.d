30cdc7a4d8d9079c6261da085a770559468fc132
Kaushal Modi <kaushal.modi@gmail.com>
2015-02-24 08:38:29 -0500

    Add: setup-gist

---
+++ setup-files/setup-gist.el
@@ -0,0 +1,10 @@
+;; Time-stamp: <2015-02-24 00:03:55 kmodi>
+
+;; Gist
+;; https://github.com/defunkt/gist.el
+
+(use-package gist
+  :commands (gist-list gist-buffer gist-region))
+
+
+(provide 'setup-gist)
