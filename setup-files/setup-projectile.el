250b44648a3227944751affe20dab1760652b9fd
Kaushal Modi <kaushal.modi@gmail.com>
2015-02-24 19:45:46 -0500

    Perf: autoload projectile functions
    - Put the projectile commands to `:commands` keyword of `use-package`
    - Moved the projectile key bindings to `:init`
    - The latest version of ag (as of today) supports the `-0` argument. So
      it is not longer needed to do `tr '\n' '\0'` to generate null char
      separated output

--- setup-files/setup-projectile.el
+++ setup-files/setup-projectile.el
@@ -1,74 +1,30 @@
-;; Time-stamp: <2015-02-23 16:40:22 kmodi>
+;; Time-stamp: <2015-02-24 15:28:27 kmodi>
 
 ;; Projectile
 ;; Source: https://github.com/bbatsov/projectile
 
 (use-package projectile
+  :commands (projectile-ag
+             projectile-switch-to-buffer
+             projectile-invalidate-cache
+             projectile-find-dir
+             projectile-find-file
+             projectile-find-file-dwim
+             projectile-find-file-in-directory
+             projectile-ibuffer
+             projectile-kill-buffers
+             projectile-multi-occur
+             projectile-switch-project
+             projectile-recentf
+             projectile-remove-known-project
+             projectile-cleanup-known-projects
+             projectile-cache-current-file
+             projectile-find-file-other-window
+             projectile-find-file-dwim-other-window
+             projectile-find-dir-other-window
+             projectile-switch-to-buffer-other-window)
   :init
   (progn
-    (setq projectile-ag-command (concat "\\ag " ; used unaliased version of `ag': \ag
-                                        "-i " ; case insensitive
-                                        "-f " ; follow symbolic links
-                                        "--skip-vcs-ignores "
-                                        ; Ignore files/dirs ONLY from `.agignore',
-                                        ; NOT from vcs ignore files like .gitignore
-                                        "-g ''" ; get file names matching the regex '',
-                                        ; that is, ALL files not in ignore lists
-                                        " | tr '\\n' '\\0'"
-                                        ; Projectile needs null-character separated
-                                        ; string (\0); ag returns newline (\n)
-                                        ; separated list; So pipe the `ag' output
-                                        ; to `tr' and replace all \n with \0.
-                                        )))
-  :config
-  (progn
-
-    (defun projectile-get-ext-command ()
-      "Override the projectile-defined function so that `ag' is always used for
-getting a list of all files in a project."
-      projectile-ag-command)
-
-    ;; Make the file list creation faster by NOT calling `projectile-get-sub-projects-files'
-    (defun projectile-get-repo-files ()
-      (projectile-files-via-ext-command (projectile-get-ext-command)))
-
-    (add-to-list 'projectile-ignored-projects `,(concat (getenv "HOME") "/")) ; Don't consider my home dir as a project
-
-    (defun projectile-cache-files-find-file-hook ()
-      "Function for caching files with `find-file-hook'."
-      (when (and projectile-enable-caching
-                 (projectile-project-p)
-                 (not (member (projectile-project-p) projectile-ignored-projects)))
-        (projectile-cache-current-file)))
-
-    ;; (setq projectile-enable-caching nil)
-    (setq projectile-enable-caching t) ; Enable caching, otherwise
-                                        ; `projectile-find-file' is really slow
-                                        ; for large projects
-    (dolist (item '(".SOS" "nobackup"))
-      (add-to-list 'projectile-globally-ignored-directories item))
-    (dolist (item '("GTAGS" "GRTAGS" "GPATH"))
-      (add-to-list 'projectile-globally-ignored-files item))
-    ;; Customize the Projectile mode-line lighter
-    ;; (setq projectile-mode-line '(:eval (format " Projectile[%s]" (projectile-project-name))))
-    (setq projectile-mode-line " Prj")
-
-    (defun projectile-project-name ()
-      "Return project name."
-      (let ((project-root
-             (condition-case nil
-                 (projectile-project-root)
-               (error default-directory)))
-            project-name)
-        (setq project-name (file-name-nondirectory
-                            (directory-file-name project-root)))
-        (while (string-match "\\(sos_\\|\\bsrc\\b\\)" project-name)
-          (setq project-root (replace-regexp-in-string
-                              "\\(.*\\)/.+/*$" "\\1" project-root))
-          (setq project-name (file-name-nondirectory
-                              (directory-file-name project-root))))
-        project-name))
-
     (defhydra hydra-projectile-other-window (:color teal)
       "projectile-other-window"
       ("f"  projectile-find-file-other-window        "file")
@@ -114,8 +70,68 @@ _s-f_: file            _a_: ag              ^^_i_: Ibuffer           _c_: cache 
       ("`"   hydra-projectile-other-window/body "other window")
       ("q"   nil                                "cancel" :color blue))
 
-    (bind-key "s-f" #'hydra-projectile/body  modi-mode-map)
-    (bind-key "s-p" #'hydra-projectile/body  modi-mode-map)
+    (bind-key "s-f" #'hydra-projectile/body modi-mode-map)
+    (bind-key "s-p" #'hydra-projectile/body modi-mode-map))
+  :config
+  (progn
+    (defvar projectile-ag-command
+      (concat "\\ag" ; used unaliased version of `ag': \ag
+              " -i" ; case insensitive
+              " -f" ; follow symbolic links
+              " --skip-vcs-ignores" ; Ignore files/dirs ONLY from `.agignore',
+              " -0" ; output null separated results
+              " -g ''") ; get file names matching the regex ''
+      "Ag command to be used by projectile to generate file cache.")
+
+    (defun projectile-get-ext-command ()
+      "Override the projectile-defined function so that `ag' is always used for
+getting a list of all files in a project."
+      projectile-ag-command)
+
+    ;; Make the file list creation faster by NOT calling `projectile-get-sub-projects-files'
+    (defun projectile-get-repo-files ()
+      (projectile-files-via-ext-command (projectile-get-ext-command)))
+
+    ;; Don't consider my home dir as a project
+    (add-to-list 'projectile-ignored-projects `,(concat (getenv "HOME") "/"))
+
+    (defun projectile-cache-files-find-file-hook ()
+      "Function for caching files with `find-file-hook'."
+      (when (and projectile-enable-caching
+                 (projectile-project-p)
+                 (not (member (projectile-project-p) projectile-ignored-projects)))
+        (projectile-cache-current-file)))
+
+    ;; (setq projectile-enable-caching nil)
+    (setq projectile-enable-caching t) ; Enable caching, otherwise
+                                        ; `projectile-find-file' is really slow
+                                        ; for large projects
+    (dolist (item '(".SOS" "nobackup"))
+      (add-to-list 'projectile-globally-ignored-directories item))
+    (dolist (item '("GTAGS" "GRTAGS" "GPATH"))
+      (add-to-list 'projectile-globally-ignored-files item))
+    ;; Customize the Projectile mode-line lighter
+    ;; (setq projectile-mode-line '(:eval (format " Projectile[%s]" (projectile-project-name))))
+    (setq projectile-mode-line " Prj")
+
+    (defun projectile-project-name ()
+      "Return project name.
+If the project root contains \"sos_\" or \"src\", remove the directory basename
+from the project root name. E.g. if `projectile-project-root' is \"/a/b/src\",
+remove the \"src\" portion from it and make it \"/a/b\"."
+      (let ((project-root
+             (condition-case nil
+                 (projectile-project-root)
+               (error default-directory)))
+            project-name)
+        (setq project-name (file-name-nondirectory
+                            (directory-file-name project-root)))
+        (while (string-match "\\(sos_\\|\\bsrc\\b\\)" project-name)
+          (setq project-root (replace-regexp-in-string
+                              "\\(.*\\)/.+/*$" "\\1" project-root))
+          (setq project-name (file-name-nondirectory
+                              (directory-file-name project-root))))
+        project-name))
 
     (projectile-global-mode)))
 
