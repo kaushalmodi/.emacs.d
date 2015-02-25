ae00ae9c96eab3455c4a9c2225d13606085d094a
Kaushal Modi <kaushal.modi@gmail.com>
2015-02-25 17:18:13 -0500

    Add: `gk-markdown-preview-buffer` fn

--- setup-files/setup-markdown.el
+++ setup-files/setup-markdown.el
@@ -1,11 +1,34 @@
-;; Time-stamp: <2015-02-23 11:42:32 kmodi>
+;; Time-stamp: <2015-02-25 17:10:49 kmodi>
 
-;; Markdown / Pancake.io
+;; Markdown Mode
+;; http://jblevins.org/projects/markdown-mode/
 
 (use-package markdown-mode
-  :commands (markdown-mode)
-  :mode (("\\.md\\'"       . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode)))
+    :mode (("\\.md\\'"       . markdown-mode)
+           ("\\.markdown\\'" . markdown-mode))
+    :config
+    (progn
+      ;; http://daringfireball.net/projects/markdown/
+      ;; Download the Markdown source from above, extract the .pl from that
+      ;; and place it in one of the folders in the environment PATH
+      (setq markdown-command "Markdown.pl")
+
+      ;; https://github.com/cadadr/emacs.d
+      (defun gk-markdown-preview-buffer ()
+        (interactive)
+        (require 'shr)
+        (let* ((buf-this (buffer-name (current-buffer)))
+               (buf-html (get-buffer-create
+                          (format "*md-html (%s)*" buf-this))))
+          (markdown-other-window (buffer-name buf-html))
+          (shr-render-buffer buf-html)
+          (eww-mode)
+          (kill-buffer buf-html)))
+
+      (bind-keys
+       :map modi-mode-map
+       ;; Mimicking the org-export style bindings
+       ("C-c C-e o" . gk-markdown-preview-buffer))))
 
 
 (provide 'setup-markdown)
