ed0cbd5a3def7f0e7cc5e1467aef83e50b023857
Kaushal Modi <kaushal.modi@gmail.com>
2015-02-26 13:25:41 -0500

    Change: Modify `stripe-hl-line` face
    - The face is modified differently for light and dark backgrounds

--- setup-files/setup-stripe-buffer.el
+++ setup-files/setup-stripe-buffer.el
@@ -1,13 +1,27 @@
-;; Time-stamp: <2015-02-24 23:38:41 kmodi>
+;; Time-stamp: <2015-02-26 12:00:16 kmodi>
 
 ;; Stripe Mode
 ;; https://github.com/sabof/stripe-buffer
 
 (use-package stripe-buffer
-  :config
+    :config
   (progn
+    ;; How can I override a pre-defined face for light and dark backgrounds?
+    ;; http://emacs.stackexchange.com/q/9600/115
+    (defface my/stripe-hl-line
+      '((((background dark))  (:overline "gray" :underline "gray" :foreground "dodger blue"))
+        (t                    (:overline "gray" :underline "gray" :foreground "red")))
+      "Bold face for highlighting the current line in Hl-Line mode."
+      :group 'stripe-buffer)
+
+    (defun my/stripe-hl-line-face-remap ()
+      (face-remap-add-relative 'stripe-hl-line 'my/stripe-hl-line))
+
+    (advice-add 'stripe-listify-buffer :after #'my/stripe-hl-line-face-remap)
+
     (add-hook 'package-menu-mode-hook #'stripe-listify-buffer)
-    ;; (add-hook 'dired-mode-hook        #'stripe-listify-buffer)
+    ;; (add-hook 'dired-mode-hook #'stripe-listify-buffer)
+
     (add-hook 'org-mode-hook          #'turn-on-stripe-table-mode)))
 
 
