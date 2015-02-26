015f2c1c09c53c08a82e67cc20498d6cc35d0b7f
Kaushal Modi <kaushal.modi@gmail.com>
2015-02-26 13:25:17 -0500

    Perf: autoloads for sunshine commands

--- setup-files/setup-sunshine.el
+++ setup-files/setup-sunshine.el
@@ -1,16 +1,18 @@
-;; Time-stamp: <2015-02-23 11:42:33 kmodi>
+;; Time-stamp: <2015-02-26 12:36:39 kmodi>
 
 ;; Sunshine - weather forecast
 ;; Source: https://github.com/aaronbieber/sunshine.el
 
 (use-package sunshine
-  :config
-  (progn
-    (setq sunshine-location "27278,USA")
-    (setq sunshine-show-icons t)
-
-    (bind-to-modi-map "w" sunshine-quick-forecast)
-    (bind-to-modi-map "W" sunshine-forecast)))
+    :commands (sunshine-quick-forecast sunshine-forecast)
+    :init
+    (progn
+      (bind-to-modi-map "w" sunshine-quick-forecast)
+      (bind-to-modi-map "W" sunshine-forecast))
+    :config
+    (progn
+      (setq sunshine-location "27278,USA")
+      (setq sunshine-show-icons t)))
 
 
 (provide 'setup-sunshine)
