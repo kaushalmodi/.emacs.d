6c657c16eabc012286b1433d205506ded0f5a5f7
Kaushal Modi <kaushal.modi@gmail.com>
2015-03-03 09:45:20 -0500

    Minor: Replace `require` with `use-package`

--- setup-files/setup-server.el
+++ setup-files/setup-server.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2014-08-13 11:26:09 kmodi>
+;; Time-stamp: <2015-03-03 09:19:56 kmodi>
 
 ;; server setup
 
@@ -7,27 +7,35 @@
 ;; opened using `emacsclient FILENAME&`
 
 ;; create the server directory if it doesn't exist
-(setq server-dir (concat user-emacs-directory "/server"))
-(unless (file-exists-p server-dir)
-  (make-directory server-dir))
-
-(require 'server)
-
-;; Suppress error "directory  ~/.emacs.d/server is unsafe" when
-;; running on cygwin
-(when (and (>= emacs-major-version 23)
-           (equal window-system 'x))
-  (setq server-use-tcp t)
-  (defun server-ensure-safe-dir (dir) "Noop" t))
-
-;; start a server only if one is not already running
-;; `server-running-p' returns "t" if a server is already running
-(if (not (server-running-p))
-    (progn
-      (server-start))
+(use-package server
+    :config
   (progn
-    (setq server-name "temp")
-    (server-start)))
+    (setq server-dir (concat user-emacs-directory "/server_"
+                             emacs-version-short))
+
+    (unless (file-exists-p server-dir)
+      (make-directory server-dir))
+
+    ;; Suppress error "directory  ~/.emacs.d/server is unsafe" when
+    ;; running on cygwin
+    (>=e "23.0"
+         (when (equal window-system 'x)
+           (setq server-use-tcp t)
+           (defun server-ensure-safe-dir (dir) "Noop" t)))
+
+    ;; start a server only if one is not already running
+    ;; `server-running-p' returns "t" if a server is already running
+    (defvar modi/server-temp nil
+      "If t, start a “temp” server if a server is already running;
+otherwise do nothing.")
+
+    (if (not (server-running-p))
+        (progn
+          (server-start))
+      (progn
+        (when modi/server-temp
+          (setq server-name "temp")
+          (server-start))))))
 
 
 (provide 'setup-server)
