f6b232f8b77ffe5f8c93b344cdf48a46251135c3
Kaushal Modi <kaushal.modi@gmail.com>
2015-03-12 16:57:47 -0400

    Change: Enable `ace-window-display-mode`
    - Use custom face for ace window number in the mode-line. The face
      foreground changes based on the theme background.
    - Use home row alphabet keys instead of numbers for ace window
    - Enable `ace-window-display-mode`

--- setup-files/setup-ace-window.el
+++ setup-files/setup-ace-window.el
@@ -1,16 +1,39 @@
-;; Time-stamp: <2015-02-23 11:42:32 kmodi>
+;; Time-stamp: <2015-03-12 15:48:28 kmodi>
 
 ;; Ace Window
-;; Source: https://github.com/abo-abo/ace-window
+;; https://github.com/abo-abo/ace-window
 
 (use-package ace-window
-  :config
+    :config
   (progn
+
+    (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+
+    (defface modi/aw-mode-line-face
+      '((((background dark)) (:foreground "gray50"))
+        (t                   (:foreground "black")))
+      "Face used for displaying the ace window key in the mode-line.")
+
+    ;; Patch `aw-update' to use the background sensitive face
+    (defun aw-update ()
+      "Update ace-window-path window parameter for all windows."
+      (avy-traverse
+       (avy-tree (aw-window-list) aw-keys)
+       (lambda (path leaf)
+         (set-window-parameter
+          leaf 'ace-window-path
+          (propertize
+           (apply #'string (reverse path))
+           'face 'modi/aw-mode-line-face)))))
+
+    (ace-window-display-mode t)
+
     (bind-keys
      :map modi-mode-map
      ;; Important to use my minor mode map as I want my bindings to override
      ;; bindings in other major modes (esp org-mode)
-     ("C-x o" . ace-window))))
+     ("C-x o" . ace-window)
+     ("M-o"   . ace-window))))
 
 
 (provide 'setup-ace-window)
