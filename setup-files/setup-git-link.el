b9d14a1d7835bbb603f13e13a7f93e4024465f33
Kaushal Modi <kaushal.modi@gmail.com>
2015-03-17 12:44:24 -0400

    Add: binding to copy git link to current code

--- setup-files/setup-git-link.el
+++ setup-files/setup-git-link.el
@@ -1,10 +1,16 @@
-;; Time-stamp: <2015-03-17 12:07:24 kmodi>
+;; Time-stamp: <2015-03-17 12:43:54 kmodi>
 
 ;; Diff-hl
 ;; https://github.com/sshaw/git-link
 
 (use-package git-link
     :load-path "elisp/git-link"
-    )
+    :config
+    (progn
+      (when (featurep 'region-bindings-mode)
+        (bind-keys
+         :map region-bindings-mode-map
+         ("g" . git-link-force-hash)))))
+
 
 (provide 'setup-git-link)
