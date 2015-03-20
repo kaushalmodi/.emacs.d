73e689be9a78c346f4a45c059f366fe1cf6ac43b
Kaushal Modi <kaushal.modi@gmail.com>
2015-03-20 16:30:19 -0400

    Add: `modi/kill-non-project-buffers` fn

--- setup-files/setup-projectile.el
+++ setup-files/setup-projectile.el
@@ -1,22 +1,22 @@
-;; Time-stamp: <2015-03-12 10:04:14 kmodi>
+;; Time-stamp: <2015-03-20 15:56:21 kmodi>
 
 ;; Projectile
 ;; Source: https://github.com/bbatsov/projectile
 
 (use-package projectile
     :init
-    (progn
-      (defhydra hydra-projectile-other-window (:color teal)
-        "projectile-other-window"
-        ("f"  projectile-find-file-other-window        "file")
-        ("g"  projectile-find-file-dwim-other-window   "file dwim")
-        ("d"  projectile-find-dir-other-window         "dir")
-        ("b"  projectile-switch-to-buffer-other-window "buffer")
-        ("q"  nil                                      "cancel" :color blue))
+  (progn
+    (defhydra hydra-projectile-other-window (:color teal)
+      "projectile-other-window"
+      ("f"  projectile-find-file-other-window        "file")
+      ("g"  projectile-find-file-dwim-other-window   "file dwim")
+      ("d"  projectile-find-dir-other-window         "dir")
+      ("b"  projectile-switch-to-buffer-other-window "buffer")
+      ("q"  nil                                      "cancel" :color blue))
 
-      (defhydra hydra-projectile (:color teal
-                                  :hint  nil)
-        "
+    (defhydra hydra-projectile (:color teal
+                                :hint  nil)
+      "
      PROJECTILE: %(if (fboundp 'projectile-project-root) (projectile-project-root) \"TBD\")
 
      Find File            Search/Tags          Buffers                Cache
@@ -28,97 +28,115 @@ _s-f_: file            _a_: ag                _i_: Ibuffer           _c_: cache 
   _d_: dir
 
 "
-        ("a"   projectile-ag)
-        ("b"   projectile-switch-to-buffer)
-        ("c"   projectile-invalidate-cache)
-        ("d"   projectile-find-dir)
-        ("s-f" projectile-find-file)
-        ("ff"  projectile-find-file-dwim)
-        ("fd"  projectile-find-file-in-directory)
-        ("g"   ggtags-update-tags)
-        ("s-g" ggtags-update-tags)
-        ("i"   projectile-ibuffer)
-        ("K"   projectile-kill-buffers)
-        ("s-k" projectile-kill-buffers)
-        ("m"   projectile-multi-occur)
-        ("o"   projectile-multi-occur)
-        ("s-p" projectile-switch-project "switch project")
-        ("p"   projectile-switch-project)
-        ("s"   projectile-switch-project)
-        ("r"   projectile-recentf)
-        ("x"   projectile-remove-known-project)
-        ("X"   projectile-cleanup-known-projects)
-        ("z"   projectile-cache-current-file)
-        ("`"   hydra-projectile-other-window/body "other window")
-        ("q"   nil "cancel" :color blue))
+      ("a"   projectile-ag)
+      ("b"   projectile-switch-to-buffer)
+      ("c"   projectile-invalidate-cache)
+      ("d"   projectile-find-dir)
+      ("s-f" projectile-find-file)
+      ("ff"  projectile-find-file-dwim)
+      ("fd"  projectile-find-file-in-directory)
+      ("g"   ggtags-update-tags)
+      ("s-g" ggtags-update-tags)
+      ("i"   projectile-ibuffer)
+      ("K"   projectile-kill-buffers)
+      ("s-k" projectile-kill-buffers)
+      ("m"   projectile-multi-occur)
+      ("o"   projectile-multi-occur)
+      ("s-p" projectile-switch-project "switch project")
+      ("p"   projectile-switch-project)
+      ("s"   projectile-switch-project)
+      ("r"   projectile-recentf)
+      ("x"   projectile-remove-known-project)
+      ("X"   projectile-cleanup-known-projects)
+      ("z"   projectile-cache-current-file)
+      ("`"   hydra-projectile-other-window/body "other window")
+      ("q"   nil "cancel" :color blue))
 
-      (bind-key "s-f" #'hydra-projectile/body modi-mode-map)
-      (bind-key "s-p" #'hydra-projectile/body modi-mode-map))
-    :config
-    (progn
-      (defvar projectile-ag-command
-        (concat "\\ag" ; used unaliased version of `ag': \ag
-                " -i" ; case insensitive
-                " -f" ; follow symbolic links
-                " --skip-vcs-ignores" ; Ignore files/dirs ONLY from `.agignore',
-                " -0" ; output null separated results
-                " -g ''") ; get file names matching the regex ''
-        "Ag command to be used by projectile to generate file cache.")
+    (bind-key "s-f" #'hydra-projectile/body modi-mode-map)
+    (bind-key "s-p" #'hydra-projectile/body modi-mode-map))
+  :config
+  (progn
+    (defvar projectile-ag-command
+      (concat "\\ag" ; used unaliased version of `ag': \ag
+              " -i" ; case insensitive
+              " -f" ; follow symbolic links
+              " --skip-vcs-ignores" ; Ignore files/dirs ONLY from `.agignore',
+              " -0" ; output null separated results
+              " -g ''") ; get file names matching the regex ''
+      "Ag command to be used by projectile to generate file cache.")
 
-      (when (executable-find "ag")
-        (defun projectile-get-ext-command ()
-          "Override the projectile-defined function so that `ag' is always used for
+    (when (executable-find "ag")
+      (defun projectile-get-ext-command ()
+        "Override the projectile-defined function so that `ag' is always used for
 getting a list of all files in a project."
-          projectile-ag-command))
+        projectile-ag-command))
 
-      ;; Make the file list creation faster by NOT calling `projectile-get-sub-projects-files'
-      (defun projectile-get-repo-files ()
-        (projectile-files-via-ext-command (projectile-get-ext-command)))
+    ;; Make the file list creation faster by NOT calling `projectile-get-sub-projects-files'
+    (defun projectile-get-repo-files ()
+      (projectile-files-via-ext-command (projectile-get-ext-command)))
 
-      ;; Don't consider my home dir as a project
-      (add-to-list 'projectile-ignored-projects `,(concat (getenv "HOME") "/"))
+    ;; Don't consider my home dir as a project
+    (add-to-list 'projectile-ignored-projects `,(concat (getenv "HOME") "/"))
 
-      (defun projectile-cache-files-find-file-hook ()
-        "Function for caching files with `find-file-hook'."
-        (when (and projectile-enable-caching
-                   (projectile-project-p)
-                   (not (member (projectile-project-p) projectile-ignored-projects)))
-          (projectile-cache-current-file)))
+    (defun projectile-cache-files-find-file-hook ()
+      "Function for caching files with `find-file-hook'."
+      (when (and projectile-enable-caching
+                 (projectile-project-p)
+                 (not (member (projectile-project-p) projectile-ignored-projects)))
+        (projectile-cache-current-file)))
 
-      ;; (setq projectile-enable-caching nil)
-      (setq projectile-enable-caching t) ; Enable caching, otherwise
+    ;; (setq projectile-enable-caching nil)
+    (setq projectile-enable-caching t) ; Enable caching, otherwise
                                         ; `projectile-find-file' is really slow
                                         ; for large projects
-      (dolist (item '(".SOS" "nobackup"))
-        (add-to-list 'projectile-globally-ignored-directories item))
-      (dolist (item '("GTAGS" "GRTAGS" "GPATH"))
-        (add-to-list 'projectile-globally-ignored-files item))
-      ;; Customize the Projectile mode-line lighter
-      ;; (setq projectile-mode-line '(:eval (format " Projectile[%s]" (projectile-project-name))))
-      ;; (setq projectile-mode-line " ∏")
-      ;; (setq projectile-mode-line " ℙ")
-      (setq projectile-mode-line " ρ")
+    (dolist (item '(".SOS" "nobackup"))
+      (add-to-list 'projectile-globally-ignored-directories item))
+    (dolist (item '("GTAGS" "GRTAGS" "GPATH"))
+      (add-to-list 'projectile-globally-ignored-files item))
+    ;; Customize the Projectile mode-line lighter
+    ;; (setq projectile-mode-line '(:eval (format " Projectile[%s]" (projectile-project-name))))
+    ;; (setq projectile-mode-line " ∏")
+    ;; (setq projectile-mode-line " ℙ")
+    (setq projectile-mode-line " ρ")
 
-      (defun projectile-project-name ()
-        "Return project name.
+    (defun projectile-project-name ()
+      "Return project name.
 If the project root contains \"sos_\" or \"src\", remove the directory basename
 from the project root name. E.g. if `projectile-project-root' is \"/a/b/src\",
 remove the \"src\" portion from it and make it \"/a/b\"."
-        (let ((project-root
-               (condition-case nil
-                   (projectile-project-root)
-                 (error default-directory)))
-              project-name)
+      (let ((project-root
+             (condition-case nil
+                 (projectile-project-root)
+               (error default-directory)))
+            project-name)
+        (setq project-name (file-name-nondirectory
+                            (directory-file-name project-root)))
+        (while (string-match "\\(sos_\\|\\bsrc\\b\\)" project-name)
+          (setq project-root (replace-regexp-in-string
+                              "\\(.*\\)/.+/*$" "\\1" project-root))
           (setq project-name (file-name-nondirectory
-                              (directory-file-name project-root)))
-          (while (string-match "\\(sos_\\|\\bsrc\\b\\)" project-name)
-            (setq project-root (replace-regexp-in-string
-                                "\\(.*\\)/.+/*$" "\\1" project-root))
-            (setq project-name (file-name-nondirectory
-                                (directory-file-name project-root))))
-          project-name))
+                              (directory-file-name project-root))))
+        project-name))
 
-      (projectile-global-mode)))
+    ;; http://emacs.stackexchange.com/a/10187/115
+    (defun modi/kill-non-project-buffers (&optional kill-special)
+      "Kill buffers that do not belong to a `projectile' project.
+
+With prefix argument (`C-u'), also kill the special buffers."
+      (interactive "P")
+      (let ((bufs (buffer-list (selected-frame))))
+        (dolist (buf bufs)
+          (with-current-buffer buf
+            (let ((buf-name (buffer-name buf)))
+              (when (or (null (projectile-project-p))
+                        (and kill-special
+                             (string-match "^\*" buf-name)))
+                ;; Preserve buffers with names starting with *scratch or *Messages
+                (unless (string-match "^\\*\\(\\scratch\\|Messages\\)" buf-name)
+                  (message "Killing buffer %s" buf-name)
+                  (kill-buffer buf))))))))
+
+    (projectile-global-mode)))
 
 
 (provide 'setup-projectile)
