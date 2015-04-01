83b25550ade8fd4c7f3b42c2eedc309b85313ed2
Kaushal Modi <kaushal.modi@gmail.com>
2015-04-01 09:11:58 -0400

    Minor: Remove key-chord duplicates from list

--- setup-files/setup-key-chord.el
+++ setup-files/setup-key-chord.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-04-01 08:59:07 kmodi>
+;; Time-stamp: <2015-04-01 09:10:33 kmodi>
 
 ;; Key Chord Mode
 ;; http://www.emacswiki.org/emacs/key-chord.el
@@ -12,23 +12,18 @@
 (provide 'setup-key-chord)
 
 ;; John Cook's post http://www.johndcook.com/blog/2015/02/01/rare-bigrams/
-;; provides a list of rare bi-grams that would work great for key-chords
-;;      bf bg bq bx bz
-;;      cj cv
-;;      dx dz
-;;      fb fj fq fv fz
-;;      gb gj gp gq gv gx gz
-;;      hj hq hv
+;; provides a list of rare bi-grams that would work great for key-chords.
+
+;; Below list is based off that after removing all the key-chord duplicates
+;; like `xs' and `sx'.
+
+;;      fb
+;;      gb gp
 ;;  jj  jc jf jg jh jk jl jm jp jq js jt jv jw jx jy jz
-;;  kk  kj kq kv kx kz
-;;      lj lq
-;;      mj mq mv mx mz
-;;      pg pj pq pv pz
-;;  qq  qb qf qg qh qj qk ql qm qp qt qv qw qx qy qz
-;;      sj sx sz
-;;      tj tq
-;;  vv  vc vf vg vh vj vk vm vp vq vw vz
-;;  ww  wj wq wv wx wz
-;;      xb xd xg xj xk xm xq xs xw xz
-;;  yy  yj yq
-;;      zb zd zf zg zj zk zm zp zq zs zv zw zx
+;;  kk
+;;  qq  qb qf qg qh qk ql qm qp qt qv qw qx qy qz
+;;  vv  vc vf vg vh vk vm vp vw vz
+;;  ww
+;;      xb xd xg xk xm xs xw
+;;  yy
+;;      zb zd zf zg zk zm zp zs zw zx
