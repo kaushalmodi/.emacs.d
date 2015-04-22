f78fe65b31fd06b64853942050b33984abb818b6
Kaushal Modi <kaushal.modi@gmail.com>
2015-04-22 09:48:09 -0400

    Add: `git-link-force-hash` fn

--- setup-files/setup-git-link.el
+++ setup-files/setup-git-link.el
@@ -1,16 +1,22 @@
-;; Time-stamp: <2015-03-17 12:43:54 kmodi>
+;; Time-stamp: <2015-04-22 09:47:50 kmodi>
 
 ;; Diff-hl
 ;; https://github.com/sshaw/git-link
 
 (use-package git-link
-    :load-path "elisp/git-link"
-    :config
-    (progn
-      (when (featurep 'region-bindings-mode)
-        (bind-keys
-         :map region-bindings-mode-map
-         ("g" . git-link-force-hash)))))
+  :load-path "elisp/git-link"
+  :config
+  (progn
+    (defun git-link-force-hash ()
+      "This function is the same as `git-link' except that it copies the link
+with the commit hash in the link even if branch name is available."
+      (interactive)
+      (let ((git-link-use-commit t))
+        (call-interactively 'git-link)))
+    (when (featurep 'region-bindings-mode)
+      (bind-keys
+       :map region-bindings-mode-map
+        ("g" . git-link-force-hash)))))
 
 
 (provide 'setup-git-link)
