a332a8f043c8457fb23e709eb68722db4de6d237
Kaushal Modi <kaushal.modi@gmail.com>
2015-05-06 11:51:06 -0400

    Change: Enable pgbl mode for more major modes

--- setup-files/setup-page-break-lines.el
+++ setup-files/setup-page-break-lines.el
@@ -1,4 +1,7 @@
-;; Time-stamp: <2015-02-23 11:42:33 kmodi>
+;; Time-stamp: <2015-05-06 11:50:36 kmodi>
+
+;; Show actual lines instead of the page break char ^L
+;; https://github.com/purcell/page-break-lines
 
 (use-package page-break-lines
   :config
@@ -10,7 +13,9 @@
             compilation-mode
             outline-mode
             help-mode
-            text-mode))
+            text-mode
+            fundamental-mode
+            special-mode))
     ;; Calling `global-page-break-lines-mode' will enable `page-break-mode'
     ;; automatically in all the modes listed in `page-break-lines-modes' var
     (global-page-break-lines-mode)))
