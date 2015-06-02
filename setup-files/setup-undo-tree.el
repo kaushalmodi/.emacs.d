f3a38abd64fb7482d531074d82239792ed7809ee
Kaushal Modi <kaushal.modi@gmail.com>
2015-06-02 17:16:53 -0400

    Major: Enable persistent undo-tree history
    - Now undo-tree history will be persistent across emacs sessions
    - History is saved in ~/.emacs.d/undo-tree-history/ directory
    - Improve the key binding notes in the comments

--- setup-files/setup-undo-tree.el
+++ setup-files/setup-undo-tree.el
@@ -1,16 +1,28 @@
-;; Time-stamp: <2015-03-20 13:51:15 kmodi>
+;; Time-stamp: <2015-06-02 17:16:11 kmodi>
 
 ;; Undo Tree
 ;; http://www.dr-qubit.org/emacs.php
 
 (use-package undo-tree
-    :config
+  :config
   (progn
+    ;; Persistent undo-tree history across emacs sessions
+    (setq modi/undo-tree-history-dir (let ((dir (concat user-emacs-directory
+                                                        "undo-tree-history/")))
+                                       (make-directory dir :parents)
+                                       dir))
+    (setq undo-tree-history-directory-alist `(("." . ,modi/undo-tree-history-dir)))
+    (setq undo-tree-auto-save-history t)
+
+    ;; Compress the history files as .gz files
+    (advice-add 'undo-tree-make-history-save-file-name :filter-return
+                (lambda (return-val) (concat return-val ".gz")))
+
     (bind-keys
      :map undo-tree-map
-     ("C-_" . undo-tree-undo)
-     ("C-/" . undo-tree-undo)
-     ("M-_" . undo-tree-redo)) ; default
+      ("C-_" . undo-tree-undo)
+      ("C-/" . undo-tree-undo)
+      ("M-_" . undo-tree-redo)) ; default
 
     (global-undo-tree-mode)))
 
@@ -18,58 +30,48 @@
 (provide 'setup-undo-tree)
 
 ;; `undo-tree' package also remaps `undo' and `undo-only' to `undo-tree-undo'
-
+;;
 ;; * `undo-tree-visualize' bindings
 ;;
-;; | [remap previous-line] | 'undo-tree-visualize-undo |
-;; | [remap next-line]     | 'undo-tree-visualize-redo |
-;; | [up]                  | 'undo-tree-visualize-undo |
-;; | "p"                   | 'undo-tree-visualize-undo |
-;; | "\C-p"                | 'undo-tree-visualize-undo |
-;; | [down]                | 'undo-tree-visualize-redo |
-;; | "n"                   | 'undo-tree-visualize-redo |
-;; | "\C-n"                | 'undo-tree-visualize-redo |
+;; | [remap previous-line] | undo-tree-visualize-undo |
+;; | [remap next-line]     | undo-tree-visualize-redo |
+;; | C-p                   | undo-tree-visualize-undo |
+;; | C-n                   | undo-tree-visualize-redo |
 ;;
 ;; Horizontal motion keys switch branch
-;; | [remap forward-char]  | 'undo-tree-visualize-switch-branch-right) |
-;; | [remap backward-char] | 'undo-tree-visualize-switch-branch-left)  |
-;; | [right]               | 'undo-tree-visualize-switch-branch-right  |
-;; | "f"                   | 'undo-tree-visualize-switch-branch-right  |
-;; | "\C-f"                | 'undo-tree-visualize-switch-branch-right  |
-;; | [left]                | 'undo-tree-visualize-switch-branch-left   |
-;; | "b"                   | 'undo-tree-visualize-switch-branch-left   |
-;; | "\C-b"                | 'undo-tree-visualize-switch-branch-left   |
+;; | [remap forward-char]  | undo-tree-visualize-switch-branch-right |
+;; | [remap backward-char] | undo-tree-visualize-switch-branch-left  |
+;; | C-f                   | undo-tree-visualize-switch-branch-right |
+;; | C-b                   | undo-tree-visualize-switch-branch-left  |
 ;;
 ;; Paragraph motion keys undo/redo to significant points in tree
-;; | [remap backward-paragraph] | 'undo-tree-visualize-undo-to-x |
-;; | [remap forward-paragraph]  | 'undo-tree-visualize-redo-to-x |
-;; | "\M-{"                     | 'undo-tree-visualize-undo-to-x |
-;; | "\M-}"                     | 'undo-tree-visualize-redo-to-x |
-;; | [C-up]                     | 'undo-tree-visualize-undo-to-x |
-;; | [C-down]                   | 'undo-tree-visualize-redo-to-x |
+;; | [remap backward-paragraph] | undo-tree-visualize-undo-to-x |
+;; | [remap forward-paragraph]  | undo-tree-visualize-redo-to-x |
+;; | M-{                        | undo-tree-visualize-undo-to-x |
+;; | M-}                        | undo-tree-visualize-redo-to-x |
+;; | C-up                       | undo-tree-visualize-undo-to-x |
+;; | C-down                     | undo-tree-visualize-redo-to-x |
 ;;
 ;; Mouse sets buffer state to node at click
-;; | [mouse-1] | 'undo-tree-visualizer-mouse-set |
+;; | [mouse-1] | undo-tree-visualizer-mouse-set |
 ;;
 ;; Toggle timestamps
-;; | "t" | 'undo-tree-visualizer-toggle-timestamps |
+;; | t | undo-tree-visualizer-toggle-timestamps |
 ;;
 ;; Toggle diff
-;; | "d" | 'undo-tree-visualizer-toggle-diff |
+;; | d | undo-tree-visualizer-toggle-diff |
 ;;
 ;; Toggle selection mode
-;; | "s" | 'undo-tree-visualizer-selection-mode |
+;; | s | undo-tree-visualizer-selection-mode |
 ;;
 ;; Horizontal scrolling may be needed if the tree is very wide
-;; | "," | 'undo-tree-visualizer-scroll-left  |
-;; | "." | 'undo-tree-visualizer-scroll-right |
-;; | "<" | 'undo-tree-visualizer-scroll-left  |
-;; | ">" | 'undo-tree-visualizer-scroll-right |
+;; | , / < | undo-tree-visualizer-scroll-left  |
+;; | . / > | undo-tree-visualizer-scroll-right |
 ;;
 ;; Vertical scrolling may be needed if the tree is very tall
-;; | [next]  | 'undo-tree-visualizer-scroll-up   |
-;; | [prior] | 'undo-tree-visualizer-scroll-down |
+;; | Page Down | undo-tree-visualizer-scroll-up   |
+;; | Page Up   | undo-tree-visualizer-scroll-down |
 ;;
 ;; Quit/abort visualizer
-;; | "q"    | 'undo-tree-visualizer-quit  |
-;; | "\C-q" | 'undo-tree-visualizer-abort |
+;; | q   | undo-tree-visualizer-quit  |
+;; | C-q | undo-tree-visualizer-abort |
