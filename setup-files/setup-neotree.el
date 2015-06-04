f5b84c55abb62e1a1f1589bfc21743fe40dd14a7
Kaushal Modi <kaushal.modi@gmail.com>
2015-06-04 11:52:34 -0400

    Add: Fns to view next/prev file in dir
    - Use neotree to enabling viewing next/prev files in current dir
    - Patch neo-vc-for-note.. but note that this patch is still not fully functional

--- setup-files/setup-neotree.el
+++ setup-files/setup-neotree.el
@@ -1,10 +1,10 @@
-;; Time-stamp: <2015-03-23 09:25:23 kmodi>
+;; Time-stamp: <2015-06-04 11:25:56 kmodi>
 
 ;; Neotree
 ;; https://github.com/jaypei/emacs-neotree
 
 (use-package neotree
-    :init
+  :init
   (progn
     (setq-default neo-smart-open t) ;  every time when the neotree window is
                                         ;  opened, it will try to find current
@@ -33,23 +33,81 @@
 
     (setq neo-theme 'nerd) ; 'classic, 'nerd, 'ascii, 'arrow
 
+    (setq neo-vc-integration '(face char))
+
+    ;; Patch to fix vc integration
+    (defun neo-vc-for-node (node)
+      (let* ((backend (vc-backend node))
+             (vc-state (when backend (vc-state node backend))))
+        ;; (message "%s %s %s" node backend vc-state)
+        (cons (cdr (assoc vc-state neo-vc-state-char-alist))
+              (cl-case vc-state
+                (up-to-date       neo-vc-up-to-date-face)
+                (edited           neo-vc-edited-face)
+                (needs-update     neo-vc-needs-update-face)
+                (needs-merge      neo-vc-needs-merge-face)
+                (unlocked-changes neo-vc-unlocked-changes-face)
+                (added            neo-vc-added-face)
+                (removed          neo-vc-removed-face)
+                (conflict         neo-vc-conflict-face)
+                (missing          neo-vc-missing-face)
+                (ignored          neo-vc-ignored-face)
+                (unregistered     neo-vc-unregistered-face)
+                (user             neo-vc-user-face)
+                (t                neo-vc-default-face)))))
+
     (defun modi/neotree-go-up-dir ()
       (interactive)
       (goto-char (point-min))
       (forward-line 2)
       (neotree-change-root))
 
+    ;; http://emacs.stackexchange.com/a/12156/115
+    (defun modi/find-file-next-in-dir (&optional prev)
+      "Open the next file in the directory.
+When PREV is non-nil, open the previous file in the directory."
+      (interactive "P")
+      (let ((neo-init-state (neo-global--window-exists-p)))
+        (if (null neo-init-state)
+            (neotree-show))
+        (neo-global--select-window)
+        (if (if prev
+                (neotree-previous-line)
+              (neotree-next-line))
+            (progn
+              (neo-buffer--execute nil
+                                   (quote neo-open-file)
+                                   (lambda (full-path &optional arg)
+                                     (message "Reached dir: %s/" full-path)
+                                     (if prev
+                                         (neotree-next-line)
+                                       (neotree-previous-line)))))
+          (progn
+            (if prev
+                (message "You are already on the first file in the directory.")
+              (message "You are already on the last file in the directory."))))
+        (if (null neo-init-state)
+            (neotree-hide))))
+
+    (defun modi/find-file-prev-in-dir ()
+      "Open the next file in the directory."
+      (interactive)
+      (modi/find-file-next-in-dir :prev))
+
+    (bind-keys
+     :map modi-mode-map
+      ("C-c >" . modi/find-file-next-in-dir)
+      ("C-c <" . modi/find-file-prev-in-dir))
+
     (bind-keys
      :map neotree-mode-map
-     ("^"          . modi/neotree-go-up-dir)
-     ("<C-return>" . neotree-change-root)
-     ("C"          . neotree-change-root)
-     ("c"          . neotree-create-node)
-     ("+"          . neotree-create-node)
-     ("d"          . neotree-delete-node)
-     ("r"          . neotree-rename-node))
-
-    (bind-to-modi-map "n" neotree-toggle)))
+      ("^"          . modi/neotree-go-up-dir)
+      ("<C-return>" . neotree-change-root)
+      ("C"          . neotree-change-root)
+      ("c"          . neotree-create-node)
+      ("+"          . neotree-create-node)
+      ("d"          . neotree-delete-node)
+      ("r"          . neotree-rename-node))))
 
 
 (provide 'setup-neotree)
