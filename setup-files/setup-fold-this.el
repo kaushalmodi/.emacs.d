8b2d0c7a18af2c3a39a2d50ab9ea2fdaabcafc90
Kaushal Modi <kaushal.modi@gmail.com>
2015-06-08 17:28:23 -0400

    Major: Revamp fold-this settings
    - Change file name saved in fold-this-persistent-folds-file for
      consistency
    - Bind left-mouse click to unfold fold at point
    - Bind 'f' in region-bindings-mode to fold
    - If the last command was `fold-this`, make undo or undo-tree-undo
      unfold that

--- setup-files/setup-fold-this.el
+++ setup-files/setup-fold-this.el
@@ -1,20 +1,42 @@
-;; Time-stamp: <2015-06-01 14:31:28 kmodi>
+;; Time-stamp: <2015-06-08 17:23:23 kmodi>
 
 ;; Fold This
 ;; https://github.com/magnars/fold-this.el
 
 (use-package fold-this
-  :init
-  (progn
-    (setq fold-this-persistent-folds-file (locate-user-emacs-file
-                                           "fold-this-cache.el")))
   :config
   (progn
+    (setq fold-this-persistent-folds-file (locate-user-emacs-file "saved-folds"))
     (setq fold-this-persistent-folds t)
+
+    (bind-keys
+     :map fold-this-keymap
+      ("<mouse-1>" . fold-this-unfold-at-point)) ; left-click on ellipsis to unfold
+    (when (featurep 'region-bindings-mode)
+      (bind-keys
+       :map region-bindings-mode-map
+        ("f" . fold-this)))
     ;; Below bindings are made in global map and not in my minor mode as I want
     ;; other modes to override those bindings
     (bind-keys
-     ("C-c C-f" . fold-this))))
+     ("C-c C-f" . fold-this))
+
+    (defun modi/unfold-if-last-command-fold (&rest args)
+      "If the `last-command' was `fold-this', undo that fold action."
+      (let ((last-cmd-was-fold (eq last-command #'fold-this)))
+        (when last-cmd-was-fold
+          (backward-char 1)
+          (fold-this-unfold-at-point))
+        last-cmd-was-fold))
+    ;; Advice `undo-tree-undo' to unfold the previous fold
+    (with-eval-after-load 'undo-tree
+      (advice-add #'undo-tree-undo :before-until #'modi/unfold-if-last-command-fold))
+    ;; Advice `undo' to unfold the previous fold
+    (with-eval-after-load 'undo-tree
+      (advice-add #'undo :before-until #'modi/unfold-if-last-command-fold))))
 
 
 (provide 'setup-fold-this)
+
+;; Fold the selected region using `fold-this' binding.
+;; If you move point into the ellipsis and press `RET' or `C-g', it is unfolded.
