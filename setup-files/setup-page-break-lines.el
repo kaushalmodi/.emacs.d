8fbab4dcc2a08413366633218af0980071201084
Kaushal Modi <kaushal.modi@gmail.com>
2015-06-09 14:55:57 -0400

    Make a note on the issue of wrapping page ln rule
    - Using the fix suggested by the package author did not work for me

--- setup-files/setup-page-break-lines.el
+++ setup-files/setup-page-break-lines.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-05-06 11:50:36 kmodi>
+;; Time-stamp: <2015-06-09 14:55:27 kmodi>
 
 ;; Show actual lines instead of the page break char ^L
 ;; https://github.com/purcell/page-break-lines
@@ -6,16 +6,22 @@
 (use-package page-break-lines
   :config
   (progn
-    (setq page-break-lines-modes
-          '(emacs-lisp-mode
-            lisp-mode
-            scheme-mode
-            compilation-mode
-            outline-mode
-            help-mode
-            text-mode
-            fundamental-mode
-            special-mode))
+    (setq page-break-lines-modes '(emacs-lisp-mode
+                                   lisp-mode
+                                   scheme-mode
+                                   compilation-mode
+                                   outline-mode
+                                   help-mode
+                                   text-mode
+                                   fundamental-mode
+                                   special-mode))
+
+    ;; Fix the issue of page break rule wrapping across multiple lines
+    ;; - Above fix doesn't work -- Tue Jun 09 14:03:38 EDT 2015 - kmodi
+    ;; (set-fontset-font "fontset-default"
+    ;;                   (cons page-break-lines-char page-break-lines-char)
+    ;;                   (face-attribute 'default :family))
+
     ;; Calling `global-page-break-lines-mode' will enable `page-break-mode'
     ;; automatically in all the modes listed in `page-break-lines-modes' var
     (global-page-break-lines-mode)))
