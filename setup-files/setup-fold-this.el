57aee0861adeef88f20f2ddc8f5e3e5a6b4fb5f8
Kaushal Modi <kaushal.modi@gmail.com>
2015-06-09 00:10:50 -0400

    Fix: Make undo fold more robust
    - Patch the original `fold-this` function to save the last fold overlay

--- setup-files/setup-fold-this.el
+++ setup-files/setup-fold-this.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-06-08 17:23:23 kmodi>
+;; Time-stamp: <2015-06-09 00:02:38 kmodi>
 
 ;; Fold This
 ;; https://github.com/magnars/fold-this.el
@@ -8,6 +8,22 @@
   (progn
     (setq fold-this-persistent-folds-file (locate-user-emacs-file "saved-folds"))
     (setq fold-this-persistent-folds t)
+
+    (defvar modi/fold-this--last-overlay nil
+      "Store the last overlay created by `fold-this'.")
+
+    (defun fold-this (beg end)
+      (interactive "r")
+      (let ((o (make-overlay beg end nil t nil)))
+        (overlay-put o 'type 'fold-this)
+        (overlay-put o 'invisible t)
+        (overlay-put o 'keymap fold-this-keymap)
+        (overlay-put o 'face 'fold-this-overlay)
+        (overlay-put o 'modification-hooks '(fold-this--unfold-overlay))
+        (overlay-put o 'display (propertize "..." 'face 'fold-this-overlay))
+        (overlay-put o 'evaporate t)
+        (setq modi/fold-this--last-overlay o))
+      (deactivate-mark))
 
     (bind-keys
      :map fold-this-keymap
@@ -25,8 +41,7 @@
       "If the `last-command' was `fold-this', undo that fold action."
       (let ((last-cmd-was-fold (eq last-command #'fold-this)))
         (when last-cmd-was-fold
-          (backward-char 1)
-          (fold-this-unfold-at-point))
+          (delete-overlay modi/fold-this--last-overlay))
         last-cmd-was-fold))
     ;; Advice `undo-tree-undo' to unfold the previous fold
     (with-eval-after-load 'undo-tree
