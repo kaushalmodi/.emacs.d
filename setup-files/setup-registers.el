d683e276cab8578d92d1d61a423d7e78b7f0eb52
Kaushal Modi <kaushal.modi@gmail.com>
2015-06-16 17:30:00 -0400

    Minor updates

--- setup-files/setup-registers.el
+++ setup-files/setup-registers.el
@@ -1,32 +1,31 @@
-;; Time-stamp: <2015-05-22 11:58:34 kmodi>
+;; Time-stamp: <2015-06-16 17:27:04 kmodi>
 
-;; Source: http://stackoverflow.com/questions/12558019/shortcut-to-open-a-specific-file-in-emacs
-;; Source: http://www.gnu.org/software/emacs/manual/html_node/emacs/File-Registers.html#File-Registers
+;; Registers
+
+;; http://stackoverflow.com/questions/12558019/shortcut-to-open-a-specific-file-in-emacs
+;; http://www.gnu.org/software/emacs/manual/html_node/emacs/File-Registers.html
 ;; Save the frequently accessed file locations in registers for quick access
 
 
-(set-register ?e (cons 'file (expand-file-name "init.el" user-emacs-directory))) ;; C-x r j e
+(set-register ?e (cons 'file (expand-file-name "init.el" user-emacs-directory))) ; C-x r j e
 
-(set-register ?j (cons 'file (expand-file-name "journal.org" org-directory))) ;; C-x r j j
+(set-register ?j (cons 'file (expand-file-name "journal.org" org-directory))) ; C-x r j j
 
-(set-register ?a (cons 'file (expand-file-name ".alias" user-home-directory ))) ;; C-x r j a
-(set-register ?t (cons 'file (expand-file-name ".tmux.conf" user-home-directory ))) ;; C-x r j t
+(set-register ?a (cons 'file (expand-file-name ".alias" user-home-directory ))) ; C-x r j a
+(set-register ?t (cons 'file (expand-file-name ".tmux.conf" user-home-directory ))) ; C-x r j t
 (set-register ?i (cons 'file (expand-file-name
                               "index.html"
-                              (concat user-home-directory "public_html/")))) ;; C-x r j i
+                              (concat user-home-directory "public_html/")))) ; C-x r j i
 
 (set-register ?v (cons 'file (expand-file-name
                               "IEEE_STD_1800-2012_SystemVerilog.pdf"
-                              (concat user-home-directory "docs/")))) ;; C-x r j v
+                              (concat user-home-directory "docs/")))) ; C-x r j v
 
 
 (provide 'setup-registers)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Registers
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Text-Registers.html
 
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Text-Registers.html#Text-Registers
 ;;     C-x r s REG <- Copy region to register REG
 ;; C-u C-x r s REG <- CUT region and move to register REG
 ;;     C-x r i REG <- Insert text from register REG
