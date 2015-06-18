d7a06803f725d74b495d103491786a2fae165955
Kaushal Modi <kaushal.modi@gmail.com>
2015-06-18 09:49:35 -0400

    Provide hydra-projectile an extra binding: C-c f

--- setup-files/setup-projectile.el
+++ setup-files/setup-projectile.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-06-09 08:17:40 kmodi>
+;; Time-stamp: <2015-06-18 00:40:58 kmodi>
 
 ;; Projectile
 ;; Source: https://github.com/bbatsov/projectile
@@ -53,7 +53,8 @@ _f_/_s-f_: file               _a_: ag                ^^    _i_: Ibuffer         
       ("q"   nil "cancel" :color blue))
     (bind-key "s-f" #'hydra-projectile/body modi-mode-map)
     (setq projectile-keymap-prefix (kbd "C-c P"))
-    (bind-key "C-c p" #'hydra-projectile/body modi-mode-map))
+    (bind-key "C-c p" #'hydra-projectile/body modi-mode-map)
+    (bind-key "C-c f" #'hydra-projectile/body modi-mode-map))
   :config
   (progn
     (when (not (bound-and-true-p disable-pkg-ivy))
