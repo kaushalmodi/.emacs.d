7029f21edb1c1a07c19805134fba89f91e4a8eff
Kaushal Modi <kaushal.modi@gmail.com>
2015-06-19 10:09:07 -0400

    Add git-timemachine

--- setup-files/setup-git-timemachine.el
+++ setup-files/setup-git-timemachine.el
@@ -1,10 +1,15 @@
-;; Time-stamp: <2015-04-14 15:46:47 kmodi>
+;; Time-stamp: <2015-06-18 17:03:19 kmodi>
 
 ;; Git Time Machine
 ;; https://github.com/pidu/git-timemachine
 
-(use-package git-timemachine
-  )
+(use-package git-timemachine)
 
 
 (provide 'setup-git-timemachine)
+
+;; - First do M-x git-timemachine
+;; - Then navigate versions by hitting p and n keys
+;;
+;; Do NOT call git-timemachine-mode or git-timemachine-show-previous-revision
+;; or other functions directly!
