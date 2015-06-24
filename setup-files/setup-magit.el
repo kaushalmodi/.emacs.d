c56742e0fba8c617d80e79e74f5bb8fba27628b2
Kaushal Modi <kaushal.modi@gmail.com>
2015-06-23 22:46:34 -0400

    Don't show whitespc diffs when reviewing in magit

--- setup-files/setup-magit.el
+++ setup-files/setup-magit.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-06-17 13:02:01 kmodi>
+;; Time-stamp: <2015-06-22 09:40:27 kmodi>
 
 ;; magit
 ;; https://github.com/magit/magit
@@ -16,6 +16,13 @@
     (setq magit-repo-dirs                `( ,user-emacs-directory))
     (setq magit-diff-options             nil) ; default
     ;; (setq magit-diff-options             '("--ignore-space-change"))
+
+    ;; Make diffs not show whitespace difference when calling `magit-show-level-4'
+    ;; using the bindings like `4' in magit Status buffer
+    (defun modi/magit-ignore-whitespace-diff (orig-fun &rest args)
+      (let ((magit-diff-options '("--ignore-space-change")))
+        (apply orig-fun args)))
+    (advice-add 'magit-show-level-4 :around #'modi/magit-ignore-whitespace-diff)
 
     (magit-auto-revert-mode -1)))
 
