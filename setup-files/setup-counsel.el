4fb7c0bfd33f8952460bc94e61b25e8cbe735e60
Kaushal Modi <kaushal.modi@gmail.com>
2015-07-02 17:40:27 -0400

    Add `delete-file` action to `counsel-find-file`

--- setup-files/setup-counsel.el
+++ setup-files/setup-counsel.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-06-19 12:21:39 kmodi>
+;; Time-stamp: <2015-07-02 17:37:07 kmodi>
 
 ;; Counsel (comes packaged with the `swiper' package)
 
@@ -15,9 +15,28 @@
            ;; but still allow backup files named like abc.el~timestamp~
            "\\|\\(?:\\`[^~]+?[#~]\\'\\)"))
 
-    ;; (bind-keys
-    ;;  :map counsel-describe-map
-    ;;   ("SPC" . minibuffer-complete-word))
+    (defun counsel-find-file ()
+      "Forward to `find-file'."
+      (interactive)
+      (ivy-read "Find file: " 'read-file-name-internal
+                :matcher #'counsel--find-file-matcher
+                :action (cons
+                         1
+                         `(("default"
+                            (lambda (x)
+                              (find-file
+                               (expand-file-name x ivy--directory))))
+                           (,(propertize "delete"
+                                         'face 'font-lock-warning-face)
+                            (lambda (x)
+                              (delete-file
+                               (expand-file-name x ivy--directory))))))
+                :preselect (when counsel-find-file-at-point
+                             (require 'ffap)
+                             (ffap-guesser))
+                :require-match 'confirm-after-completion
+                :history 'file-name-history
+                :keymap counsel-find-file-map))
 
     (bind-keys
      :map modi-mode-map
