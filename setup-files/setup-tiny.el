b01c3c88ef751d9b5600c9b5942544cfee39b669
Kaushal Modi <kaushal.modi@gmail.com>
2015-07-24 17:49:45 -0400

    Fix when the | char should be inserted
    - Add tiny-helper examples
    - Update the modi/tiny-helper binding to C-c \

--- setup-files/setup-tiny.el
+++ setup-files/setup-tiny.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-07-24 14:10:23 kmodi>
+;; Time-stamp: <2015-07-24 17:48:37 kmodi>
 
 ;; https://github.com/abo-abo/tiny
 
@@ -10,9 +10,9 @@
 
 If prefix arg is non-nil, call `tiny-expand' directly.
 
-Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
-       M-x COMMAND 9  RET 2 RET _ RET +1*x2  RET    RET -> 5_7_9_11_13_15_17_19
-       M-x COMMAND 15 RET 1 RET   RET -30*2x RET %x RET -> 1c 1a 18 16 14 12 10 e c a 8 6 4 2 0
+Usage: M-x COMMAND ↵↵↵↵↵            -> 0 1 2 3 4 5 6 7 8 9
+       M-x COMMAND 9↵2↵_↵+1*x2↵↵    -> 5_7_9_11_13_15_17_19
+       M-x COMMAND 15↵1↵↵-30*2x↵%x↵ -> 1c 1a 18 16 14 12 10 e c a 8 6 4 2 0
 "
       (interactive
        (when (null current-prefix-arg)
@@ -34,7 +34,8 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
                                     "eg: *xx | (+ x ?A) | *2+3x]: "))
                (read-string (concat "Format "
                                     "[eg: %x | 0x%x | %c | %s | %(+ x x) | "
-                                    "%014.2f | %03d]: ")))))
+                                    "%014.2f | %03d; parentheses required "
+                                    "here for sexps]: ")))))
       (when (null current-prefix-arg)
         (let ((tiny-key-binding (substitute-command-keys "\\[tiny-expand]"))
               (begin-val-num (string-to-number begin-val))
@@ -55,12 +56,13 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
                            ", End value = " end-val ".")))
           (when (string= sep "")
             (setq sep " ")) ; `sep' cannot be empty string
+          (when (not (string= fmt ""))
+            ;; When non-nil, prefix `fmt' with the `|' char for reading clarity
+            (setq fmt (concat "|" fmt)))
           (when (and (string= begin-val "0")
                      (string= sep " "))
             (setq tiny-expr-concise (concat "m" end-val op fmt)))
-          (setq tiny-expr (concat "m" begin-val sep end-val op
-                                  (when (and (not (string= op ""))
-                                             (not (string= fmt ""))) "|") fmt))
+          (setq tiny-expr (concat "m" begin-val sep end-val op fmt))
           (message "%s" (concat "This "
                                 (propertize "tiny"
                                             'face 'font-lock-function-name-face)
@@ -81,7 +83,7 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
           (insert tiny-expr)))
       (tiny-expand))
 
-    (bind-key "C-c =" #'modi/tiny-helper modi-mode-map)
+    (bind-key "C-c \\" #'modi/tiny-helper modi-mode-map)
     (key-chord-define-global "]\\" #'tiny-expand)))
 
 
@@ -89,9 +91,9 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; General Format
 ;; mBSEO|F
-;;  ││││││
+;;  ││││―
 ;;  │││││└──> (optional) Format - %x | 0x%x | %c | %s | %(+ x x) | %014.2f | %03d | %(date "Jan 16" (* x 7))
-;;  ││││└───> (optional) Pipe character to separate Format for reading clarity
+;;  ││││└───> (optional) Pipe character used if Format specified for reading clarity
 ;;  │││└────> (optional) Lisp Operation - *xx | (* x x) | (+ x ?A) | *2+3x | (* 2 (+ 3 x))
 ;;  ││└─────> End value
 ;;  │└──────> (optional) Separator - Space | , | \n (default=Space)
@@ -101,11 +103,13 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; ================================================================================
 ;; m10 or m0 10
+;; TINY HELPER 10↵↵↵↵↵
 ;; --------------------------------------------------------------------------------
 ;; 0 1 2 3 4 5 6 7 8 9 10
 
 ;; ================================================================================
 ;; m\n10 or m0\n10
+;; TINY HELPER 10↵↵\n↵↵↵
 ;; --------------------------------------------------------------------------------
 ;; 0
 ;; 1
@@ -121,66 +125,81 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; ================================================================================
 ;; m5 10
+;; TINY HELPER 10↵5↵↵↵↵
 ;; --------------------------------------------------------------------------------
 ;; 5 6 7 8 9 10
 
 ;; ================================================================================
 ;; m5,10
+;; TINY HELPER 10↵5↵,↵↵↵
 ;; --------------------------------------------------------------------------------
 ;; 5,6,7,8,9,10
 
 ;; ================================================================================
 ;; m5 10*xx or m5 10* x x or m5 10(* x x)
+;; TINY HELPER 10↵5↵↵*xx↵↵
 ;; --------------------------------------------------------------------------------
 ;; 25 36 49 64 81 100
 
 ;; ================================================================================
 ;; m10-10x or m0 10(- 10 x) (decrementing numbers)
+;; TINY HELPER 10↵↵↵-10x↵↵
 ;; --------------------------------------------------------------------------------
 ;; 10 9 8 7 6 5 4 3 2 1 0
 
 ;; ================================================================================
 ;; m5 15*xx%x
+;; TINY HELPER 15↵5↵↵*xx↵%x↵
 ;; --------------------------------------------------------------------------------
 ;; 19 24 31 40 51 64 79 90 a9 c4 e1
 
 ;; ================================================================================
 ;; m5 10*xx|0x%x
+;; TINY HELPER 10↵5↵↵*xx↵0x%x↵
 ;; --------------------------------------------------------------------------------
 ;; 0x19 0x24 0x31 0x40 0x51 0x64
 
 ;; ================================================================================
 ;; m25+x?a%c or m25(+ x ?a) | %c
+;; TINY HELPER 25↵↵↵+x?a↵%c↵
 ;; --------------------------------------------------------------------------------
 ;; a b c d e f g h i j k l m n o p q r s t u v w x y z
 
 ;; ================================================================================
 ;; m25+x?A%c or m25(+ x ?A) | %c or m25+x65%c
+;; TINY HELPER 25↵↵↵+x?A↵%c↵
 ;; --------------------------------------------------------------------------------
 ;; A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 
 ;; ================================================================================
 ;; m97,122(string x)
+;; TINY HELPER 122↵97↵,↵stringx↵↵
 ;; --------------------------------------------------------------------------------
 ;; a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
 
 ;; ================================================================================
 ;; m97,122stringxx
+;; TINY HELPER 122↵97↵,↵stringxx↵↵
 ;; --------------------------------------------------------------------------------
 ;; aa,bb,cc,dd,ee,ff,gg,hh,ii,jj,kk,ll,mm,nn,oo,pp,qq,rr,ss,tt,uu,vv,ww,xx,yy,zz
 
 ;; ================================================================================
 ;; m97,120stringxupcasex
+;; TINY HELPER 120↵97↵,↵stringxupcasex↵↵
 ;; --------------------------------------------------------------------------------
 ;; aA,bB,cC,dD,eE,fF,gG,hH,iI,jJ,kK,lL,mM,nN,oO,pP,qQ,rR,sS,tT,uU,vV,wW,xX
 
 ;; ================================================================================
-;; m97,120stringxupcasex)x
+;; m97,120stringxupcasex)x (Note that the parenthesis separating the last two x's.
+;;                          That is required as we do NOT want to upcase the last
+;;                          x too.)
+;; TINY HELPER 120↵97↵,↵stringxupcasex)x↵↵
 ;; --------------------------------------------------------------------------------
 ;; aAa,bBb,cCc,dDd,eEe,fFf,gGg,hHh,iIi,jJj,kKk,lLl,mMm,nNn,oOo,pPp,qQq,rRr,sSs,tTt,uUu,vVv,wWw,xXx
 
 ;; ================================================================================
 ;; m\n;; 10|%(+ x x) and %(* x x) and %s
+;; TINY HELPER 10↵↵\n;; ↵↵%(+ x x) and %(* x x) and %s↵
 ;; --------------------------------------------------------------------------------
 ;; ;; 0 and 0 and 0
 ;; ;; 2 and 1 and 1
@@ -196,11 +215,13 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; ================================================================================
 ;; m10*2+3x or m10(* 2 (+ 3 x))
+;; TINY HELPER 10↵↵↵*2+3x↵↵
 ;; --------------------------------------------------------------------------------
 ;; 6 8 10 12 14 16 18 20 22 24 26
 
 ;; ================================================================================
 ;; m\n;; 10expx
+;; TINY HELPER 10↵↵\n;; ↵expx↵↵
 ;; --------------------------------------------------------------------------------
 ;; ;; 1.0
 ;; ;; 2.718281828459045
@@ -216,6 +237,7 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; ================================================================================
 ;; m5\n;; 20expx%014.2f
+;; TINY HELPER 20↵5↵\n;; ↵expx↵%014.2f↵
 ;; --------------------------------------------------------------------------------
 ;; ;; 00000000148.41
 ;; ;; 00000000403.43
@@ -236,21 +258,26 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; ================================================================================
 ;; m7|%(expt 2 x)
+;; TINY HELPER 7↵↵↵↵%(expt 2 x)↵ (Note that parentheses are required for
+;;                                    sexps in Format)
 ;; --------------------------------------------------------------------------------
 ;; 1 2 4 8 16 32 64 128
 
 ;; ================================================================================
 ;; m, 7|0x%02x
+;; TINY HELPER 7↵↵, ↵↵0x%02x↵
 ;; --------------------------------------------------------------------------------
 ;; 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07
 
 ;; ================================================================================
-;; m10|%0.2f
+;; m9|%0.2f
+;; TINY HELPER ↵↵↵↵%0.2f↵
 ;; --------------------------------------------------------------------------------
-;; 0.00 1.00 2.00 3.00 4.00 5.00 6.00 7.00 8.00 9.00 10.00
+;; 0.00 1.00 2.00 3.00 4.00 5.00 6.00 7.00 8.00 9.00
 
 ;; ================================================================================
 ;; m1\n14|*** TODO http://emacsrocks.com/e%02d.html
+;; TINY HELPER 14↵1↵\n↵↵*** TODO http://emacsrocks.com/e%02d.html↵
 ;; --------------------------------------------------------------------------------
 ;; *** TODO http://emacsrocks.com/e01.html
 ;; *** TODO http://emacsrocks.com/e02.html
@@ -269,6 +296,7 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; ================================================================================
 ;; m1\n10|convert img%s.jpg -monochrome -resize 50%% -rotate 180 img%s_mono.pdf
+;; TINY HELPER 10↵1↵\n↵↵convert img%s.jpg -monochrome -resize 50%% -rotate 180 img%s_mono.pdf↵
 ;; --------------------------------------------------------------------------------
 ;; convert img1.jpg -monochrome -resize 50% -rotate 180 img1_mono.pdf
 ;; convert img2.jpg -monochrome -resize 50% -rotate 180 img2_mono.pdf
@@ -283,11 +311,13 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; ================================================================================
 ;; (setq foo-list '(m1 11+x96|?%c))
+;; (setq foo-list '(TINY HELPER 11↵1↵↵+x96↵?%c))↵
 ;; --------------------------------------------------------------------------------
 ;; (setq foo-list '(?a)) ?b)) ?c)) ?d)) ?e)) ?f)) ?g)) ?h)) ?i)) ?j)) ?k))
 
 ;; ================================================================================
 ;; m1\n10listx+x96|convert img%s.jpg -monochrome -resize 50%% -rotate 180 img%c_mono.pdf
+;; TINY HELPER 10↵1↵\n↵listx+x96↵convert img%s.jpg -monochrome -resize 50%% -rotate 180 img%c_mono.pdf
 ;; --------------------------------------------------------------------------------
 ;; convert img1.jpg -monochrome -resize 50% -rotate 180 imga_mono.pdf
 ;; convert img2.jpg -monochrome -resize 50% -rotate 180 imgb_mono.pdf
@@ -302,6 +332,8 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; ================================================================================
 ;; m1\n10(- (+ x ?A) 1)|;; convert img%(+ x 0).jpg -monochrome -resize 50%% -rotate 180 img%c_mono.pdf
+;; TINY HELPER 10↵1↵\n↵(- (+ x ?A) 1)↵;; convert img%(+ x 0).jpg -monochrome -resize 50%% -rotate 180 img%c_mono.pdf
+;;  - Here parentheses are used in the Lisp Operation section for clarity
 ;; --------------------------------------------------------------------------------
 ;; convert img1.jpg -monochrome -resize 50% -rotate 180 imgA_mono.pdf
 ;; convert img2.jpg -monochrome -resize 50% -rotate 180 imgB_mono.pdf
@@ -316,6 +348,7 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; ================================================================================
 ;; m\n;; 16list*xxx)*xx%s:%s:%s
+;; TINY HELPER 16↵↵\n;; ↵list*xxx)*xx↵%s:%s:%s
 ;; --------------------------------------------------------------------------------
 ;; ;; 0:0:0
 ;; ;; 1:1:1
@@ -337,6 +370,7 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; ================================================================================
 ;; m\n8|**** TODO Learning from Data Week %(+ x 2) \nSCHEDULED: <%(date "Oct 7" (* x 7))> DEADLINE: <%(date "Oct 14" (* x 7))>
+;; TINY HELPER 8↵↵\n;; ↵↵**** TODO Learning from Data Week %(+ x 2) \nSCHEDULED: <%(date "Oct 7" (* x 7))> DEADLINE: <%(date "Oct 14" (* x 7))>
 ;; --------------------------------------------------------------------------------
 ;; **** TODO Learning from Data Week 2
 ;; SCHEDULED: <2015-10-07 Wed> DEADLINE: <2015-10-14 Wed>
@@ -359,6 +393,7 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; ================================================================================
 ;; m\n8%(date "Jan 16" (* x 7))
+;; TINY HELPER 8↵↵\n↵↵%(date "Jan 16" (* x 7))
 ;; --------------------------------------------------------------------------------
 ;; 2015-01-16 Fri
 ;; 2015-01-23 Fri
@@ -372,6 +407,7 @@ Usage: M-x COMMAND    RET   RET   RET        RET    RET -> 0 1 2 3 4 5 6 7 8 9
 
 ;; ================================================================================
 ;; m-7\n0%(date "Jan 16" (* x 7))
+;; TINY HELPER ↵-7↵\n↵↵%(date "Jan 16" (* x 7))
 ;; --------------------------------------------------------------------------------
 ;; 2014-11-28 Fri
 ;; 2014-12-05 Fri
