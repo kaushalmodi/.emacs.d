8a1d7e9f6418c70e779c810b5bf97dcf00962ae7
Kaushal Modi <kaushal.modi@gmail.com>
2015-08-19 14:22:22 -0400

    Tweak the ag call arguments as per my needs

--- setup-files/setup-counsel.el
+++ setup-files/setup-counsel.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-07-28 13:52:12 kmodi>
+;; Time-stamp: <2015-08-18 17:28:23 kmodi>
 
 ;; Counsel (comes packaged with the `swiper' package)
 
@@ -19,6 +19,24 @@
      `(("x"
         (lambda (x) (delete-file (expand-file-name x ivy--directory)))
         ,(propertize "delete" 'face 'font-lock-warning-face))))
+
+    ;; Redefine `counsel-ag-function' with my required options, especially
+    ;; the `--follow' option to allow search through symbolic links
+    (defun counsel-ag-function (string &optional _pred &rest _unused)
+      "Grep in the current directory for STRING."
+      (if (< (length string) 3)
+          (counsel-more-chars 3)
+        (let ((regex (counsel-unquote-regex-parens (ivy--regex string))))
+          (counsel--async-command
+           (format (concat "ag "
+                           "--nogroup "
+                           "--nocolor "
+                           "--skip-vcs-ignores "
+                           "--smart-case "
+                           "--follow " ; follow symlinks
+                           "%S")
+                   regex))
+          nil)))
 
     (bind-keys
      :map modi-mode-map
