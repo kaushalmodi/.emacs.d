9928e8328c243b5e2b5a5572b8da32e83a5789cd
Kaushal Modi <kaushal.modi@gmail.com>
2015-08-19 14:22:51 -0400

    hardcore-mode can be disabled from setup-var-ov..

--- personal/setup-var-overrides-EXAMPLE.el
+++ personal/setup-var-overrides-EXAMPLE.el
@@ -1,15 +1,15 @@
-;; Time-stamp: <2015-06-16 17:11:37 kmodi>
+;; Time-stamp: <2015-08-19 14:10:08 kmodi>
 
 ;; Variable overrides
 
 ;; This file can be used to override variables using `setq' forms.
+
 ;; An example use is to disable loading of selected packages.
 ;;   Format to be used: (setq disable-pkg-PKGNAME t)
 ;;   Example: (setq disable-pkg-pdf-tools t) ; Disable loading of `pdf-tools'
-;; Note that all packages CANNOT be disabled in this manner.
+;; *Note that all packages CANNOT be disabled in this manner.
 
-;; (setq user-personal-directory "~/.emacs.d/personal/")
-
+(setq disable-pkg-hardcore-mode t)
 ;; (setq disable-pkg-pdf-tools t)
 ;; (setq disable-pkg-yasnippet t)
 ;; (setq disable-pkg-ivy t)
@@ -17,6 +17,7 @@
 ;; (setq disable-pkg-fci t)
 ;; (setq disable-pkg-hl-anything t)
 
+;; (setq user-personal-directory "~/.emacs.d/personal/")
 
 ;; Below line *has to be uncommented* for the setup in this file to be effective
 ;; (provide 'setup-var-overrides)
--- setup-files/setup-hardcore.el
+++ setup-files/setup-hardcore.el
@@ -1,8 +1,10 @@
-;; Time-stamp: <2015-02-25 12:38:22 kmodi>
+;; Time-stamp: <2015-08-19 14:08:27 kmodi>
 
 ;; Hardcore mode
+;; https://github.com/magnars/hardcore-mode.el
 
 (use-package hardcore-mode
+  :if (not (bound-and-true-p disable-pkg-hardcore-mode))
   :init
   (progn
     (setq too-hardcore-backspace t)
