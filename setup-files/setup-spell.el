b6f3d68c5d31f2d02b11a637b9045eca903a4aec
Kaushal Modi <kaushal.modi@gmail.com>
2015-08-20 16:18:45 -0400

    Upgrade the advice system

--- setup-files/setup-spell.el
+++ setup-files/setup-spell.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-03-17 09:36:58 kmodi>
+;; Time-stamp: <2015-08-20 16:17:25 kmodi>
 ;;
 ;; Spell check
 ;; ispell, flyspell
@@ -32,52 +32,49 @@
 ;; http://blog.binchen.org/posts/what-s-the-best-spell-check-set-up-in-emacs.html
 
 (use-package ispell
-    :defer 15
-    :config
-    (progn
-      (cond
-        ((executable-find "aspell")
-         (setq ispell-program-name "aspell")
-         (setq ispell-extra-args   '("--sug-mode=ultra"
-                                     "--lang=en_US")))
-        ((executable-find "hunspell")
-         (setq ispell-program-name "hunspell")
-         (setq ispell-extra-args   '("-d en_US"))))
-      ;; Save a new word to personal dictionary without asking
-      (setq ispell-silently-savep t)
+  :defer 15
+  :config
+  (progn
+    (cond
+     ((executable-find "aspell")
+      (setq ispell-program-name "aspell")
+      (setq ispell-extra-args   '("--sug-mode=ultra"
+                                  "--lang=en_US")))
+     ((executable-find "hunspell")
+      (setq ispell-program-name "hunspell")
+      (setq ispell-extra-args   '("-d en_US"))))
+    ;; Save a new word to personal dictionary without asking
+    (setq ispell-silently-savep t)
 
-      (use-package flyspell
-          :config
-        (progn
-          (setq flyspell-use-meta-tab nil)
-          ;; Stop flyspell overriding other key bindings
-          (define-key flyspell-mode-map (kbd "C-;") nil)
-          (define-key flyspell-mode-map (kbd "C-,") nil)
-          (define-key flyspell-mode-map (kbd "C-.") nil)
-          (define-key flyspell-mode-map "\M-\t"     nil)
+    (use-package flyspell
+      :config
+      (progn
+        (setq flyspell-use-meta-tab nil)
+        ;; Stop flyspell overriding other key bindings
+        (define-key flyspell-mode-map (kbd "C-;") nil)
+        (define-key flyspell-mode-map (kbd "C-,") nil)
+        (define-key flyspell-mode-map (kbd "C-.") nil)
+        (define-key flyspell-mode-map "\M-\t"     nil)
 
-          (add-hook 'prog-mode-hook #'flyspell-prog-mode)
-          (with-eval-after-load 'auto-complete
-            (ac-flyspell-workaround))
-          ;; https://github.com/larstvei/dot-emacs#flyspell
-          (add-hook 'text-mode-hook #'turn-on-flyspell)
-          (add-hook 'org-mode-hook  #'turn-on-flyspell)
+        (add-hook 'prog-mode-hook #'flyspell-prog-mode)
+        (with-eval-after-load 'auto-complete
+          (ac-flyspell-workaround))
+        ;; https://github.com/larstvei/dot-emacs#flyspell
+        (add-hook 'text-mode-hook #'turn-on-flyspell)
+        (add-hook 'org-mode-hook  #'turn-on-flyspell)
 
-          ;; Flyspell signals an error if there is no spell-checking tool is installed.
-          ;; We can advice turn-on=flyspell and flyspell-prog-mode to only try to enable
-          ;; flyspell if a spell-checking tool is available.
-          (defadvice turn-on-flyspell (around check nil activate)
-            "Turns on flyspell only if a spell-checking tool is installed."
-            (when (executable-find ispell-program-name)
-              ad-do-it))
-          (defadvice flyspell-prog-mode (around check nil activate)
-            "Turns on flyspell only if a spell-checking tool is installed."
-            (when (executable-find ispell-program-name)
-              ad-do-it))
+        ;; Flyspell signals an error if there is no spell-checking tool is
+        ;; installed. We can advice `turn-on-flyspell' and `flyspell-prog-mode'
+        ;; to try to enable flyspell only if a spell-checking tool is available.
+        (defun modi/ispell-not-avail-p (&rest args)
+          "Return `nil' if `ispell-program-name' is available; `t' otherwise."
+          (not (executable-find ispell-program-name)))
+        (advice-add 'turn-on-flyspell   :before-until #'modi/ispell-not-avail-p)
+        (advice-add 'flyspell-prog-mode :before-until #'modi/ispell-not-avail-p)
 
-          (bind-keys
-           :map modi-mode-map
-           ("<f12>" . flyspell-auto-correct-previous-word))))))
+        (bind-keys
+         :map modi-mode-map
+          ("<f12>" . flyspell-auto-correct-previous-word))))))
 
 
 (provide 'setup-spell)
