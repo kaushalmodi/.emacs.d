046cc9daeea30b68426160c13699ad3e6da502de
Kaushal Modi <kaushal.modi@gmail.com>
2015-09-18 12:36:13 -0400

    Major which-key setup update
    - Add documentation for various which-key customizations
    - Replace the sort of deprecated
      which-key-key-based-description-replacement-alist variable with the
      use of which-key-add-key-based-replacements function.
    - Update which-key-description-replacement-alist
    - Customization the highlighting of selected commands.

--- setup-files/setup-which-key.el
+++ setup-files/setup-which-key.el
@@ -1,15 +1,18 @@
-;; Time-stamp: <2015-07-28 09:20:45 kmodi>
+;; Time-stamp: <2015-09-18 12:42:58 kmodi>
 
 ;; Which Key
 ;; https://github.com/justbur/emacs-which-key
 
 (use-package which-key
-  :defer 15
+  :defer 10
   :config
   (progn
     (setq which-key-popup-type 'side-window) ; default
     ;; (setq which-key-popup-type 'minibuffer)
 
+    ;; Replacements for how KEY is replaced when which-key displays
+    ;;   KEY → FUNCTION
+    ;; Eg: After "C-c", display "right → winner-redo" as "▶ → winner-redo"
     (setq which-key-key-replacement-alist
           '(("<\\([[:alnum:]-]+\\)>" . "\\1")
             ("left"                . "◀")
@@ -21,6 +24,35 @@
             ("next"                . "PgDn")
             ("prior"               . "PgUp")))
 
+    ;; Replacements for how part or whole of FUNCTION is replaced when
+    ;; which-key displays
+    ;;   KEY → FUNCTION
+    ;; Eg: After "d" in `calc', display "6 → calc-hex-radix" as "6 → 🖩hex-radix"
+    (setq which-key-description-replacement-alist
+          '(("Prefix Command" . "prefix")
+            ("which-key-show-next-page" . "wk next pg")
+            ("\\`calc-" . "") ; Hide "calc-" prefixes when listing M-x calc keys
+            ("/body\\'" . "") ; Remove display the "/body" portion of hydra fn names
+            ("modi/" . "m/") ; The car is intentionally not "\\`modi/"
+            ("\\`hydra-" . "+h/")))
+    ;; Use cool unicode characters if available
+    (with-eval-after-load 'setup-symbola
+      (when font-symbola-p
+        (add-to-list 'which-key-description-replacement-alist '("\\`calc-" . "🖩"))))
+
+    ;; Change what string to display for a given *complete* key binding
+    ;; Eg: After "C-x", display "8 → +unicode" instead of "8 → +prefix"
+    (which-key-add-key-based-replacements
+      "C-x 8"   "unicode"
+      "C-x a"   "abbrev/expand"
+      "C-x r"   "rect/reg"
+      "C-x w"   "hi-lock-map"
+      "C-c /"   "engine-mode-map"
+      "C-c C-v" "org-babel"
+      "C-x 8 0" "ZWS")
+
+    ;; List of "special" keys for which a KEY is displayed as just K but with
+    ;; "inverted video" face.
     (setq which-key-special-keys '("SPC"
                                    "TAB"
                                    "RET"
@@ -28,27 +60,17 @@
                                    "BS" ; backspace key
                                    "ESC"))
 
-    (setq which-key-key-based-description-replacement-alist
-          '(("C-x 8"   . "unicode")
-            ("C-x a"   . "abbrev/expand")
-            ("C-x r"   . "rect/reg")
-            ("C-x w"   . "hi-lock-map")
-            ("C-c /"   . "engine-mode-map")
-            ("C-c C-v" . "org-babel")
-            ("C-x 8 0" . "ZWS")))
+    ;; Highlight certain commands
+    (defface modi/wk-highlight-modi-face
+      '((t . (:inherit which-key-command-description-face :foreground "indian red")))
+      "Face for highlighting commands starting with \"modi/\".")
 
-    (with-eval-after-load 'setup-symbola
-      (if font-symbola-p
-          (setq which-key-description-replacement-alist
-                '(("Prefix Command" . "prefix")
-                  ("which-key-show-next-page" . "wk next pg")
-                  ;; Hide the "calc-" prefixes when listing keys for M-x calc
-                  ("\\`calc-" . "🖩")))
-        (setq which-key-description-replacement-alist
-              '(("Prefix Command" . "prefix")
-                ("which-key-show-next-page" . "wk next pg")
-                ;; Hide the "calc-" prefixes when listing keys for M-x calc
-                ("\\`calc-" . "")))))
+    (setq which-key-highlighted-command-list
+          '(("\\`hydra-" . which-key-group-description-face)
+            ("\\`modi/" . modi/wk-highlight-modi-face)
+            ;; Highlight using the default `which-key-highlighted-command-face'
+            "\\(rectangle-\\)\\|\\(-rectangle\\)"
+            "\\`org-"))
 
     (which-key-mode 1)))
 
