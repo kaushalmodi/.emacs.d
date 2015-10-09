6d94bb3e2cc5da99bfa23928bcaa49ea305b3e64
Kaushal Modi <kaushal.modi@gmail.com>
2015-10-09 13:17:16 -0400

    Rearrange the code; use :bind in use-package

--- setup-files/setup-iregister.el
+++ setup-files/setup-iregister.el
@@ -1,17 +1,22 @@
-;; Time-stamp: <2015-07-09 13:11:44 kmodi>
+;; Time-stamp: <2015-10-08 18:15:57 kmodi>
 
 ;; IRegister (Interactive Register)
 ;; https://github.com/atykhonov/iregister.el
 
-(defalias 'my/iregister-copy          'iregister-point-or-text-to-register-kill-ring-save)
-(defalias 'my/iregister-cut           'iregister-copy-to-register-kill)
-(defalias 'my/iregister-copy-append   'iregister-append-to-latest-register)
-(defalias 'my/iregister-delete-append 'iregister-append-to-latest-register-delete)
-
 (use-package iregister
+  :preface
+  (progn
+    (defalias 'my/iregister-copy          'iregister-point-or-text-to-register-kill-ring-save)
+    (defalias 'my/iregister-cut           'iregister-copy-to-register-kill)
+    (defalias 'my/iregister-copy-append   'iregister-append-to-latest-register)
+    (defalias 'my/iregister-delete-append 'iregister-append-to-latest-register-delete))
+  :bind (;; If region is active then `my/iregister-copy' will store the selected
+         ;; text to any empty register, otherwise it stores the point.
+         ("C-c w"   . my/iregister-copy) ; Works like `M-w'
+         ("C-c W"   . my/iregister-cut) ; Works like `C-w'
+         ("C-x r a" . hydra-append/body))
   :config
   (progn
-
     (defun my/iregister-copy-append-sep (start end &optional separator)
       (interactive "r\nsSeparator: ")
       (if (region-active-p)
@@ -62,13 +67,6 @@ Cut  and Append        _x._        _xs_         _xn_
       ("xs" (lambda (b e) (interactive "r") (my/iregister-delete-append-sep b e " ")))
       ("xn" (lambda (b e) (interactive "r") (my/iregister-delete-append-sep b e "\n")))
       ("q"  nil "cancel" :color blue))
-
-    (bind-keys
-     ;; If region is active then `my/iregister-copy' will store the selected
-     ;; text to any empty register, otherwise it stores the point.
-     ("C-c w"   . my/iregister-copy) ; Works like `M-w'
-     ("C-c W"   . my/iregister-cut) ; Works like `C-w'
-     ("C-x r a" . hydra-append/body))
 
     (bind-to-modi-map "i" #'iregister-latest-text)))
 
