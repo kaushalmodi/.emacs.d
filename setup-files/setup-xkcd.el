bc4eb466618ec53f5fd68c24f2e290cbcbdf0193
Kaushal Modi <kaushal.modi@gmail.com>
2015-10-09 17:20:22 -0400

    Revamp xkcd setup :)

--- setup-files/setup-xkcd.el
+++ setup-files/setup-xkcd.el
@@ -1,24 +1,42 @@
-;; Time-stamp: <2015-05-22 11:51:49 kmodi>
+;; Time-stamp: <2015-10-09 17:19:39 kmodi>
 
 ;; xkcd
+;; https://github.com/vibhavp/emacs-xkcd
 
 (use-package xkcd
   :commands (xkcd)
+  :init
+  (progn
+    (setq xkcd-cache-dir (let ((dir (concat user-emacs-directory
+                                            "xkcd/"))) ; must end with /
+                           (make-directory dir :parents)
+                           dir)))
   :config
   (progn
-    ;; create the xkcd directory if it doesn't exist
-    (setq xkcd-dir (concat user-emacs-directory "xkcd/")) ; must end with /
-    (unless (file-exists-p xkcd-dir)
-      (make-directory xkcd-dir))
     (bind-keys
      :map xkcd-mode-map
-      ("C-p" . xkcd-prev)
-      ("C-n" . xkcd-next)
-      ("r"   . xkcd-rand)
-      ("t"   . xkcd-alt-text)
-      ("q"   . xkcd-kill-buffer)
-      ("o"   . xkcd-open-browser)
-      ("e"   . xkcd-open-explanation-browser))))
+      ("/" . xkcd-get)
+      ("g" . xkcd-get-latest)
+      ("c" . xkcd-get-latest-cached)
+      ("p" . xkcd-prev)
+      ("n" . xkcd-next)
+      ("v" . xkcd-open-browser)
+      ("?" . xkcd-open-explanation-browser))))
 
 
 (provide 'setup-xkcd)
+
+;; |---------+--------------------------------|
+;; | Binding | Description                    |
+;; |---------+--------------------------------|
+;; | /       | Go to a specific comic         |
+;; | g       | Get the latest comic (refresh) |
+;; | c       | Get the latest cached comic    |
+;; | p       | Previous comic                 |
+;; | n       | Next comic                     |
+;; | r       | Random                         |
+;; | v       | Open in browser                |
+;; | ?       | Explain the comic              |
+;; | t       | Show alt text                  |
+;; | q       | Quit                           |
+;; |---------+--------------------------------|
