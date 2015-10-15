e6eb294b5ebcbfe8d062904d5f550c136788a1e0
Kaushal Modi <kaushal.modi@gmail.com>
2015-10-15 12:58:53 -0400

    Remove the hydra-paradox-filter definition
    as it is now part of the source, paradox-menu.el
    
    https://github.com/Malabarba/paradox/commit/5d57c3e0b53bf7df316f17803612df8eb703400a

--- setup-files/setup-paradox.el
+++ setup-files/setup-paradox.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-10-14 16:24:24 kmodi>
+;; Time-stamp: <2015-10-15 12:58:33 kmodi>
 
 ;; Paradox
 ;; https://github.com/Malabarba/paradox
@@ -15,30 +15,6 @@
 
     (setq paradox-lines-per-entry 1)
     (setq paradox-automatically-star t)
-
-    (define-key paradox--filter-map "k" nil) ; Unbind redundant key
-
-    (defhydra hydra-paradox-filter (:color blue
-                                    :hint nil)
-      "
-_f_ilter                 filter _r_egexp            _i_nstalled      _d_ependencies
-g_n_u elpa packages      available _u_pgrades       _a_vailable      _c_lear filter
-_m_elpa packages         packages with _s_tars      _b_uilt-in
-"
-      ("f" package-menu-filter)
-      ("n" (package-menu-filter "arc:gnu"))
-      ("m" (package-menu-filter "arc:melpa"))
-      ("r" paradox-filter-regexp)
-      ("u" paradox-filter-upgrades)
-      ("s" paradox-filter-stars)
-      ("i" (package-menu-filter "status:installed"))
-      ("a" (package-menu-filter "status:available"))
-      ("b" (package-menu-filter "status:built-in"))
-      ("d" (package-menu-filter "status:dependency"))
-      ("c" paradox-filter-clear)
-      ("g" paradox-filter-clear)
-      ("q" nil "cancel" :color blue))
-    (bind-key "f" #'hydra-paradox-filter/body paradox-menu-mode-map)
 
     (paradox-enable)))
 
