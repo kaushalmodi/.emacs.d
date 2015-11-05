9f7650af65b8ee3e95ebeeffac3eb45f83283c93
Kaushal Modi <kaushal.modi@gmail.com>
2015-11-05 13:15:38 -0500

    Add edit-server package setup for Windows

--- setup-files/setup-server.el
+++ setup-files/setup-server.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-11-05 12:12:04 kmodi>
+;; Time-stamp: <2015-11-05 13:14:57 kmodi>
 
 ;; server/daemon setup
 
@@ -19,7 +19,35 @@
       ;;   On Windows, also set the EMACS_SERVER_FILE environment variable to
       ;; point to the `server' file. For example, for emacs 25.0, that location
       ;; would be "PATH\TO\.emacs.d\server_25_0\server".
-      (defun server-ensure-safe-dir (dir) "Noop" t))
+      (defun server-ensure-safe-dir (dir) "Noop" t)
+
+      ;; http://www.emacswiki.org/emacs/Edit_with_Emacs
+      ;; `edit-server' package to edit the text boxes in Chrome browser using
+      ;; emacsclient
+      (use-package edit-server
+        :ensure t
+        :config
+        (progn
+          ;; Don't pop up a new frame
+          (setq edit-server-new-frame nil)
+          (setq edit-server-default-major-mode 'org-mode)
+          (setq edit-server-url-major-mode-alist
+                '(("github\\.com" . markdown-mode)
+                  ("stackexchange\\.com" . markdown-mode)
+                  ("stackoverflow\\.com" . markdown-mode)
+                  ("reddit\\.com" . markdown-mode)))
+
+          ;; Integration with Gmail
+          (use-package edit-server-htmlize
+            :ensure t
+            :commands (edit-server-maybe-dehtmlize-buffer
+                       edit-server-maybe-htmlize-buffer)
+            :config
+            (progn
+              (add-hook 'edit-server-start-hook #'edit-server-maybe-dehtmlize-buffer)
+              (add-hook 'edit-server-done-hook  #'edit-server-maybe-htmlize-buffer)))
+
+          (edit-server-start))))
 
     ;; Start a server only if one is not already running
     ;; `server-running-p' returns "t" if a server is already running
@@ -36,4 +64,4 @@ otherwise do nothing.")
           (server-start))))))
 
 
-  (provide 'setup-server)
+(provide 'setup-server)
