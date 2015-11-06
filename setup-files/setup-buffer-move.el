8cd7417c23f5d54369c884816092b9fa78c4dedd
Kaushal Modi <kaushal.modi@gmail.com>
2015-11-06 11:02:56 -0500

    Make “move” the dflt behav for buf-move cmds
    - Also added wrapper functions to pick either the 'swap or 'move
      behavior

--- setup-files/setup-buffer-move.el
+++ setup-files/setup-buffer-move.el
@@ -1,12 +1,49 @@
-;; Time-stamp: <2015-06-15 17:08:47 kmodi>
+;; Time-stamp: <2015-11-06 11:01:35 kmodi>
+
+;; https://github.com/lukhas/buffer-move
 
 (use-package buffer-move
+  :commands (modi/buf-move-left
+             modi/buf-move-right
+             modi/buf-move-up
+             modi/buf-move-down)
+  :init
+  (progn
+    (bind-to-modi-map "," #'modi/buf-move-left)
+    (bind-to-modi-map "." #'modi/buf-move-right)
+    (bind-to-modi-map "k" #'modi/buf-move-up)
+    (bind-to-modi-map "j" #'modi/buf-move-down))
   :config
   (progn
-    (bind-to-modi-map "," #'buf-move-left)
-    (bind-to-modi-map "." #'buf-move-right)
-    (bind-to-modi-map "k" #'buf-move-up)
-    (bind-to-modi-map "K" #'buf-move-down)))
+    (setq buffer-move-behavior 'swap) ; values = 'swap (default), 'move
+
+    (defun modi/buf-move-left (swap)
+      "Calls `buf-move-left' with the “move” behavior.
+If SWAP is non-nil, the default “swap” behavior is used."
+      (interactive "P")
+      (let ((buffer-move-behavior (if swap 'swap 'move)))
+        (buf-move-left)))
+
+    (defun modi/buf-move-right (swap)
+      "Calls `buf-move-right' with the “move” behavior.
+If SWAP is non-nil, the default “swap” behavior is used."
+      (interactive "P")
+      (let ((buffer-move-behavior (if swap 'swap 'move)))
+        (buf-move-right)))
+
+    (defun modi/buf-move-up (swap)
+      "Calls `buf-move-up' with the “move” behavior.
+If SWAP is non-nil, the default “swap” behavior is used."
+      (interactive "P")
+      (let ((buffer-move-behavior (if swap 'swap 'move)))
+        (buf-move-up)))
+
+    (defun modi/buf-move-down (swap)
+      "Calls `buf-move-down' with the “move” behavior.
+If SWAP is non-nil, the default “swap” behavior is used."
+      (interactive "P")
+      (let ((buffer-move-behavior (if swap 'swap 'move)))
+        (buf-move-down)))))
 
 
 (provide 'setup-buffer-move)
