e225d97e137c8b0ef7d5bdafa0602c705c9b515a
Kaushal Modi <kaushal.modi@gmail.com>
2015-11-09 13:30:49 -0500

    Autoload manage-minor-mode pkg

--- setup-files/setup-manage-minor-mode.el
+++ setup-files/setup-manage-minor-mode.el
@@ -1,9 +1,11 @@
-;; Time-stamp: <2015-03-03 09:22:27 kmodi>
+;; Time-stamp: <2015-11-09 13:30:07 kmodi>
 
 ;; manage-minor-mode
 ;; https://github.com/ShingoFukuyama/manage-minor-mode
 
-(use-package manage-minor-mode)
+(use-package manage-minor-mode
+  :commands (manage-minor-mode
+             manage-minor-mode-bals))
 
 
 (provide 'setup-manage-minor-mode)
