61dde47f2d4f67d98587bedbb51a789a1e332eca
Kaushal Modi <kaushal.modi@gmail.com>
2016-01-05 14:23:07 -0500

    Customize the now avail counsel-ag-base-command

--- setup-files/setup-counsel.el
+++ setup-files/setup-counsel.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-09-29 11:42:59 kmodi>
+;; Time-stamp: <2016-01-05 14:22:55 kmodi>
 
 ;; Counsel (comes packaged with the `swiper' package)
 
@@ -14,32 +14,22 @@
            ;; file names ending with # or ~
            "\\|\\(?:\\`.+?[#~]\\'\\)"))
 
+    ;; Redefine `counsel-ag-base-command' with my required options, especially
+    ;; the `--follow' option to allow search through symbolic links.
+    (setq counsel-ag-base-command (concat "ag "
+                                          "--noheading "
+                                          "--nogroup "
+                                          "--nocolor "
+                                          "--skip-vcs-ignores "
+                                          "--smart-case "
+                                          "--follow " ; follow symlinks
+                                          "%S"))
+
     (ivy-set-actions
      'counsel-find-file
      `(("x"
         (lambda (x) (delete-file (expand-file-name x ivy--directory)))
         ,(propertize "delete" 'face 'font-lock-warning-face))))
-
-    ;; Redefine `counsel-ag-function' with my required options, especially
-    ;; the `--follow' option to allow search through symbolic links
-    (defun counsel-ag-function (string &optional _pred &rest _unused)
-      "Grep in the current directory for STRING."
-      (if (< (length string) 3)
-          (counsel-more-chars 3)
-        (let ((regex (counsel-unquote-regex-parens
-                      (setq ivy--old-re
-                            (ivy--regex string)))))
-          (counsel--async-command
-           (format (concat "ag "
-                           "--noheading "
-                           "--nogroup "
-                           "--nocolor "
-                           "--skip-vcs-ignores "
-                           "--smart-case "
-                           "--follow " ; follow symlinks
-                           "%S")
-                   regex))
-          nil)))
 
     (bind-keys
      :map modi-mode-map
