34dfeff044bf90934ff591d3652d8f4f7553f138
Kaushal Modi <kaushal.modi@gmail.com>
2016-02-17 10:56:51 -0500

    Add git-link support for emacs source code
    - for git.savannah.gnu.org repos

--- setup-files/setup-git-link.el
+++ setup-files/setup-git-link.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2016-02-05 16:30:43 kmodi>
+;; Time-stamp: <2016-02-17 11:00:18 kmodi>
 
 ;; Git Link
 ;; https://github.com/sshaw/git-link
@@ -13,7 +13,27 @@
 with the commit hash in the link even if branch name is available."
       (interactive)
       (let ((git-link-use-commit t))
-        (call-interactively 'git-link)))))
+        (call-interactively 'git-link)))
+
+    ;; Add support for git.savannah.gnu.org
+    (add-to-list 'git-link-remote-alist '("git.savannah.gnu.org" git-link-savannah-gnu))
+    (add-to-list 'git-link-commit-remote-alist '("git.savannah.gnu.org" git-link-commit-savannah-gnu))
+
+    (defun git-link-savannah-gnu (hostname dirname filename branch commit start end)
+      ;; Example: http://git.savannah.gnu.org/cgit/emacs.git/tree/lisp/README?id=d4b93e11a519cf71beca69654fda158d01a26c3b#n1
+      (format "http://%s/cgit/%s/tree/%s?id=%s%s"
+              hostname
+              (replace-regexp-in-string "^r/\\(.*\\)" "\\1.git" dirname)
+              filename
+              (or branch commit)
+              (format "#n%s" start)))
+
+    (defun git-link-commit-savannah-gnu (hostname dirname commit)
+      ;; Example: http://git.savannah.gnu.org/cgit/emacs.git/commit/?id=2abcb06cab03cf9040348146fcc0e3e93ae24a58
+      (format "http://%s/cgit/%s/commit/?id=%s"
+              hostname
+              (replace-regexp-in-string "^r/\\(.*\\)" "\\1.git" dirname)
+              commit))))
 
 
 (provide 'setup-git-link)
