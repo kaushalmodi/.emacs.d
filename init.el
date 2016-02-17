b84da12581c8c19ca015f29edb7eac04cdaca4b4
Kaushal Modi <kaushal.modi@gmail.com>
2016-02-17 17:46:04 -0500

    Code cleanup #major
    - Use defvar/defconst.. don't use setq to initialize vars
    - No need to require 'cl and 'cl-lib
    - Earlier defuns library is now general library
    - Load general library in the very beginning so that macros like ">=e"
      defined in it can be used in setup-packages.el
    - Do *not* require 'setup-ido. It's not needed any more as I am now
      using ivy. Also removed ido & Co packages from the my-packages variable.

--- init.el
+++ init.el
@@ -1,169 +1,170 @@
-;; Time-stamp: <2016-02-17 13:46:36 kmodi>
+;; Time-stamp: <2016-02-17 17:45:44 kmodi>
 ;; Author: Kaushal Modi
 
 ;; Global variables
 ;; https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
-(setq gc-cons-threshold--orig gc-cons-threshold)
+(defvar gc-cons-threshold--orig gc-cons-threshold)
 (setq gc-cons-threshold (* 100 1024 1024)) ; 100 MB before garbage collection
 
-(setq user-home-directory     (concat (getenv "HOME") "/")) ; must end with /
-(setq user-emacs-directory    (concat user-home-directory ".emacs.d/")) ; must end with /
-(setq emacs-version-short     (replace-regexp-in-string
-                               "\\([0-9]+\\)\\.\\([0-9]+\\).*"
-                               "\\1_\\2" emacs-version)) ; 25.0.50.1 -> 25_0
-(setq org-directory           (let ((dir (concat user-home-directory
-                                                 "org/"))) ; must end with /
-                                (make-directory dir :parents)
-                                dir))
-(setq setup-packages-file     (locate-user-emacs-file "setup-packages.el"))
-(setq custom-file             (locate-user-emacs-file
-                               (concat "custom_" emacs-version-short ".el")))
-(setq user-personal-directory (let ((dir (concat user-emacs-directory
-                                                 "personal/"))) ; must end with /
-                                (make-directory dir :parents)
-                                dir))
+(defvar user-home-directory (concat (getenv "HOME") "/")) ; must end with /
 
-;; A list of packages to ensure are installed at launch
-(setq my-packages
-      '(
-        ;; git-gutter git-gutter-fringe git-gutter+ git-gutter-fringe+ ; < diff-hl
-        ;; popwin ; < shackle
-        ;; ox-reveal ; used to export to HTML slides; using latest from git
-        ;; helm helm-swoop ; < swiper
-        ;; helm-gtags ; < ggtags
-        ace-window
-        adaptive-wrap ; indented line wrapping
-        ag wgrep wgrep-ag s ; ag > ack > grep
+(defvar emacs-version-short (format "%s_%s"
+                                    emacs-major-version emacs-minor-version)
+  "A variable to store the current emacs versions as <MAJORVER>_<MINORVER>.
+So, for emacs version 25.0.50.1, this variable will be 25_0.")
+
+(defvar user-personal-directory (let ((dir (concat user-emacs-directory
+                                                   "personal/"))) ; must end with /
+                                  (make-directory dir :parents)
+                                  dir)
+  "User's personal directory to contain non-git-controlled files.")
+
+(setq user-emacs-directory (concat user-home-directory ".emacs.d/")) ; must end with /
+(setq org-directory (let ((dir (concat user-home-directory
+                                       "org/"))) ; must end with /
+                      (make-directory dir :parents)
+                      dir))
+(setq custom-file (locate-user-emacs-file
+                   (concat "custom_" emacs-version-short ".el")))
+
+(defconst my-packages
+  '(ace-window
+    adaptive-wrap ; indented line wrapping
+    ag wgrep wgrep-ag s ; ag > ack > grep
                                         ; wgrep+wgrep-ag allow editing files
                                         ; directly in ag buffer
-        aggressive-indent
-        all all-ext ; edit ALL lines matching regex
-        anzu   ; shows total search hits in mode line, > query-replace
-        ascii-art-to-unicode
-        auto-complete fuzzy
-        auto-highlight-symbol
-        avy ; > ace-jump-mode
-        beacon ; visual flash to show the cursor position
-        benchmark-init
-        bm ; visual bookmarks
-        bookmark+ ; able to bookmark desktop sessions
-        buffer-move
-        command-log-mode ; for logging commands; useful when demoing emacs
-        cperl-mode
-        csv-nav ; editing csv files
-        deft ; quick note taking and management
-        diff-hl
-        dired-single dired+
-        discover-my-major ; Discover key bindings for the major mode
-        drag-stuff
-        easy-escape ; Make the \\ escape chars more pleasant looking in elisp regexps
-        elfeed
-        elisp-slime-nav ; tag based code navigation for elisp; works even for compressed code
-        engine-mode ; search engines
-        expand-region
-        eww-lnum ; jump to links in eww buffer ace-jump style
-        fill-column-indicator
-        fold-this
-        gist
-        git-link ; get git links with line numbers and commit-hash/branch
-        git-timemachine ; walk through git revisions
-        ggtags etags-select etags-table ctags-update
-        hardcore-mode
-        header2
-        help-fns+
-        hideshow-org hideshowvis
-        htmlize
-        hl-line+
-        hungry-delete
-        hydra
-        ibuffer-projectile
-        ido-vertical-mode flx-ido ido-ubiquitous
-        imenu-list
-        indent-guide
-        info+
-        interleave ; takes notes associated to pdf files in org mode
-        iregister ; Interactive access to registers
-        isend-mode ; used in setup-perl.el
-        iy-go-to-char ; Go to next char which is similar to "f" and "t" in vim
-        key-chord ; map pairs of simultaneously pressed keys to commands
-        keyfreq ; find which commands you use the most
-        kurecolor ; library to tweak colors
-        linum-relative
-        list-environment
-        magit ; for git management
-        manage-minor-mode
-        markdown-mode
-        minibuffer-line
-        multi-term
-        multiple-cursors
-        neotree
-        nlinum ; reviews say it's better than linum
-        org-cliplink ; paste copied links as well-formatted org-mode links with desc
-        org-plus-contrib ; latest stable version of org-mode, includes org-eww
-        org-tree-slide
-        outorg navi-mode ; supporting packages for outshine
-        outshine ; org-mode navigation and organization outside org-mode
-        ox-twbs ; export to twitter bootstrap html
-        paradox ; package menu improvements
-        page-break-lines ; Convert the ^L (form feed) chars to horizontal lines
-        pomodoro
-        poporg ; edit comments from any other mode in org mode
-        projectile ; Better than fiplr
-        rainbow-delimiters
-        rainbow-mode
-        region-bindings-mode ; complements really well with multiple-cursors
-        rectangle-utils
-        rpn-calc
-        shackle
-        smart-compile
-        smart-mark
-        smart-mode-line popup rich-minority
-        smex ; smart M-x
-        stripe-buffer
-        sunshine forecast ; weather
-        swiper counsel
-        sx
-        tiny
-        transpose-frame ; for the priceless `rotate-frame' and `transpose-frame'
-        undo-tree ; supercool undo visualization
-        use-package use-package-chords ; optimize package loading
-        visual-regexp
-        volatile-highlights
-        web-mode
-        which-key ; > guide-key
-        wordnut ; offline dictionary (requires installing Wordnet wn application)
-        wrap-region ; wrap selection with punctuations, tags (org-mode, markdown-mode, ..)
-        writegood-mode ; highlight passive voice, weasel words and duplicates
-        xkcd ; comic
-        yafolding ; indentation detected code folding
-        yaml-mode ; Useful for editing Octopress' _config.yml
-        yasnippet
-        zop-to-char
+    aggressive-indent
+    all all-ext ; edit ALL lines matching regex
+    anzu   ; shows total search hits in mode line, > query-replace
+    ascii-art-to-unicode
+    auto-complete fuzzy
+    auto-highlight-symbol
+    avy ; > ace-jump-mode
+    beacon ; visual flash to show the cursor position
+    benchmark-init
+    bm ; visual bookmarks
+    bookmark+ ; able to bookmark desktop sessions
+    buffer-move
+    command-log-mode ; for logging commands; useful when demoing emacs
+    cperl-mode
+    csv-nav ; editing csv files
+    deft ; quick note taking and management
+    diff-hl
+    dired-single dired+
+    discover-my-major ; Discover key bindings for the major mode
+    drag-stuff
+    easy-escape ; Make the \\ escape chars more pleasant looking in elisp regexps
+    elfeed
+    elisp-slime-nav ; tag based code navigation for elisp; works even for compressed code
+    engine-mode ; search engines
+    expand-region
+    eww-lnum ; jump to links in eww buffer ace-jump style
+    fill-column-indicator
+    fold-this
+    gist
+    git-link ; get git links with line numbers and commit-hash/branch
+    git-timemachine ; walk through git revisions
+    ggtags etags-select etags-table ctags-update
+    hardcore-mode
+    header2
+    help-fns+
+    hideshow-org hideshowvis
+    htmlize
+    hl-line+
+    hungry-delete
+    hydra
+    ibuffer-projectile
+    imenu-list
+    indent-guide
+    info+
+    interleave ; takes notes associated to pdf files in org mode
+    iregister ; Interactive access to registers
+    isend-mode ; used in setup-perl.el
+    iy-go-to-char ; Go to next char which is similar to "f" and "t" in vim
+    key-chord ; map pairs of simultaneously pressed keys to commands
+    keyfreq ; find which commands you use the most
+    kurecolor ; library to tweak colors
+    linum-relative
+    list-environment
+    magit ; for git management
+    manage-minor-mode
+    markdown-mode
+    minibuffer-line
+    multi-term
+    multiple-cursors
+    neotree
+    nlinum ; reviews say it's better than linum
+    org-cliplink ; paste copied links as well-formatted org-mode links with desc
+    org-plus-contrib ; latest stable version of org-mode, includes org-eww
+    org-tree-slide
+    outorg navi-mode ; supporting packages for outshine
+    outshine ; org-mode navigation and organization outside org-mode
+    ox-twbs ; export to twitter bootstrap html
+    paradox ; package menu improvements
+    page-break-lines ; Convert the ^L (form feed) chars to horizontal lines
+    pomodoro
+    poporg ; edit comments from any other mode in org mode
+    projectile ; Better than fiplr
+    rainbow-delimiters
+    rainbow-mode
+    region-bindings-mode ; complements really well with multiple-cursors
+    rectangle-utils
+    rpn-calc
+    shackle
+    smart-compile
+    smart-mark
+    smart-mode-line popup rich-minority
+    smex ; smart M-x
+    stripe-buffer
+    sunshine forecast ; weather
+    swiper counsel
+    sx
+    tiny
+    transpose-frame ; for the priceless `rotate-frame' and `transpose-frame'
+    undo-tree ; supercool undo visualization
+    use-package use-package-chords ; optimize package loading
+    visual-regexp
+    volatile-highlights
+    web-mode
+    which-key ; > guide-key
+    wordnut ; offline dictionary (requires installing Wordnet wn application)
+    wrap-region ; wrap selection with punctuations, tags (org-mode, markdown-mode, ..)
+    writegood-mode ; highlight passive voice, weasel words and duplicates
+    xkcd ; comic
+    yafolding ; indentation detected code folding
+    yaml-mode ; Useful for editing Octopress' _config.yml
+    yasnippet
+    zop-to-char
 
-        ;; Themes
-        ;; zenburn-theme ; < forked version
-        ;; smyx-theme ; < forked version
-        ample-theme ; ample, ample-flat, ample-light
-        darktooth-theme ; coffee
-        leuven-theme ; awesome white background theme
-        planet-theme ; dark blue
-        tao-theme ; monochrome
-        twilight-bright-theme
-        twilight-anti-bright-theme
-        ))
+    ;; Themes
+    ;; zenburn-theme ; < forked version
+    ;; smyx-theme ; < forked version
+    ample-theme ; ample, ample-flat, ample-light
+    darktooth-theme ; coffee
+    leuven-theme ; awesome white background theme
+    planet-theme ; dark blue
+    tao-theme ; monochrome
+    twilight-bright-theme
+    twilight-anti-bright-theme
+    ;; Crypt
+    ;; ido-vertical-mode flx-ido ido-ubiquitous ; < ivy, counsel
+    ;; git-gutter git-gutter-fringe git-gutter+ git-gutter-fringe+ ; < diff-hl
+    ;; popwin ; < shackle
+    ;; ox-reveal ; used to export to HTML slides; using latest from git
+    ;; helm helm-swoop ; < swiper
+    ;; helm-gtags ; < ggtags
+    )
+  "A list of packages to ensure are installed at launch")
 
 ;; Basic requires
-(require 'cl)
-(require 'cl-lib)
-
 ;; Place `setup-var-overrides.el' with `(provide 'setup-var-overrides)' in
 ;; `user-personal-directory'
 (add-to-list 'load-path user-personal-directory)
 (require 'setup-var-overrides nil :noerror)
 
 (load custom-file :noerror :nomessage) ; Load the emacs `M-x customize` generated file
-
-(load setup-packages-file nil :nomessage) ; Load the packages
+(load (locate-user-emacs-file "general.el") nil :nomessage)
+(load (locate-user-emacs-file "setup-packages.el") nil :nomessage)
 ;; (package-initialize) ; Do NOT delete this comment
 ;;   In emacs 25+, the `package-initialize' call is auto-added to the top of
 ;; init.el unless the user already has a commented or uncommented
@@ -174,12 +175,16 @@
 ;; Start `benchmark-init' as soon as possible
 (require 'benchmark-init)
 
+(>=e "25.0"
+    nil
+  (require 'cl)
+  (require 'cl-lib))
+
 (eval-when-compile
   (require 'use-package)
   (setq use-package-always-ensure nil))
 (require 'bind-key)
 (require 'use-package-chords)
-(require 'defuns)
 
 ;; Enable `modi-mode' unless `disable-pkg-modi-mode' is set to `t' in
 ;; `setup-var-overrides.el'.
@@ -243,7 +248,8 @@
 (require 'setup-htmlize)
 (require 'setup-hungry-delete)
 (require 'setup-ibuffer)
-(require 'setup-ido)
+(when (bound-and-true-p disable-pkg-ivy)
+  (require 'setup-ido))
 (require 'setup-imenu-list)
 (require 'setup-indent-guide)
 (require 'setup-iregister)
