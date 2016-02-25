af85a0349f1bd58b06602af59f4e96f39d280b9e
Kaushal Modi <kaushal.modi@gmail.com>
2016-02-25 11:13:56 -0500

    Re-organize/cleanup setup-dired

--- setup-files/setup-dired.el
+++ setup-files/setup-dired.el
@@ -1,21 +1,36 @@
-;; Time-stamp: <2015-06-04 11:16:15 kmodi>
+;; Time-stamp: <2016-02-25 11:32:34 kmodi>
 
-;; dired, dired-x, dired+, dired-single
+;; Dired
+
 ;; http://www.emacswiki.org/emacs-en/dired-single.el
-;; http://truongtx.me/2013/04/24/dired-as-default-file-manager-1-introduction
-
 (use-package dired-single
-  :commands (dired dired-single-magic-buffer-current-dir dired-single-magic-buffer)
-  :init
+  :bind (:map modi-mode-map
+         ;; Change the default `C-x C-d` key binding from `ido-list-directory'
+         ("C-x C-d" . dired-single-magic-buffer-current-dir)
+         ;; Change the default `C-x C-j` key binding from `dired-jump'
+         ;; Opens dired-single-magic-buffer but asks which directory to open that
+         ;; dired buffer for.
+         ("C-x C-j" . dired-single-magic-buffer))
+  :config
   (progn
+    (defun dired-single-magic-buffer-current-dir ()
+      "Open a single magic dired buffer for the current buffer directory."
+      (interactive)
+      (dired-single-magic-buffer default-directory))
+
+    (defun dired-single-up-directory ()
+      (interactive)
+      (dired-single-buffer ".."))
+
     (bind-keys
-     :map modi-mode-map
-      ;; Change the default `C-x C-d` key binding from `ido-list-directory'
-      ("C-x C-d" . dired-single-magic-buffer-current-dir)
-      ;; Change the default `C-x C-j` key binding from `dired-jump'
-      ;; Opens dired-single-magic-buffer but asks which directory to open that
-      ;; dired buffer for.
-      ("C-x C-j" . dired-single-magic-buffer)))
+     :map dired-mode-map
+      ("<return>"         . dired-single-buffer)
+      ("<double-mouse-1>" . dired-single-buffer-mouse)
+      ("^"                . dired-single-up-directory))))
+
+(use-package dired
+  :commands (dired dired-toggle-read-only
+                   dired-get-filename) ; called by `dired-single'
   :config
   (progn
     (setq dired-recursive-deletes 'always)
@@ -27,15 +42,37 @@
     ;; in the prompt for file copy, rename etc.
     (setq dired-dwim-target t)
 
-    (use-package dired+
+    (defun modi/dired-rename-buffer-name ()
+      "Rename the dired buffer name to distinguish it from file buffers.
+It added extra strings at the front and back of the default dired buffer name."
+      (let ((name (buffer-name)))
+        (if (not (string-match "/$" name))
+            (rename-buffer (concat "*Dired* " name "/") t))))
+
+    (defun modi/dired-truncate-lines ()
+      (toggle-truncate-lines 1))
+
+    (add-hook 'dired-mode-hook #'modi/dired-rename-buffer-name)
+    (add-hook 'dired-mode-hook #'modi/dired-truncate-lines)
+
+    (use-package dired-x
       :config
       (progn
-        ;; detail toggling is bound to `(' in dired-mode by default
-        (setq diredp-hide-details-initially-flag nil)
-        (setq diredp-hide-details-last-state     nil)
+        (setq dired-omit-verbose nil)
+        ;; hide backup, autosave, *.*~ files
+        ;; omit mode can be toggled using `M-o' in dired buffer
+        (add-hook 'dired-mode-hook #'dired-omit-mode)))
 
+    ;; http://www.emacswiki.org/DiredPlus
+    (use-package dired+
+      :init
+      (progn
+        ;; detail toggling is bound to "(" in `dired-mode' by default
+        (setq diredp-hide-details-initially-flag nil))
+      :config
+      (progn
         ;; Privilege indicator faces
-        (defun modi/dired-update-priv-faces ()
+        (defun modi/dired-update-privilege-faces ()
           (set-face-attribute 'diredp-dir-priv nil
                               :foreground "#7474FFFFFFFF"
                               :background (face-background 'default))
@@ -58,39 +95,7 @@
                               :background (face-background 'default))
           (set-face-attribute 'diredp-link-priv nil
                               :foreground "#00007373FFFF"))
-        (add-hook 'dired-mode-hook #'modi/dired-update-priv-faces)))
-
-    (use-package dired-x
-      :config
-      (progn
-        (setq dired-omit-verbose nil)
-        ;; hide backup, autosave, *.*~ files
-        ;; omit mode can be toggled using `M-o' in dired buffer
-        (add-hook 'dired-mode-hook #'dired-omit-mode)))
-
-    (defun dired-single-magic-buffer-current-dir ()
-      "Open a single magic dired buffer for the current buffer directory."
-      (interactive)
-      (dired-single-magic-buffer default-directory))
-
-    (defun dired-single-up-directory ()
-      (interactive)
-      (dired-single-buffer ".."))
-
-    (defun rename-dired-buffer-name ()
-      "Rename the dired buffer name to distinguish it from file buffers.
-It added extra strings at the front and back of the default dired buffer name."
-      (let ((name (buffer-name)))
-        (if (not (string-match "/$" name))
-            (rename-buffer (concat "*Dired* " name "/") t))))
-    (add-hook 'dired-mode-hook #'rename-dired-buffer-name)
-    (add-hook 'dired-mode-hook (lambda () (toggle-truncate-lines 1))) ; enable truncation
-
-    (bind-keys
-     :map dired-mode-map
-      ("<return>"         . dired-single-buffer)
-      ("<double-mouse-1>" . dired-single-buffer-mouse)
-      ("^"                . dired-single-up-directory))))
+        (add-hook 'dired-mode-hook #'modi/dired-update-privilege-faces)))))
 
 
 (provide 'setup-dired)
@@ -112,3 +117,5 @@ It added extra strings at the front and back of the default dired buffer name."
 ;;    To make it copy the absolute path: `0 w'
 
 ;; (3) To copy the path to the folder you’re looking at in dired: `M-< w'
+
+;; http://truongtx.me/2013/04/24/dired-as-default-file-manager-1-introduction
