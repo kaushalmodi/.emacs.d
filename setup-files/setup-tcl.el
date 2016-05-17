c40b09e99eb0a2abdcc06af9a4e2736ec78da523
Kaushal Modi <kaushal.modi@gmail.com>
2016-05-17 14:45:39 -0400

    Recognize svcf files as tcl files

--- setup-files/setup-tcl.el
+++ setup-files/setup-tcl.el
@@ -1,10 +1,11 @@
-;; Time-stamp: <2015-03-31 09:50:54 kmodi>
+;; Time-stamp: <2016-05-17 14:44:02 kmodi>
 
 ;; Tcl
 
 (use-package tcl-mode
   :mode (("\\.tcl\\'"   . tcl-mode)
          ("\\.tcons\\'" . tcl-mode)
+         ("\\.svcf\\'" . tcl-mode)
          ("\\.cer\\'"   . tcl-mode)))
 
 
