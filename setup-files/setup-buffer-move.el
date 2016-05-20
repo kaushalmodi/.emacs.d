33be3dc48c017d41547e6e647aa53a39b72fdb78
Kaushal Modi <kaushal.modi@gmail.com>
2016-05-20 00:14:38 -0400

    Heavily optimize buffer-move setup

--- setup-files/setup-buffer-move.el
+++ setup-files/setup-buffer-move.el
@@ -1,49 +1,23 @@
-;; Time-stamp: <2015-11-06 11:01:35 kmodi>
+;; Time-stamp: <2016-05-20 00:13:41 kmodi>
 
 ;; https://github.com/lukhas/buffer-move
 
 (use-package buffer-move
-  :commands (modi/buf-move-left
-             modi/buf-move-right
-             modi/buf-move-up
-             modi/buf-move-down)
+  :defer t
   :init
   (progn
-    (bind-to-modi-map "," #'modi/buf-move-left)
-    (bind-to-modi-map "." #'modi/buf-move-right)
-    (bind-to-modi-map "k" #'modi/buf-move-up)
-    (bind-to-modi-map "j" #'modi/buf-move-down))
+    (bind-to-modi-map "," #'buf-move-left)
+    (bind-to-modi-map "." #'buf-move-right)
+    (bind-to-modi-map "k" #'buf-move-up)
+    (bind-to-modi-map "j" #'buf-move-down))
   :config
   (progn
-    (setq buffer-move-behavior 'swap) ; values = 'swap (default), 'move
-
-    (defun modi/buf-move-left (swap)
-      "Calls `buf-move-left' with the “move” behavior.
-If SWAP is non-nil, the default “swap” behavior is used."
-      (interactive "P")
-      (let ((buffer-move-behavior (if swap 'swap 'move)))
-        (buf-move-left)))
-
-    (defun modi/buf-move-right (swap)
-      "Calls `buf-move-right' with the “move” behavior.
-If SWAP is non-nil, the default “swap” behavior is used."
-      (interactive "P")
-      (let ((buffer-move-behavior (if swap 'swap 'move)))
-        (buf-move-right)))
-
-    (defun modi/buf-move-up (swap)
-      "Calls `buf-move-up' with the “move” behavior.
-If SWAP is non-nil, the default “swap” behavior is used."
-      (interactive "P")
-      (let ((buffer-move-behavior (if swap 'swap 'move)))
-        (buf-move-up)))
-
-    (defun modi/buf-move-down (swap)
-      "Calls `buf-move-down' with the “move” behavior.
-If SWAP is non-nil, the default “swap” behavior is used."
-      (interactive "P")
-      (let ((buffer-move-behavior (if swap 'swap 'move)))
-        (buf-move-down)))))
+    (defun modi/buf-move-swap-option (orig-fun &rest args)
+      "Set default `buf-move-to' behavior to “move” buffers.
+If prefix argument is used, use the “swap” behavior."
+      (let ((buffer-move-behavior (if current-prefix-arg 'swap 'move)))
+        (apply orig-fun args)))
+    (advice-add 'buf-move-to :around #'modi/buf-move-swap-option)))
 
 
 (provide 'setup-buffer-move)
