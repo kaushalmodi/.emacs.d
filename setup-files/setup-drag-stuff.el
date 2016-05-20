88952b14c23d2c0639096d71bcc976b13fdd6754
Kaushal Modi <kaushal.modi@gmail.com>
2016-05-20 18:06:29 -0400

    Change drag-stuff bindings to be terminal-friendly
    Remove unnecessary code like enabling drag-stuff-global-mode

--- setup-files/setup-drag-stuff.el
+++ setup-files/setup-drag-stuff.el
@@ -1,14 +1,16 @@
-;; Time-stamp: <2015-07-24 15:30:07 kmodi>
+;; Time-stamp: <2016-05-20 18:06:00 kmodi>
 
 ;; Drag Stuff
 ;; https://github.com/rejeep/drag-stuff
 
 (use-package drag-stuff
+  :bind (:map modi-mode-map
+         ("M-O" . drag-stuff-up)
+         ("M-L" . drag-stuff-down) 
+         ("M-K" . drag-stuff-left)
+         ("M-\"" . drag-stuff-right))
   :config
   (progn
-    ;; Do not use `drag-stuff' keys
-    (defun drag-stuff-define-keys ())
-
     ;; http://emacs.stackexchange.com/a/13942/115
     ;; https://github.com/rejeep/drag-stuff.el/issues/4
     (defvar modi/drag-stuff--point-adjusted nil)
@@ -43,16 +45,7 @@ the vertical drag is done."
         (setq modi/drag-stuff--point-mark-exchanged nil)))
 
     (add-hook 'drag-stuff-before-drag-hook #'modi/drag-stuff--adj-pt-pre-drag)
-    (add-hook 'drag-stuff-after-drag-hook  #'modi/drag-stuff--rst-pt-post-drag)
-
-    (bind-keys
-     :map modi-mode-map
-      ("C-S-o" . drag-stuff-up)
-      ("C-S-l" . drag-stuff-down)
-      ("C-:"   . drag-stuff-left)
-      ("C-\""  . drag-stuff-right))
-
-    (drag-stuff-global-mode 1)))
+    (add-hook 'drag-stuff-after-drag-hook  #'modi/drag-stuff--rst-pt-post-drag)))
 
 
 (provide 'setup-drag-stuff)
