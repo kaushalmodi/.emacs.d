82c8b766a9711e7367f92a677b3666fe4a39116e
Kaushal Modi <kaushal.modi@gmail.com>
2016-07-15 18:52:37 -0400

    Do not run hungry-delete-mode in wdired-mode

--- setup-files/setup-hungry-delete.el
+++ setup-files/setup-hungry-delete.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2015-02-23 11:42:32 kmodi>
+;; Time-stamp: <2016-07-15 16:31:29 kmodi>
 
 ;; Hungry Delete
 ;; https://github.com/nflath/hungry-delete
@@ -7,8 +7,16 @@
   :config
   (progn
     (setq hungry-delete-chars-to-skip " \t\r\f\v")
-    (global-hungry-delete-mode nil)
-    (add-hook 'prog-mode-hook 'hungry-delete-mode)))
+
+    (add-hook 'prog-mode-hook #'hungry-delete-mode)
+
+    (defun modi/turn-off-hungry-delete-mode ()
+      "Turn off hungry delete mode."
+      (hungry-delete-mode -1))
+
+    ;; `hungry-delete-mode'-loaded backspace does not work in `wdired-mode',
+    ;; i.e. when editing file names in the *Dired* buffer.
+    (add-hook 'wdired-mode-hook #'modi/turn-off-hungry-delete-mode)))
 
 
 (provide 'setup-hungry-delete)
