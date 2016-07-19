4e42b99245925d6c28ba738adf684d92c97eedcf
Kaushal Modi <kaushal.modi@gmail.com>
2016-07-19 17:28:00 -0400

    Enable `hungry-delete-mode` almost everywhere
    In the previous commit, I made the mistake of removing
    the (global-hungry-delete-mode) call. By doing that, I realized that
    this mode did not get enabled in major modes like
    conf-space-mode (because that mode is not derived from prog-mode).
    
    Now hungry-delete-mode is enable everywhere except in wdired-mode.

--- setup-files/setup-hungry-delete.el
+++ setup-files/setup-hungry-delete.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2016-07-15 16:31:29 kmodi>
+;; Time-stamp: <2016-07-19 17:26:52 kmodi>
 
 ;; Hungry Delete
 ;; https://github.com/nflath/hungry-delete
@@ -8,12 +8,14 @@
   (progn
     (setq hungry-delete-chars-to-skip " \t\r\f\v")
 
-    (add-hook 'prog-mode-hook #'hungry-delete-mode)
-
     (defun modi/turn-off-hungry-delete-mode ()
       "Turn off hungry delete mode."
       (hungry-delete-mode -1))
 
+    ;; Enable `hungry-delete-mode' everywhere ..
+    (global-hungry-delete-mode)
+
+    ;; Except ..
     ;; `hungry-delete-mode'-loaded backspace does not work in `wdired-mode',
     ;; i.e. when editing file names in the *Dired* buffer.
     (add-hook 'wdired-mode-hook #'modi/turn-off-hungry-delete-mode)))
