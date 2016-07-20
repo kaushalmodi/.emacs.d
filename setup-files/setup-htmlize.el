6b4a6889c596414f7dc7e5e9c153b35f67cda6c1
Kaushal Modi <kaushal.modi@gmail.com>
2016-07-20 15:22:40 -0400

    Complete re-write of setup-htmlize
    Now separate functions are added to the htmlize before/after hooks based
    on if the fill-column-indicator and/or flyspell-mode package(s) are
    available.

--- setup-files/setup-htmlize.el
+++ setup-files/setup-htmlize.el
@@ -1,41 +1,50 @@
-;; Time-stamp: <2015-06-24 13:26:59 kmodi>
+;; Time-stamp: <2016-07-20 15:19:10 kmodi>
 
 ;; Htmlize
 
 (use-package htmlize
+  :defer t
   :config
   (progn
-    (defvar modi/htmlize-initial-fci-state nil
-      "Variable to store the state of `fci-mode' when `htmlize-buffer' is called.")
-    (defvar modi/htmlize-initial-flyspell-state nil
-      "Variable to store the state of `flyspell-mode' when `htmlize-buffer' is called.")
 
-    (defun modi/htmlize-before-hook-fn ()
-      ;; It is required to disable `fci-mode' when `htmlize-buffer' is called;
-      ;; otherwise the invisible fci characters show up as funky looking
-      ;; visible characters in the source code blocks in the html file
-      ;; http://lists.gnu.org/archive/html/emacs-orgmode/2014-09/msg00777.html
-      (when (fboundp 'fci-mode)
+    ;; It is required to disable `fci-mode' when `htmlize-buffer' is called;
+    ;; otherwise the invisible fci characters show up as funky looking
+    ;; visible characters in the source code blocks in the html file.
+    ;; http://lists.gnu.org/archive/html/emacs-orgmode/2014-09/msg00777.html
+    (with-eval-after-load 'fill-column-indicator
+      (defvar modi/htmlize-initial-fci-state nil
+        "Variable to store the state of `fci-mode' when `htmlize-buffer' is called.")
+
+      (defun modi/htmlize-before-hook-fci-disable ()
         (setq modi/htmlize-initial-fci-state fci-mode)
         (when fci-mode
           (fci-mode -1)))
-      ;; `flyspell-mode' also has to be disabled because depending on the
-      ;; theme, the squiggly underlines can either show up in the html file
-      ;; or cause elisp errors
-      (when (fboundp 'flyspell-mode)
-        (setq modi/htmlize-initial-flyspell-state flyspell-mode)
-        (when flyspell-mode
-          (flyspell-mode -1))))
-    (add-hook 'htmlize-before-hook #'modi/htmlize-before-hook-fn)
 
-    (defun modi/htmlize-after-hook-fn ()
-      (when (fboundp 'fci-mode)
+      (defun modi/htmlize-after-hook-fci-enable-maybe ()
         (when modi/htmlize-initial-fci-state
           (fci-mode 1)))
-      (when (fboundp 'flyspell-mode)
+
+      (add-hook 'htmlize-before-hook #'modi/htmlize-before-hook-fci-disable)
+      (add-hook 'htmlize-after-hook #'modi/htmlize-after-hook-fci-enable-maybe))
+
+    ;; `flyspell-mode' also has to be disabled because depending on the
+    ;; theme, the squiggly underlines can either show up in the html file
+    ;; or cause elisp errors.
+    (with-eval-after-load 'flyspell-mode
+      (defvar modi/htmlize-initial-flyspell-state nil
+        "Variable to store the state of `flyspell-mode' when `htmlize-buffer' is called.")
+
+      (defun modi/htmlize-before-hook-flyspell-disable ()
+        (setq modi/htmlize-initial-flyspell-state flyspell-mode)
+        (when flyspell-mode
+          (flyspell-mode -1)))
+
+      (defun modi/htmlize-after-hook-flyspell-enable-maybe ()
         (when modi/htmlize-initial-flyspell-state
-          (flyspell-mode 1))))
-    (add-hook 'htmlize-after-hook #'modi/htmlize-after-hook-fn)))
+          (flyspell-mode 1)))
+
+      (add-hook 'htmlize-before-hook #'modi/htmlize-before-hook-flyspell-disable)
+      (add-hook 'htmlize-after-hook #'modi/htmlize-after-hook-flyspell-enable-maybe))))
 
 
-  (provide 'setup-htmlize)
+(provide 'setup-htmlize)
