4e80cf417e48f3ce0029370b5f6caad12822224b
Kaushal Modi <kaushal.modi@gmail.com>
2016-08-09 11:58:35 -0400

    Start using fci again
    The bug that bothered me (debbugs#23949) is now fixed in the master
    branch build.

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2016-07-27 23:24:20 kmodi>
+;; Time-stamp: <2016-08-09 11:54:34 kmodi>
 ;; Author: Kaushal Modi
 
 ;; Global variables
@@ -216,9 +216,10 @@ So, for emacs version 25.0.50.1, this variable will be 25_0.")
 (require 'setup-elisp-slime-nav)
 (require 'setup-engine-mode)
 (require 'setup-expand-region)
-;; Uncommenting below will cause emacs to freeze on evaluating "(string-match-p "." nil)"
+;; Below will cause emacs to freeze on evaluating "(string-match-p "." nil)"
+;; on emacs 25.1 or older.
 ;; http://debbugs.gnu.org/cgi/bugreport.cgi?bug=23949
-;; (require 'setup-fci)
+(require 'setup-fci)
 (require 'setup-fold)
 (require 'setup-gist)
 (when (executable-find "git")
@@ -253,6 +254,7 @@ So, for emacs version 25.0.50.1, this variable will be 25_0.")
 (require 'setup-poporg)
 (with-eval-after-load 'setup-tags
   ;; Below causes `help-function-arglist' error on evaluating "(string-match-p "." nil)"
+  ;; on emacs 25.1 or older.
   ;; http://debbugs.gnu.org/cgi/bugreport.cgi?bug=23949
   (require 'setup-projectile))
 (require 'setup-rainbow-delimiters)
