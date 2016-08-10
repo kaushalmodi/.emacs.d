cb2b87bdb49651c6712acfdecec3dda36b10afa0
Kaushal Modi <kaushal.modi@gmail.com>
2016-08-10 11:08:16 -0400

    Use my fork of el2markdown

--- setup-files/setup-el2markdown.el
+++ setup-files/setup-el2markdown.el
@@ -1,8 +1,9 @@
-;; Time-stamp: <2016-08-10 01:26:44 kmodi>
+;; Time-stamp: <2016-08-10 11:07:52 kmodi>
 
 ;; https://github.com/Lindydancer/el2markdown
 
 (use-package el2markdown
+  :load-path "elisp/el2markdown"
   :bind (:map modi-mode-map
          ("C-c R" . el2markdown-write-readme)))
 
