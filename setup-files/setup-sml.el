af48c4e4943f59ef96fde28b34ed33214bf32b1a
Kaushal Modi <kaushal.modi@gmail.com>
2016-08-11 01:51:26 -0400

    Add modi/restart-sml-and-run function
    This is a DWIM function to force restart SML REPL and run the *right*
    file.
    
    The command does the following:
     - Kill live sml process if running
     - Restart the process
     - Run the *right* file
    
    These were tested to work:
    - Whether this command is run from the REPL buffer or the sml code
    buffer
    - Whether the sml process is alive or dead
    - Whether the sml buffer exists or not
    - Even if it is run from various .sml files from different directories

--- setup-files/setup-sml.el
+++ setup-files/setup-sml.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2016-08-09 08:16:06 kmodi>
+;; Time-stamp: <2016-08-11 01:44:20 kmodi>
 
 ;; Standard ML
 
@@ -41,6 +41,53 @@
       ;; (electric-indent-local-mode -1)
       (auto-fill-mode))
     (add-hook 'sml-mode-hook #'modi/sml-mode-hook-fn)
+
+    (defun modi/restart-sml-and-run ()
+      "Restarts the SML REPL and tries to load the correct .sml file.
+
+The sml file loaded is the one at the top of the list returned by
+`buffer-list'.
+
+If this command is executed while in an sml buffer, the point is returned back
+to that buffer after restarting the REPL and loading that file."
+      (interactive)
+      (let* ((in-sml-buf (derived-mode-p 'sml-mode))
+             (repl-buf (get-buffer "*sml*"))
+             (sml-file (if in-sml-buf
+                           (buffer-file-name)
+                         (catch 'break
+                           (dolist (buf (buffer-list))
+                             (let ((file (buffer-file-name buf)))
+                               (when (and (stringp file)
+                                          (string-match-p ".*\\.sml\\'" file))
+                                 ;; Return the first .sml file name from the list
+                                 ;; and end the loop.
+                                 (throw 'break file)))))))
+             (msg "Restarted SML"))
+        (when repl-buf
+          (when in-sml-buf
+            ;; If the REPL buffer already exists and you are not in it, switch
+            ;; to it.
+            (switch-to-buffer-other-window repl-buf))
+          ;; If the REPL has a live process, kill it
+          (when (get-buffer-process (current-buffer))
+            (goto-char (point-max)) ; First go to the EOB
+            (comint-delchar-or-maybe-eof 1) ; Then kill the REPL (C-d)
+            (sleep-for 0.1))) ; Somehow this is needed between kill and respawn
+        (if sml-file
+            (setq msg (concat msg (format " and now loading `%s'"
+                                          (file-name-nondirectory sml-file))))
+          ;; If no sml file was found in the buffer list
+          (setq sml-file ""))
+        (setq msg (concat msg "."))
+        (message msg)
+        ;; Then start a new process
+        (sml-run "sml" sml-file) ; C-c C-s + typing "use foo.sml" in REPL
+        ;; Switch back to the sml buffer if you executed this command from there
+        (when in-sml-buf
+          (switch-to-buffer-other-window (get-file-buffer sml-file)))))
+    (bind-key "C-c <C-return>" #'modi/restart-sml-and-run inferior-sml-mode-map)
+    (bind-key "C-c <C-return>" #'modi/restart-sml-and-run sml-mode-map)
 
     ;; Do not bind M-SPC to `sml-electric-space' in `sml-mode-map'.
     ;; I prefer the default binding to `just-one-space'.
