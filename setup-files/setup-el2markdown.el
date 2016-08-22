180afe5dae81cab0c82ff56f1f1d80b445e4988c
Kaushal Modi <kaushal.modi@gmail.com>
2016-08-22 18:43:27 -0400

    Use Melpa version of el2markdown
    Because the "empty line in code block" issue that I reported is now
    fixed -- https://github.com/Lindydancer/el2markdown/issues/7

--- setup-files/setup-el2markdown.el
+++ setup-files/setup-el2markdown.el
@@ -1,9 +1,8 @@
-;; Time-stamp: <2016-08-10 11:07:52 kmodi>
+;; Time-stamp: <2016-08-22 18:42:59 kmodi>
 
 ;; https://github.com/Lindydancer/el2markdown
 
 (use-package el2markdown
-  :load-path "elisp/el2markdown"
   :bind (:map modi-mode-map
          ("C-c R" . el2markdown-write-readme)))
 
