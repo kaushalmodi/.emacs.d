2e44614e2c6476d983134f6dd613326575b8d977
Kaushal Modi <kaushal.modi@gmail.com>
2017-01-26 11:49:55 -0500

    Do not enable whitespace-mode in verilog-mode; magit doesn't like it

--- elisp/.dir-locals.el
+++ elisp/.dir-locals.el
@@ -1,8 +1,13 @@
 (("verilog-mode/src"
   . ((nil . ((do-not-delete-trailing-whitespace . t)
-             (eval . (whitespace-mode 1))))
+             ;; Thu Jan 26 11:12:48 EST 2017 - kmodi
+             ;; Do not turn on whitespace-mode as magit does not like it.
+             ;; user-error: Whitespace-Mode isn’t compatible with Magit.  See
+             ;; `magit-diff-paint-whitespace' for an alternative.
+             ;; (eval . (whitespace-mode 1))
+             ))
      (emacs-lisp-mode . ((lisp-indent-function . lisp-indent-function)
-                         (aggressive-indent-mode . nil))))) 
+                         (aggressive-indent-mode . nil)))))
  ("matlab-emacs/src"
   . ((emacs-lisp-mode . ((lisp-indent-function . lisp-indent-function)
                          (aggressive-indent-mode . nil))))))
