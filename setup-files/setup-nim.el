1350fc45ebff4caf9c4b0bd07d97e3a97de91abf
Kaushal Modi <kaushal.modi@gmail.com>
2017-04-15 00:28:15 -0400

    Add nim compilation command to smart-compile-alist

--- setup-files/setup-nim.el
+++ setup-files/setup-nim.el
@@ -1,11 +1,16 @@
-;; Time-stamp: <2017-03-24 16:44:20 kmodi>
+;; Time-stamp: <2017-04-14 23:15:22 kmodi>
 
 ;; Nim
 ;; https://github.com/nim-lang/nim-mode
 
 (use-package nim-mode
   :ensure t
-  :mode (("\\.nim\\'" . nim-mode)))
+  :mode (("\\.nim\\'" . nim-mode))
+  :config
+  (progn
+    (with-eval-after-load 'smart-compile
+      (add-to-list 'smart-compile-alist
+                   '(nim-mode . "nim c --verbosity:0 %f")))))
 
 
 (provide 'setup-nim)
