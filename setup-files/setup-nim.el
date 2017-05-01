618b7ed4d21b29a0772a83663129cd921182dbb7
Kaushal Modi <kaushal.modi@gmail.com>
2017-05-01 17:28:31 -0400

    Enable nimsuggest

--- setup-files/setup-nim.el
+++ setup-files/setup-nim.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-04-14 23:15:22 kmodi>
+;; Time-stamp: <2017-05-01 17:28:23 kmodi>
 
 ;; Nim
 ;; https://github.com/nim-lang/nim-mode
@@ -10,7 +10,13 @@
   (progn
     (with-eval-after-load 'smart-compile
       (add-to-list 'smart-compile-alist
-                   '(nim-mode . "nim c --verbosity:0 %f")))))
+                   '(nim-mode . "nim c --verbosity:0 %f")))
+
+    (when (executable-find "nimsuggest")
+      (setq nim-nimsuggest-path (executable-find "nimsuggest"))
+      ;; Currently nimsuggest doesn't support nimscript files, so only nim-mode ..
+      ;; nimsuggest will provide hints in the minibuffer using `eldoc-mode'.
+      (add-hook 'nim-mode-hook #'nimsuggest-mode))))
 
 
 (provide 'setup-nim)
