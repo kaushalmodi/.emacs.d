58f19f11bd480b765bd8548e71b6fef57f29eb8f
Kaushal Modi <kaushal.modi@gmail.com>
2017-05-11 10:35:42 -0400

    Show submodules info in magit status
    Thanks to https://www.reddit.com/r/emacs/comments/6aiwk5/how_to_manage_multiple_gitrepositories_at_once/dhf47dg/

--- setup-files/setup-magit.el
+++ setup-files/setup-magit.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-05-09 15:49:51 kmodi>
+;; Time-stamp: <2017-05-11 10:25:20 kmodi>
 
 ;; magit
 ;; https://github.com/magit/magit
@@ -9,6 +9,18 @@
   :commands (magit-status magit-log-all-branches)
   :config
   (progn
+    ;; Magit Submodule support
+    ;; https://www.reddit.com/r/emacs/comments/6aiwk5/how_to_manage_multiple_gitrepositories_at_once/dhf47dg/
+    (dolist (fn '(;; Below will end up being the last of these newly added fns,
+                  ;; and the last element in `magit-status-sections-hook' too.
+                  magit-insert-modules-unpulled-from-upstream
+                  magit-insert-modules-unpushed-to-pushremote
+                  magit-insert-modules-unpushed-to-upstream
+                  magit-insert-modules-unpulled-from-pushremote
+                  ;; Below will end up being the first of these newly added fns.
+                  magit-insert-submodules))
+      (magit-add-section-hook 'magit-status-sections-hook `,fn nil :append))
+
     (defhydra hydra-magit (:color blue
                            :columns 4)
       "Magit"
