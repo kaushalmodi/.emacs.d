3b5887aa819cf187b7ec5ea264eda53ba5ad281c
Kaushal Modi <kaushal.modi@gmail.com>
2017-05-26 13:24:18 -0400

    Add a command to search cheat.sh
    Slightly modified version of the snippet posted on
    https://www.reddit.com/r/emacs/comments/6ddr7p/snippet_search_cheatsh_using_ivy/
    by /u/RenJMR.
    
    - Add an alias `cheat.sh' to `ejmr/counsel-cheat-sh'
    - Always use `eww' to launch the cheat.sh page
    - Auto-rename eww buffer to *cheat.sh <STRING>*

--- setup-files/setup-tldr.el
+++ setup-files/setup-tldr.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-05-26 11:08:19 kmodi>
+;; Time-stamp: <2017-05-26 13:23:09 kmodi>
 
 ;; TLDR
 ;; https://github.com/tldr-pages/tldr
@@ -10,6 +10,38 @@
                                       (getenv "USER") "/tldr/")) ;must end with /
     (setq tldr-saved-zip-path (concat temporary-file-directory
                                       (getenv "USER") "/tldr-source.zip"))))
+
+;; Related -- curl cheat.sh
+;; https://github.com/chubin/cheat.sh
+;; https://www.reddit.com/r/emacs/comments/6ddr7p/snippet_search_cheatsh_using_ivy/
+(defvar ejmr/counsel-cheat-sh-history nil
+  "History for `ejmr/counsel-cheat-sh'.")
+(defun ejmr/counsel-cheat-sh ()
+  "Search `http://cheat.sh/' for help on commands and code."
+  (interactive)
+  (let ((url "http://cheat.sh/")
+        ;; T - omit terminal sequences (no colors)
+        ;;     Without that, we get this error:
+        ;;       Too deeply nested to render properly; consider increasing
+        ;;       `max-specpdl-size'.
+        ;; q - quiet mode, don't show github/twitter buttons
+        (options "?T&q"))
+    (ivy-read "Command or Topic: "
+              (process-lines "curl" "--silent" (concat url ":list" options))
+              :require-match t
+              :sort t
+              :history 'ejmr/counsel-cheat-sh-history
+              :action (lambda (input)
+                        (eww-browse-url (concat url input options)))
+              :caller 'ejmr/counsel-cheat-sh)))
+(defalias 'cheat.sh 'ejmr/counsel-cheat-sh)
+
+(defun modi/eww-rename-cheat-sh-buffer (&rest _)
+  "Rename the `eww' buffer if it is showing a `cheat.sh' page."
+  (let ((url (eww-copy-page-url)))
+    (when (string-match "^http://cheat.sh/\\([^/?]+\\)" url)
+      (rename-buffer (concat "*cheat.sh " (match-string 1 url) "*") :unique))))
+(advice-add 'eww :after #'modi/eww-rename-cheat-sh-buffer)
 
 
 (provide 'setup-tldr)
