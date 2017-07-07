9cb6f9f7b49a22f96afcc6b8b83e3851f5152e81
Kaushal Modi <kaushal.modi@gmail.com>
2017-07-07 18:03:21 -0400

    First draft of fetching Python 3 Doc URL for the current Info node
    - Add new fn modi/Info-url-for-current-node that overrides the Info-url-for-node
      function from info+ package.
      - Gets the current node instead of asking the user interactively for the node
      name.
      - Supports fetching URL for `python3' Info nodes.
    - Bound "W" to call this new function in Info-mode
    
    The modi/Info-python3-doc-url fn still needs a lot of work to create node->URL
    correspondence as there doesn't seem to be any consistency in how the URL names
    are derived from the node names. But the current state of that fn in this commit
    is pretty good, usable.

--- setup-files/setup-info.el
+++ setup-files/setup-info.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-06-15 17:57:11 kmodi>
+;; Time-stamp: <2017-07-07 18:02:45 kmodi>
 
 ;; Info
 
@@ -59,12 +59,220 @@ that level."
             ;; (message "modi/Info--get-current-node-hierarchy: %S" node-hier)
             node-hier))
 
+        (defun modi/Info--python3-slug (node &optional nospace)
+          "Return the slug string for NODE string for Python 3 documentation.
+
+Replace non-alphabet characters with spaces and then one or more
+spaces with a single hyphen.  The returned string contains all
+lower-cased letters.
+
+When NOSPACE is non-nil, space characters if present in NODE are
+removed instead of replacing with hyphens."
+          (let* ((lower-node (downcase node))
+                 (nosymbols-node (replace-regexp-in-string "[^A-Za-z]" " " lower-node))
+                 (trim-node (replace-regexp-in-string "\\`[\t ]+" "" nosymbols-node))
+                 (trim-node (replace-regexp-in-string "[\t ]+\\'" "" trim-node)))
+            (if nospace
+                ;; Remove all spaces
+                (replace-regexp-in-string " +" "" trim-node)
+              ;; Replace one or more spaces with a single hyphen
+              (replace-regexp-in-string " +" "-" trim-node))))
+
+        (defun modi/Info-python3-doc-url ()
+          "Return the Python 3 Documentation URL for the current node."
+          (let* ((base-url "https://docs.python.org/3/")
+                 (node-hier (modi/Info--get-current-node-hierarchy))
+                 (level1 (cdr (assoc 1 node-hier)))
+                 (url-dir-name "")
+                 (level2 (cdr (assoc 2 node-hier)))
+                 (level3 (cdr (assoc 3 node-hier)))
+                 (level4 (cdr (assoc 4 node-hier)))
+                 (url-page-name "")
+                 (section-id-name "")
+                 (current-level (car (rassoc Info-current-node node-hier))))
+            ;; (message "dbg: level1: %s current level: %0d" level1 current-level)
+            ;; Default section ID
+            (when (or (= 3 current-level)
+                      (= 4 current-level))
+              (setq section-id-name (concat "#" (modi/Info--python3-slug Info-current-node))))
+            (cond
+             ((string-match-p "What.s New in Python" level1)
+              (setq url-dir-name "whatsnew/")
+              (when level2
+                (setq url-page-name (concat (nth 4 (split-string level2)) ;Major version number (example: "3")
+                                            "."
+                                            (nth 5 (split-string level2)))))) ;Minor version number (example: "6")
+             ((string= level1 "The Python Tutorial")
+              (setq url-dir-name "tutorial/")
+              (when level2
+                (string-match (regexp-opt '("Appetite"
+                                            "Interpreter"
+                                            "Introduction"
+                                            "Control Flow"
+                                            "Data Structures"
+                                            "Modules"
+                                            "Input and Output"
+                                            "Errors"
+                                            "Classes"
+                                            "Standard Library"
+                                            "Virtual"
+                                            "What Now"
+                                            "Interactive"
+                                            "Floating Point"
+                                            "Appendix"))
+                              level2)
+                (setq url-page-name (cond
+                                     ((string-match-p "Standard Library\\'" level2)
+                                      "stdlib")
+                                     ((string-match-p "Standard Library.+Part II\\'" level2)
+                                      "stdlib2")
+                                     ((string-match-p "Input and Output" level2)
+                                      "inputoutput")
+                                     ((string-match-p "Input and Output" level2)
+                                      "inputoutput")
+                                     ((string-match-p "Virtual" level2)
+                                      "venv")
+                                     (t
+                                      (modi/Info--python3-slug (match-string-no-properties 0 level2) :nospace))))))
+             ((string= level1 "Python Setup and Usage")
+              (setq url-dir-name "using/")
+              )
+             ((string= level1 "The Python Language Reference")
+              (setq url-dir-name "reference/")
+              )
+             ((string= level1 "The Python Standard Library")
+              (setq url-dir-name "library/")
+              (when level2
+                (string-match (regexp-opt '("Intro"
+                                            "Functions"
+                                            "Constants"
+                                            "Exceptions"
+                                            "Text"
+                                            "Binary"
+                                            "Data Types"
+                                            "Numeric"
+                                            "Functional"
+                                            "Persistence"
+                                            "Archiving"
+                                            "File Formats"
+                                            "Crypto"
+                                            "Markup"
+                                            "Internet" ;"Internet Protocols and Support"
+                                            "Frameworks"
+                                            " Tk"
+                                            "Development"
+                                            "Debug"
+                                            "Distribution"
+                                            "Python"
+                                            "Modules"
+                                            "Language"
+                                            "Misc"
+                                            "Windows"
+                                            "Unix"
+                                            "Superseded"
+                                            "Undoc"))
+                              level2)
+                (setq url-page-name (cond
+                                     ((string-match-p "Built.in Types" level2)
+                                      "stdtypes")
+                                     ((string-match-p "File and Directory Access" level2)
+                                      "filesys")
+                                     ((string-match-p "Generic Operating System Services" level2)
+                                      "allos")
+                                     ((string-match-p "Concurrent Execution" level2)
+                                      "concurrency")
+                                     ((string-match-p "Interprocess Communication and Networking" level2)
+                                      "ipc")
+                                     ((string-match-p "Internet Data Handling" level2)
+                                      "netdata")
+                                     ((string-match-p "Multimedia Services" level2)
+                                      "mm")
+                                     ((string-match-p "Internationalization" level2)
+                                      "i18n")
+                                     ((string-match-p "Custom Python Interpreters" level2)
+                                      "custominterp")
+                                     (t
+                                      (modi/Info--python3-slug (match-string-no-properties 0 level2) :nospace))))))
+             ((string= level1 "Extending and Embedding the Python Interpreter")
+              (setq url-dir-name "extending/")
+              )
+             ((string= level1 "Python/C API Reference Manual")
+              (setq url-dir-name "c-api/")
+              )
+             ((string= level1 "Distributing Python Modules")
+              (setq url-dir-name "distributing/")
+              )
+             ((string= level1 "Installing Python Modules")
+              (setq url-dir-name "installing/")
+              )
+             ((string= level1 "Python HOWTOs")
+              (setq url-dir-name "howto/")
+              )
+             ((string= level1 "Python Frequently Asked Questions")
+              (setq url-dir-name "faq/")
+              (when level2
+                (setq url-page-name (car (split-string (downcase level2)))))
+              ;; Use level 3 node name for section ID even when the current node
+              ;; level is higher than that.. because the official Python 3
+              ;; documentation gives up for section naming deeper FAQ sections.
+              ;;   Example: https://docs.python.org/3/faq/general.html#id8
+              (when (> current-level 3)
+                (setq section-id-name (concat "#" (modi/Info--python3-slug level3)))))
+             (t
+              ))
+            (unless (string= "" url-page-name)
+              (setq url-page-name (concat url-page-name ".html")))
+            (concat base-url url-dir-name url-page-name section-id-name)))
+
+        (defun modi/Info-url-for-current-node (&rest _)
+          "Copy the URL for the current node, and also return it.
+
+The node can be a node in the GNU Emacs, GNU Elisp or Python 3 manual.
+
+If this command is called with `\\[universal-argument]' prefix
+argument, also open the URL in the default browser."
+          (interactive)
+          (unless Info-current-file
+            (error "This command must be invoked from Info"))
+          (let ((node Info-current-node)
+                file url)
+            (string-match "\\s *\\((\\s *\\([^\t)]*\\)\\s *)\\s *\\|\\)\\(.*\\)" node)
+            (setq file (if (= (match-beginning 1) (match-end 1))
+                           ""
+                         (match-string 2 node)))
+	    (setq node (match-string 3 node))
+            (when (equal node "")
+              (setq node  "index"))     ;`Top' node
+            (when-let ((trim (string-match "\\s +\\'" file)))
+              (setq file (substring file 0 trim)))
+            (when-let ((trim (string-match "\\s +\\'" node)))
+              (setq node (substring node 0 trim)))
+            (when (string= "" file)
+              (setq file Info-current-file))
+            (setq file (file-name-sans-extension (file-name-nondirectory file)))
+            (cond
+             ((member file '("emacs" "elisp"))
+              (setq node (replace-regexp-in-string "[ \t]+" "-" node :fixedcase :literal))
+              (setq url (concat "http://www.gnu.org/software/emacs/manual/html_node/"
+                                file "/" node ".html")))
+             ((string= file "python3")
+              (setq url (modi/Info-python3-doc-url)))
+             (t
+              (error "Manual cannot be `%s'; it can only be `emacs', `elisp' or `python3'" file)))
+            (kill-new url)
+            (when current-prefix-arg
+              (browse-url-of-file url))
+            (message "URL: %s" url)
+            url))
+        (advice-add 'Info-url-for-node :override #'modi/Info-url-for-node)
+
         (bind-keys
          :map Info-mode-map
          ;; Allow mouse scrolling to do its normal thing
          ("<mouse-4>" . nil)
          ("<mouse-5>" . nil)
          ;; Override the Info-mode-map binding to "?" set by info+
+         ("W" . modi/Info-url-for-current-node)
          ("?" . hydra-info/body))))
 
     (defhydra hydra-info (:color blue
