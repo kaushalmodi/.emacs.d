16931f8ad3b85f00b991f25270c5067ced743835
Kaushal Modi <kaushal.modi@gmail.com>
2017-07-13 13:59:13 -0400

    Bind "C-c G" to org-hugo-publish-subtree

--- setup-files/setup-hugo.el
+++ setup-files/setup-hugo.el
@@ -1,11 +1,13 @@
-;; Time-stamp: <2017-07-07 17:51:12 kmodi>
+;; Time-stamp: <2017-07-12 17:02:44 kmodi>
 
 ;; Hugo
 ;; https://gohugo.io
 
 (use-package ox-hugo
   :after ox
-  :load-path "elisp/ox-hugo")
+  :load-path "elisp/ox-hugo"
+  :bind (:map modi-mode-map
+         ("C-c G" . org-hugo-publish-subtree)))
 
 
 (provide 'setup-hugo)
