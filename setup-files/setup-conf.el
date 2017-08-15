a2efa2455807b0ac5e1548a863f774b301c0b566
Kaushal Modi <kaushal.modi@gmail.com>
2017-08-15 12:06:05 -0400

    Set syntax of ' and " to punctuation for some conf-space-mode files

--- setup-files/setup-conf.el
+++ setup-files/setup-conf.el
@@ -1,10 +1,19 @@
-;; Time-stamp: <2015-06-30 16:51:07 kmodi>
+;; Time-stamp: <2017-08-15 12:05:32 kmodi>
 
 ;; Conf Mode
 
 (use-package conf-mode
-  :mode (("\\.conf\\'"    . conf-space-mode)
-         ("\\.setup.*\\'" . conf-space-mode)))
+  :mode (("\\.conf\\'" . conf-space-mode)
+         ("\\.setup.*\\'" . conf-space-mode))
+  :config
+  (progn
+    (defun modi/conf-quote-normal ()
+      "Enable `conf-quote-normal' for *.setup files."
+      (when-let* ((fname (buffer-file-name))
+                  (enable-conf-quote-normal (string-match-p "\\.setup.*" fname)))
+        ;; Set the syntax of ' and " to punctuation.
+        (conf-quote-normal nil)))
+    (add-hook 'conf-space-mode-hook #'modi/conf-quote-normal)))
 
 
 (provide 'setup-conf)
