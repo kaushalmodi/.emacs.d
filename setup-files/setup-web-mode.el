00874fd7b5f52ab5cdcf6bbbc9ac8213cfd9a17b
Kaushal Modi <kaushal.modi@gmail.com>
2018-02-26 16:49:44 -0500

    Use web-mode fork

--- setup-files/setup-web-mode.el
+++ setup-files/setup-web-mode.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2016-11-05 10:54:26 kmodi>
+;; Time-stamp: <2018-02-26 16:18:30 kmodi>
 
 ;; Web Mode
 ;; http://web-mode.org
@@ -7,9 +7,10 @@
   :mode ("\\.html?\\'"
          "\\.php\\'"
          "\\.as[cp]x\\'")
+  :load-path "elisp/web-mode"
   :config
   (progn
-    (setq web-mode-engines-alist '(("gtl" . ".*hugo.*html\\'"))))) ; Go Template
+    (setq web-mode-engines-alist '(("hugo" . ".*hugo.*html\\'"))))) ;Go Template
 
 
 (provide 'setup-web-mode)
