e061614e3dcfa4f9885c2600711e6ddc1989fb16
Kaushal Modi <kaushal.modi@gmail.com>
2018-05-11 13:30:14 -0400

    Remove rss FA icon

--- elisp/misc/fontawesome-choose.el
+++ elisp/misc/fontawesome-choose.el
@@ -1,13 +1,13 @@
-;; Time-stamp: <2018-05-11 01:49:19 kmodi>
+;; Time-stamp: <2018-05-11 13:27:38 kmodi>
 
 ;; Helper function `fontawesome-choose' used to uncomment only the
 ;; icons the user cares about in `fontawesome-all.js'.
 ;; https://scripter.co/optimize-your-fontawesome/
 
-(defconst fontawesome-choose-icons '("rss"
+(defconst fontawesome-choose-icons '("link"
+                                     ;; "rss"
                                      ;; "list-alt" ;categories
                                      ;; "tags"
-                                     "link"
                                      "heart" ;like
                                      "reply"
                                      "retweet"
