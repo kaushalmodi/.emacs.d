921e2a7780b3e605fca9a5d11a7a2bc230948c76
Kaushal Modi <kaushal.modi@gmail.com>
2018-08-21 14:34:29 -0400

    Separate toots using FORM FEED char (C-q C-l) instead of ascii -----

--- setup-files/setup-mastodon.el
+++ setup-files/setup-mastodon.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-08-21 14:33:31 kmodi>
+;; Time-stamp: <2018-08-21 14:34:15 kmodi>
 
 ;; Mastodon
 ;; https://github.com/jdenen/mastodon.el
@@ -10,6 +10,50 @@
   (progn
     (setq mastodon-instance-url "https://mastodon.technology")
     (setq mastodon-tl--enable-proportional-fonts t)
+
+    (with-eval-after-load 'mastodon-tl
+      (defun modi/mastodon-tl--byline (toot author-byline action-byline)
+        "Generate byline for TOOT.
+
+AUTHOR-BYLINE is function for adding the author portion of
+the byline that takes one variable.
+ACTION-BYLINE is a function for adding an action, such as boosting
+favouriting and following to the byline. It also takes a single function. By default
+it is `mastodon-tl--byline-boosted'"
+        (let ((parsed-time (date-to-time (mastodon-tl--field 'created_at toot)))
+              (faved (equal 't (mastodon-tl--field 'favourited toot)))
+              (boosted (equal 't (mastodon-tl--field 'reblogged toot))))
+          (concat
+           (propertize "\n | " 'face 'default)
+           (propertize
+            (concat (when boosted
+                      (format "(%s) "
+                              (propertize "B" 'face 'mastodon-boost-fave-face)))
+                    (when faved
+                      (format "(%s) "
+                              (propertize "F" 'face 'mastodon-boost-fave-face)))
+                    (funcall author-byline toot)
+                    (funcall action-byline toot)
+                    " "
+                    ;; TODO: Once we have a view for toot (responses etc.) make
+                    ;; this a tab stop and attach an action.
+                    (propertize
+                     (format-time-string mastodon-toot-timestamp-format parsed-time)
+                     'timestamp parsed-time
+                     'display (if mastodon-tl--enable-relative-timestamps
+                                  (mastodon-tl--relative-time-description parsed-time)
+                                parsed-time))
+                    ;; Tue Aug 21 14:27:57 EDT 2018 - kmodi
+                    ;; Start of edit
+                    ;; - Replaced "\n  ------------" with
+                    ;;   "\n"
+                    (propertize "\n" 'face 'default)
+                    ;; End of edit
+                    )
+            'favourited-p faved
+            'boosted-p    boosted
+            'byline       t))))
+      (advice-add 'mastodon-tl--byline :override #'modi/mastodon-tl--byline))
 
     (bind-keys
      :map mastodon-mode-map
