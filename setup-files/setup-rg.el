6fa724068672a60f5f7606a6c7d4467cfb6c57b8
Kaushal Modi <kaushal.modi@gmail.com>
2018-08-23 16:29:31 -0400

    Cut the cruft

--- setup-files/setup-rg.el
+++ setup-files/setup-rg.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-08-23 16:15:42 kmodi>
+;; Time-stamp: <2018-08-23 16:29:18 kmodi>
 
 ;; Deadgrep
 ;; https://github.com/Wilfred/deadgrep
@@ -11,71 +11,25 @@
     (bind-to-modi-map "r" #'deadgrep))
   :config
   (progn
-    (defconst modi/deadgrep--rg-args
-      `("--color=ansi"
-        "--line-number"
-        "--no-heading"
-        "--with-filename"
-        "--no-ignore-vcs"       ;Ignore files/dirs ONLY from `.ignore'
+    (defconst modi/deadgrep--rg-extra-args
+      `("--no-ignore-vcs"       ;Ignore files/dirs ONLY from `.ignore'
         "--follow"              ;Follow symlinks
-        "%s"                    ;--fixed-strings and/or --word-regexp
-        "%s"          ;--smart-case / --case-sensitive / --ignore-case
-        "%s"          ;file type
-        "%s"          ;context
         "--ignore-file" ,(expand-file-name ".ignore" (getenv "HOME")))
-      "rg arguments used in the `deadgrep' package.")
+      "Extra rg arguments to be added to `deadgrep--format-command' output.")
 
     ;; Thu Aug 23 15:51:37 EDT 2018 - kmodi
-    ;; Overriding the original `deadgrep--format-command' as I need to
-    ;; add few extra `rg' options.
+    ;; Adding extra arguments to the ones
+    ;; already in `deadgrep--format-command' --
     ;; https://github.com/Wilfred/deadgrep/issues/24
-    (defun modi/deadgrep--format-command (search-term search-type case context)
-      "Return a command string that we can execute in a shell
-to obtain ripgrep results."
-      (let ((cmd (format
-                  (mapconcat #'identity
-                             (append '("%s")       ;`deadgrep-executable'
-                                     modi/deadgrep--rg-args
-                                     '("-- %s .")) ;search term
-                             " ")
-                  deadgrep-executable
-                  (cond
-                   ((eq search-type 'string)
-                    "--fixed-strings")
-                   ((eq search-type 'words)
-                    "--fixed-strings --word-regexp")
-                   ((eq search-type 'regexp)
-                    "")
-                   (t
-                    (error "Unknown search type: %s" search-type)))
-                  (cond
-                   ((eq case 'smart)
-                    "--smart-case")
-                   ((eq case 'sensitive)
-                    "--case-sensitive")
-                   ((eq case 'ignore)
-                    "--ignore-case")
-                   (t
-                    (error "Unknown case: %s" case)))
-                  ;; TODO: pass this as an argument.
-                  (cond
-                   ((eq deadgrep--file-type 'all)
-                    "")
-                   ((eq (car-safe deadgrep--file-type) 'type)
-                    (format "--type %s" (cdr deadgrep--file-type)))
-                   ((eq (car-safe deadgrep--file-type) 'glob)
-                    (format "--type-add 'custom:%s' --type custom"
-                            (cdr deadgrep--file-type)))
-                   (t
-                    (error "Unknown file-type: %S" deadgrep--file-type)))
-                  (if context
-                      (format "--before-context %s --after-context %s"
-                              (car context) (cdr context))
-                    "")
-                  (shell-quote-argument search-term))))
-        ;; (message "%S" cmd)
-        cmd))
-    (advice-add 'deadgrep--format-command :override #'modi/deadgrep--format-command)))
+    (defun modi/deadgrep--format-command-advice (orig-ret-val)
+      "Add arguments from `modi/deadgrep--rg-extra-args' to ORIG-RET-VAL."
+      (replace-regexp-in-string
+       (format "\\`\\(%s \\)\\(.*\\)\\'" (regexp-quote deadgrep-executable))
+       (concat "\\1"
+               (mapconcat #'identity modi/deadgrep--rg-extra-args " ")
+               " \\2")
+       orig-ret-val))
+    (advice-add 'deadgrep--format-command :filter-return #'modi/deadgrep--format-command-advice)))
 
 
 (provide 'setup-rg)
