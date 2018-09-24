082b55b7916db9d3f43b9a9fc4070bd38cfb9c35
Kaushal Modi <kaushal.modi@gmail.com>
2018-09-24 15:56:00 -0400

    Make C-u C-c TAB in Org mode collapse all the Hugo post subtrees

--- setup-files/setup-hugo.el
+++ setup-files/setup-hugo.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-08-22 18:29:34 kmodi>
+;; Time-stamp: <2018-09-24 15:55:46 kmodi>
 
 ;; Hugo
 ;; https://gohugo.io
@@ -42,6 +42,39 @@ See `org-capture-templates' for more information."
                  ;; `org-directory' and that it has a "Blog Ideas" heading.
                  (file+olp "scripter-posts.org" "Blog Ideas")
                  (function org-hugo-new-subtree-post-capture-template))))
+
+(with-eval-after-load 'org
+  (defun modi/org-hugo-collapse-all-posts ()
+    "Collapse all post subtrees in the current Org file.
+Also collapse the Footnotes subtree and COMMENT subtrees if
+present.
+
+A post subtree is one that has the EXPORT_FILE_NAME property set."
+    (interactive)
+    (widen)
+    (org-show-all '(headings))
+    ;; Collapse all the post subtrees (ones with EXPORT_FILE_NAME
+    ;; property set).
+    (save-excursion
+      (goto-char (point-min))
+      (org-map-entries
+       (lambda ()
+         (hide-subtree))
+       "EXPORT_FILE_NAME<>\"\""))
+    ;; Also hide Footnotes and comments.
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward "^\\(\\* Footnotes\\|\\*+ COMMENT\\)" nil :noerror)
+        (hide-subtree))))
+
+  ;; C-u C-c TAB in Org mode -> `modi/org-hugo-collapse-all-posts'
+  (defun modi/org-ctrl-c-tab-advice (&rest args)
+    "Run `modi/org-hugo-collapse-all-posts' when doing \\[universal-argument] \\[org-ctrl-c-tab]."
+    (let ((do-not-run-orig-fn (equal '(4) current-prefix-arg)))
+      (when do-not-run-orig-fn
+        (modi/org-hugo-collapse-all-posts))
+      do-not-run-orig-fn))
+  (advice-add 'org-ctrl-c-tab :before-until #'modi/org-ctrl-c-tab-advice))
 
 
 (provide 'setup-hugo)
