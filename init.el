d5db4228b2473293fcd9392c965c65c73580fb85
Kaushal Modi <kaushal.modi@gmail.com>
2021-09-15 15:35:00 -0400

    Remove keyfreq package
    I haven't used this package in many years.

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2021-06-08 09:57:39 kmodi>
+;; Time-stamp: <2021-09-15 15:34:41 kmodi>
 ;; Author: Kaushal Modi
 
 ;; Global variables
@@ -92,7 +92,6 @@ need Org version to be at least 9.x.")
     isend-mode ; used in setup-perl.el
     ivy swiper counsel
     key-chord ; map pairs of simultaneously pressed keys to commands
-    keyfreq ; find which commands you use the most
     kurecolor ; Quick tweaks to color codes in CSS files
     magit ; for git management
     manage-minor-mode
@@ -269,7 +268,6 @@ need Org version to be at least 9.x.")
 (require 'setup-imenu-list)
 (require 'setup-indent-guide)
 (require 'setup-info)
-(require 'setup-keyfreq)
 (require 'setup-kurecolor)
 (require 'setup-linum)
 (require 'setup-manage-minor-mode)
