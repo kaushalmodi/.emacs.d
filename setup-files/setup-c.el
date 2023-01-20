8b4a414dee2666d53981a641848e1c80d78805de
Kaushal Modi <kaushal.modi@gmail.com>
2023-01-20 16:54:30 -0500

    feat: Open Perspec portable stimulus files in c-mode

--- setup-files/setup-c.el
+++ setup-files/setup-c.el
@@ -1,8 +1,9 @@
-;; Time-stamp: <2020-09-25 17:41:33 kmodi>
+;; Time-stamp: <2023-01-20 16:54:21 kmodi>
 
 ;; C/C++
 
 (use-package cc-mode
+  :mode (("\\.pss\\'" . c-or-c++-mode)) ;Perspec Portable Stimulus files
   :config
   (progn
     (setq-default c-basic-offset 4)))
