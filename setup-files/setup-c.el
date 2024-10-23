0b2688525871df3582bca5c11b92c8b0af04767e
Kaushal Modi <kaushal.modi@gmail.com>
2024-10-23 12:17:32 -0400

    feat: Set the major mode of all Portable Stimulus files to be C++

--- setup-files/setup-c.el
+++ setup-files/setup-c.el
@@ -1,9 +1,11 @@
-;; Time-stamp: <2024-10-15 11:27:12 kmodi>
+;; Time-stamp: <2024-10-17 18:06:19 kmodi>
 
 ;; C/C++
 
 (use-package cc-mode
-  :mode (("\\.pss\\'" . c++-mode)) ;Portable Stimulus files
+  :mode (("\\.pss\\'" . c++-mode)         ;Portable Stimulus files
+         ("\\.psf\\'" . c++-mode)
+         ("\\.sln\\'" . c++-mode))
   :config
   (progn
     (setq-default c-basic-offset 3)))
