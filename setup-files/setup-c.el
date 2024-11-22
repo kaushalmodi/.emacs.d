1d445de79c2c1ae9f153f52ce28f82dd54b762d6
Kaushal Modi <kaushal.modi@gmail.com>
2024-11-22 16:08:26 -0500

    feat: PSS sanitization: Single space on both sides of `:' when deriving

--- setup-files/setup-c.el
+++ setup-files/setup-c.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2024-11-22 09:04:55 kmodi>
+;; Time-stamp: <2024-11-22 15:50:55 kmodi>
 
 ;; C/C++
 
@@ -32,6 +32,14 @@
           (goto-char (point-min))
           (while (re-search-forward "bit\\s-+\\[" nil :noerror)
             (replace-match "bit[")))
+
+        ;; Single space on both sides of the `:' when deriving
+        ;; actions, components, structs, etc.
+        (save-excursion
+          (goto-char (point-min))
+          (while (re-search-forward "\\(^\\|<\\)\\(\\s-*\\([a-z_]+\\s-+\\)?[a-z_]+\\s-+[a-z0-9A-Z_]+\\)\\s-*:\\s-*\\([a-z0-9A-Z_]+\\)" nil :noerror)
+            ;;                              pure         component   der_c                   base_c
+            (replace-match "\\1\\2 : \\4")))
 
         ;; ;; Auto-indent the file
         ;; (indent-region (point-min) (point-max))
