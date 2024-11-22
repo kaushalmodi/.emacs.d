a6be0feaef185c4f72fd35347759a985b76ddf62
Kaushal Modi <kaushal.modi@gmail.com>
2024-11-22 09:05:45 -0500

    feat: Updates to the PSS sanitization code
    1. Now the auto semicolon insertion after `}' works even if the
       closing brace is followed by a comment. Also it does a stricter
       search for the closing brace -- the brace needs to be preceeded by
       0 or more spaces.
    2. When declaring bit vectors, don't use space between `bit' and `[N]'.

--- setup-files/setup-c.el
+++ setup-files/setup-c.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2024-10-25 09:57:16 kmodi>
+;; Time-stamp: <2024-11-22 09:04:55 kmodi>
 
 ;; C/C++
 
@@ -18,14 +18,20 @@
         ;; auto-indentation.
         (save-excursion
           (goto-char (point-min))
-          (while (re-search-forward "\\(\\}\\)\\s-*$" nil :noerror)
-            (replace-match "\\1;")))
+          (while (re-search-forward "^\\(\\s-*\\}\\)\\s-*\\(//.*\\)?$" nil :noerror)
+            (replace-match "\\1; \\2")))
 
         ;; Always a single space after commas, not before!
         (save-excursion
           (goto-char (point-min))
           (while (re-search-forward "\\s-*,\\s-*" nil :noerror)
             (replace-match ", ")))
+
+        ;; Always `bit[N]' and not `bit [N]'.
+        (save-excursion
+          (goto-char (point-min))
+          (while (re-search-forward "bit\\s-+\\[" nil :noerror)
+            (replace-match "bit[")))
 
         ;; ;; Auto-indent the file
         ;; (indent-region (point-min) (point-max))
