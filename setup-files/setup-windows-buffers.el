9b7d503973cdda58684abc92c6d8cd972705b53d
Kaushal Modi <kaushal.modi@gmail.com>
2026-09-15 17:23:26 -0400

    chore(buffers): Remove the `kill-current-buffer' polyfill
    The command has been in core since Emacs 26.1 and the `>=e "26.0"' gate
    never defined the copy on the versions this config runs on.

--- setup-files/setup-windows-buffers.el
+++ setup-files/setup-windows-buffers.el
@@ -484,23 +484,6 @@ the current window and the windows state prior to that."
 
 ;;; Kill/Bury Buffer
 
-;; http://git.savannah.gnu.org/cgit/emacs.git/commit/?id=2e4f4c9d48c563ff8bec102b66da0225587786c6
-(>=e "26.0"
-    nil  ;The `kill-current-buffer' command will be defined in core in emacs 26+
-  (defun kill-current-buffer ()
-    "Kill the current buffer.
-When called in the minibuffer, get out of the minibuffer
-using `abort-recursive-edit'.
-
-This is like `kill-this-buffer', but it doesn't have to be invoked
-via the menu bar, and pays no attention to the menu-bar's frame."
-    (interactive)
-    (let ((frame (selected-frame)))
-      (if (and (frame-live-p frame)
-               (not (window-minibuffer-p (frame-selected-window frame))))
-          (kill-buffer (current-buffer))
-        (abort-recursive-edit)))))
-
 (defun modi/kill-buffer-dwim (kill-next-error-buffer)
   "Kill the current buffer.
 When called in the minibuffer, get out of the minibuffer
