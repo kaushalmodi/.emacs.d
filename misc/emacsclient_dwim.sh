fe8ba21ca866a3f7279159aafe354dcc792336f6
Kaushal Modi <kaushal.modi@gmail.com>
2026-09-16 14:15:54 -0400

    emacsclient_dwim: Detect existing GUI frames robustly
    Use 'display-graphic-p' to check for an existing graphical frame instead
    of matching $DISPLAY against 'terminal-name' strings, which was brittle
    and broke on pgtk/Wayland builds.
    
    - Detect $WAYLAND_DISPLAY in addition to $DISPLAY
    - Build the emacsclient options in an array to avoid word splitting, and
      fix the '${*/#-nw/}' check that matched '-nw' only as the first arg
    - Honor an explicit -nw/-t/--tty by not adding -c/-n
    - Don't duplicate a user-supplied -c/--create-frame
    - Gate debug output behind $EMACSCLIENT_DWIM_DEBUG

--- misc/emacsclient_dwim.sh
+++ misc/emacsclient_dwim.sh
@@ -13,54 +13,58 @@
 #               # loads files from my saved desktop, etc.
 # > e foo.txt & # Opens foo.txt in the already opened emacsclient frame.
 
-debug=0
+debug=${EMACSCLIENT_DWIM_DEBUG:-0}
 
-if [[ "$DISPLAY" ]]
+opts=()
+
+# Did the user explicitly ask for a terminal frame?
+tty_requested=0
+# .. or explicitly ask for a new frame?
+frame_requested=0
+for arg in "$@"
+do
+    case "$arg" in
+        -nw|-t|--tty)
+            tty_requested=1
+            ;;
+        -c|--create-frame)
+            frame_requested=1
+            ;;
+    esac
+done
+
+# Works for both X11 ($DISPLAY) and Wayland/pgtk ($WAYLAND_DISPLAY) builds.
+if [[ ${tty_requested} -eq 0 ]] && [[ -n "$DISPLAY" || -n "$WAYLAND_DISPLAY" ]]
 then
-    # If $DISPLAY is ":1", set $disp to ":1"
-    # If $DISPLAY is ":1.0", still set $disp to ":1"
-    # shellcheck disable=SC2001
-    disp=$(echo "$DISPLAY" | sed 's/\.0$//')
-    # Handle the cases where the display number returned by `terminal-name'
-    # could be either something like ":1" or ":1.0", and even $DISPLAY can be
-    # ":1" or ":1.0"!
+    # Is there any graphical frame already open?
     frame=$(emacsclient -a '' \
-                        -e '(or
-                               (member "'"$disp.0"'" (mapcar (quote terminal-name) (frames-on-display-list)))
-                               (member "'"$disp"'" (mapcar (quote terminal-name) (frames-on-display-list)))
-                            )' 2>/dev/null)
+                        -e '(if (seq-some (function display-graphic-p) (frame-list)) t nil)' \
+                        2>/dev/null)
 
     if [[ ${debug} -eq 1 ]]
     then
-        echo "try1"
-        echo "disp = $disp"
-        echo "(mapcar (quote terminal-name) (frames-on-display-list)) = $(emacsclient -a '' -e '(mapcar (quote terminal-name) (frames-on-display-list))')"
-        emacsclient -a '' -e '(member "'"$disp.0"'" (mapcar (quote terminal-name) (frames-on-display-list)))'
-        echo "try2"
-        emacsclient -a '' -e '(member "'"$disp"'" (mapcar (quote terminal-name) (frames-on-display-list)))'
-        echo "dgb: DISPLAY=$DISPLAY disp=$disp frame=$frame"
+        echo "dbg: DISPLAY=$DISPLAY WAYLAND_DISPLAY=$WAYLAND_DISPLAY"
+        echo "dbg: graphical frame present = $frame"
+        echo "dbg: terminal names = $(emacsclient -a '' -e '(mapcar (function terminal-name) (frame-list))' 2>/dev/null)"
     fi
 
-    # If there is no frame open create one
-    if [[ "$frame" == "nil" ]]
+    # If there is no graphical frame open, create one.
+    if [[ "$frame" != "t" && ${frame_requested} -eq 0 ]]
     then
-        opts='-c'
+        opts+=('-c')
     fi
 
-    # Don't wait (append the -n option) only if the user hasn't specified to
-    # run in the terminal (-nw option).
-    # http://www.tldp.org/LDP/abs/html/parameter-substitution.html
-    # ${var#Pattern} Remove from $var the shortest part of $Pattern that matches
-    # the front end of $var.
-    # So if "$*" is "-nw foo.txt", ${*/#-nw/} will be just " foo.txt"
-    if [[ "${*/#-nw/}" == "$*" ]]
-    then
-        opts="$opts -n"
-    fi
-else
-    # Otherwise, open emacsclient in the terminal
-    echo "DISPLAY env var is unset"
-    opts='-nw'
+    # Don't block the shell.
+    opts+=('-n')
+elif [[ ${tty_requested} -eq 0 ]]
+then
+    # No graphical display available, so open emacsclient in the terminal.
+    opts+=('-nw')
 fi
 
-exec emacsclient -a '' $opts "$@"
+if [[ ${debug} -eq 1 ]]
+then
+    echo "dbg: emacsclient -a '' ${opts[*]} $*"
+fi
+
+exec emacsclient -a '' "${opts[@]}" "$@"
