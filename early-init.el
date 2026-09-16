4fdb8ccf0871e6c4b901a356844eaa044b60669d
Kaushal Modi <kaushal.modi@gmail.com>
2026-09-16 09:30:04 -0400

    docs: Require Emacs 30.1 or newer
    State the new minimum in the README, and error out early in
    early-init.el so an older Emacs fails with that message instead of a
    confusing `void-function' or `void-variable' partway through init.

--- early-init.el
+++ early-init.el
@@ -2,5 +2,11 @@
 ;; Time-stamp: <2018-02-20 14:49:15 kmodi>
 ;; Emacs 27.x: http://git.savannah.gnu.org/cgit/emacs.git/commit/?id=24acb31c04b4048b85311d794e600ecd7ce60d3b
 
+;; This config uses features that do not exist in earlier releases; fail
+;; here instead of at a random `void-function' later.
+(when (version< emacs-version "30.1")
+  (error "This Emacs config requires Emacs 30.1 or newer; this is Emacs %s"
+         emacs-version))
+
 (setq package-user-dir (let ((elpa-dir-name (format "elpa_%s" emacs-major-version))) ;default = ~/.emacs.d/elpa/
                          (file-name-as-directory (expand-file-name elpa-dir-name user-emacs-directory))))
